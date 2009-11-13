492c77864c7c4ad6350bd473a9da5efb714b4cf1
vimartin84 <vimartin84@users.sourceforge.net>
2009-11-13 14:04:41 +0000

    Broadcasts added before printing network

--- backends/src/net/sf/orcc/backends/c/quasistatic/CQuasiStaticBackendImpl.java
+++ backends/src/net/sf/orcc/backends/c/quasistatic/CQuasiStaticBackendImpl.java
@@ -1,49 +1,3 @@
-package net.sf.orcc.backends.c.quasistatic;
-
-/*
- * Copyright (c) 2009, Infotech Oulu ( Machine Vision Group )
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *   * Neither the name of the IETR/INSA of Rennes nor the names of its
- *     contributors may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
- * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-
-import net.sf.orcc.backends.c.CBackendImpl;
-import net.sf.orcc.backends.c.quasistatic.core.org.efsmScheduler.main.Scheduler_Simulator;
-import net.sf.orcc.backends.c.quasistatic.core.org.efsmScheduler.output.ConfigFilesCreator;
-import net.sf.orcc.backends.c.quasistatic.core.org.efsmScheduler.utilities.FileUtilities;
-import net.sf.orcc.backends.c.quasistatic.parsers.CQuasiStaticActorParser;
-import net.sf.orcc.backends.c.quasistatic.parsers.CQuasiStaticNetworksParser;
-import net.sf.orcc.backends.c.quasistatic.utils.CQuasiStaticConstants;
-import net.sf.orcc.ir.Actor;
-import net.sf.orcc.ir.NameTransformer;
-import net.sf.orcc.network.Network;
-
 /*
  * Copyright(c)2009 Victor Martin, Jani Boutellier
  * All rights reserved.
@@ -70,6 +24,25 @@ import net.sf.orcc.network.Network;
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+package net.sf.orcc.backends.c.quasistatic;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import net.sf.orcc.backends.c.CBackendImpl;
+import net.sf.orcc.backends.c.quasistatic.core.org.efsmScheduler.main.Scheduler_Simulator;
+import net.sf.orcc.backends.c.quasistatic.core.org.efsmScheduler.output.ConfigFilesCreator;
+import net.sf.orcc.backends.c.quasistatic.core.org.efsmScheduler.utilities.FileUtilities;
+import net.sf.orcc.backends.c.quasistatic.parsers.CQuasiStaticActorParser;
+import net.sf.orcc.backends.c.quasistatic.parsers.CQuasiStaticNetworksParser;
+import net.sf.orcc.backends.c.quasistatic.utils.CQuasiStaticConstants;
+import net.sf.orcc.ir.Actor;
+import net.sf.orcc.ir.NameTransformer;
+import net.sf.orcc.network.Network;
+import net.sf.orcc.network.transforms.BroadcastAdder;
 
 public class CQuasiStaticBackendImpl extends CBackendImpl {
 
@@ -172,6 +145,10 @@ public class CQuasiStaticBackendImpl extends CBackendImpl {
 	protected void printNetwork(Network network) throws Exception {
 		CQuasiStaticNetworkPrinter networkPrinter = new CQuasiStaticNetworkPrinter();
 		String outputName = path + File.separator + network.getName() + ".c";
+		
+		// Add broadcasts before printing
+		new BroadcastAdder().transform(network);
+		
 		networkPrinter.printNetwork(outputName, network, false, fifoSize);
 		parseNetworkFiles();
 	}
