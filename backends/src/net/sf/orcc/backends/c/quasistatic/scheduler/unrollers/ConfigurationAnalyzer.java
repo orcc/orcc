ad3beede10fcd21e4755028ec15938adc1640596
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2009-12-03 14:08:07 +0000

    handle simple bitand constraints. Not perfect yet though (in particular invpred is not handled yet).

--- backends/src/net/sf/orcc/backends/c/quasistatic/scheduler/unrollers/ConfigurationAnalyzer.java
+++ backends/src/net/sf/orcc/backends/c/quasistatic/scheduler/unrollers/ConfigurationAnalyzer.java
@@ -36,6 +36,7 @@ import java.util.Map;
 import java.util.Set;
 
 import jp.ac.kobe_u.cs.cream.DefaultSolver;
+import jp.ac.kobe_u.cs.cream.IntDomain;
 import jp.ac.kobe_u.cs.cream.IntVariable;
 import jp.ac.kobe_u.cs.cream.Network;
 import jp.ac.kobe_u.cs.cream.Solution;
@@ -44,17 +45,18 @@ import net.sf.orcc.ir.Action;
 import net.sf.orcc.ir.ActionScheduler;
 import net.sf.orcc.ir.Actor;
 import net.sf.orcc.ir.CFGNode;
-import net.sf.orcc.ir.Expression;
 import net.sf.orcc.ir.FSM;
 import net.sf.orcc.ir.Port;
 import net.sf.orcc.ir.Procedure;
 import net.sf.orcc.ir.Variable;
 import net.sf.orcc.ir.FSM.NextStateInfo;
-import net.sf.orcc.ir.expr.AbstractExpressionVisitor;
 import net.sf.orcc.ir.expr.BinaryExpr;
 import net.sf.orcc.ir.expr.BinaryOp;
 import net.sf.orcc.ir.expr.BoolExpr;
+import net.sf.orcc.ir.expr.ExpressionInterpreter;
 import net.sf.orcc.ir.expr.IntExpr;
+import net.sf.orcc.ir.expr.ListExpr;
+import net.sf.orcc.ir.expr.StringExpr;
 import net.sf.orcc.ir.expr.UnaryExpr;
 import net.sf.orcc.ir.expr.VarExpr;
 import net.sf.orcc.ir.instructions.Assign;
@@ -72,6 +74,214 @@ import net.sf.orcc.ir.transforms.AbstractActorTransformation;
 public class ConfigurationAnalyzer {
 
 	/**
+	 * This class defines a visitor that examines expressions that depend on a
+	 * value peeked from the configuration port.
+	 * 
+	 * @author Matthieu Wipliez
+	 * 
+	 */
+	private class ConstraintExpressionVisitor implements ExpressionInterpreter {
+
+		private IntVariable constraintVariable;
+
+		private Variable target;
+
+		/**
+		 * Creates a new constraint expression visitor that adds constraints on
+		 * the given token variables to expressions that involve the given
+		 * target.
+		 * 
+		 * @param target
+		 *            target variable
+		 * @param constraintVariable
+		 *            constraint variable
+		 */
+		public ConstraintExpressionVisitor(Variable target,
+				IntVariable constraintVariable) {
+			this.target = target;
+			this.constraintVariable = constraintVariable;
+		}
+
+		/**
+		 * Adds a constraint between an integer variable and either another
+		 * integer variable or an integer value.
+		 * 
+		 * @param v1
+		 *            an integer variable
+		 * @param op
+		 *            a binary operator
+		 * @param o2
+		 *            an integer variable or an integer value
+		 */
+		private Object addConstraint(IntVariable v1, BinaryOp op, Object o2) {
+			switch (op) {
+			case BITAND:
+				return addConstraintBitand(v1, o2);
+			case EQ:
+				if (o2 instanceof IntVariable) {
+					v1.equals((IntVariable) o2);
+				} else if (o2 instanceof Integer) {
+					v1.equals((Integer) o2);
+				} else {
+					break;
+				}
+				return v1;
+			case GE:
+				if (o2 instanceof IntVariable) {
+					v1.ge((IntVariable) o2);
+				} else if (o2 instanceof Integer) {
+					v1.ge((Integer) o2);
+				} else {
+					break;
+				}
+				return v1;
+			case GT:
+				if (o2 instanceof IntVariable) {
+					v1.gt((IntVariable) o2);
+				} else if (o2 instanceof Integer) {
+					v1.gt((Integer) o2);
+				} else {
+					break;
+				}
+				return v1;
+			case LE:
+				if (o2 instanceof IntVariable) {
+					v1.le((IntVariable) o2);
+				} else if (o2 instanceof Integer) {
+					v1.le((Integer) o2);
+				} else {
+					break;
+				}
+				return v1;
+			case LT:
+				if (o2 instanceof IntVariable) {
+					v1.lt((IntVariable) o2);
+				} else if (o2 instanceof Integer) {
+					v1.lt((Integer) o2);
+				} else {
+					break;
+				}
+				return v1;
+			case NE:
+				if (o2 instanceof IntVariable) {
+					v1.notEquals((IntVariable) o2);
+				} else if (o2 instanceof Integer) {
+					v1.notEquals((Integer) o2);
+				} else {
+					break;
+				}
+				return v1;
+			}
+
+			return null;
+		}
+
+		private Object addConstraintBitand(IntVariable v1, Object o2) {
+			if (!(o2 instanceof Integer)) {
+				return null;
+			}
+
+			int mask = (Integer) o2;
+			double numBits = Math.log(mask) / Math.log(2);
+			if (numBits - Math.floor(numBits) > 0.0) {
+				throw new OrccRuntimeException(
+						"masks not a power of two not supported");
+			}
+
+			Network network = v1.getNetwork();
+
+			// step 1: let x = v1 divided by MASK
+			// x = v1 / MASK <=> v1 = x * MASK
+			IntVariable x = new IntVariable(network);
+			IntVariable xTimesMask = x.multiply(mask);
+			xTimesMask.equals(v1);
+
+			// step 2: let result is r = x modulo 2
+			// <=> x = 2 * q + r with q in 0..MAX, r in domain 0..1
+
+			IntVariable r = new IntVariable(network, 0, 1);
+
+			// x = y + r
+			IntVariable y = new IntVariable(network);
+			IntVariable sum = y.add(r);
+			sum.equals(x);
+
+			// y = 2 * q
+			IntVariable q = new IntVariable(network, 0, IntDomain.MAX_VALUE);
+			IntVariable twoTimesQ = q.multiply(2);
+			y.equals(twoTimesQ);
+
+			return r;
+		}
+
+		@Override
+		public Object interpret(BinaryExpr expr, Object... args) {
+			Object o1 = expr.getE1().accept(this);
+			Object o2 = expr.getE2().accept(this);
+
+			if (o1 instanceof IntVariable) {
+				IntVariable v1 = (IntVariable) o1;
+				return addConstraint(v1, expr.getOp(), o2);
+			} else {
+				if (o2 instanceof IntVariable) {
+					IntVariable v2 = (IntVariable) o2;
+					return addConstraint(v2, expr.getOp()
+							.inequalityOpChangeOrder(), o1);
+				} else {
+					// no variable in binary expression
+					return null;
+				}
+			}
+		}
+
+		@Override
+		public Object interpret(BoolExpr expr, Object... args) {
+			return null;
+		}
+
+		@Override
+		public Object interpret(IntExpr expr, Object... args) {
+			return expr.getValue();
+		}
+
+		@Override
+		public Object interpret(ListExpr expr, Object... args) {
+			return null;
+		}
+
+		@Override
+		public Object interpret(StringExpr expr, Object... args) {
+			return null;
+		}
+
+		@Override
+		public Object interpret(UnaryExpr expr, Object... args) {
+			switch (expr.getOp()) {
+			case MINUS:
+				Object obj = expr.getExpr().accept(this);
+				if (obj instanceof IntVariable) {
+					return ((IntVariable) obj).negate();
+				} else if (obj instanceof Integer) {
+					return -((Integer) obj);
+				}
+			}
+
+			return null;
+		}
+
+		@Override
+		public Object interpret(VarExpr expr, Object... args) {
+			Variable variable = expr.getVar().getVariable();
+			if (variable.equals(target)) {
+				return constraintVariable;
+			} else {
+				return null;
+			}
+		}
+
+	}
+
+	/**
 	 * This class defines a visitor that examines guards that depend on a value
 	 * peeked from the configuration port. This visitor is only run if there is
 	 * a configuration port.
@@ -81,168 +291,7 @@ public class ConfigurationAnalyzer {
 	 */
 	private class GuardVisitor extends AbstractActorTransformation {
 
-		private class BitandVisitor extends AbstractExpressionVisitor {
-
-			private boolean bitandOnly;
-
-			private List<Integer> bitands;
-
-			public BitandVisitor() {
-				bitands = new ArrayList<Integer>();
-				bitandOnly = true;
-			}
-
-			/**
-			 * Returns a or'ed combination of all bitands performed on the
-			 * target variable.
-			 * 
-			 * @return an integer
-			 */
-			public int getValue() {
-				int result = 0;
-				for (Integer value : bitands) {
-					result |= value;
-				}
-
-				return result;
-			}
-
-			public boolean hasBitandOnly() {
-				return bitandOnly;
-			}
-
-			@Override
-			public void visit(BinaryExpr expr, Object... args) {
-				Expression e1 = expr.getE1();
-				int t1 = e1.getType();
-				Expression e2 = expr.getE2();
-				int t2 = e2.getType();
-				BinaryOp op = expr.getOp();
-
-				if (op == BinaryOp.NE) {
-					int val = 0;
-					BinaryExpr binary = null;
-
-					if (t1 == Expression.INT && t2 == Expression.BINARY) {
-						val = ((IntExpr) e1).getValue();
-						binary = (BinaryExpr) e2;
-					} else if (t1 == Expression.BINARY && t2 == Expression.INT) {
-						binary = (BinaryExpr) e1;
-						val = ((IntExpr) e2).getValue();
-					}
-
-					// we get a bitand with our target variable
-					if (binary != null && val == 0) {
-						binary.accept(this);
-						bitandOnly &= true;
-						return;
-					}
-				} else if (op == BinaryOp.BITAND) {
-					int val = 0;
-					Variable var = null;
-
-					if (t1 == Expression.INT && t2 == Expression.VAR) {
-						val = ((IntExpr) e1).getValue();
-						var = ((VarExpr) e2).getVar().getVariable();
-					} else if (t1 == Expression.VAR && t2 == Expression.INT) {
-						var = ((VarExpr) e1).getVar().getVariable();
-						val = ((IntExpr) e2).getValue();
-					}
-
-					// we get a bitand with our target variable
-					if (target.equals(var)) {
-						bitands.add(val);
-						bitandOnly &= true;
-						return;
-					}
-				} else if (op == BinaryOp.LOGIC_AND) {
-					// visits branches
-					e1.accept(this);
-					e2.accept(this);
-					bitandOnly &= true;
-					return;
-				}
-
-				bitandOnly = false;
-			}
-
-			@Override
-			public void visit(BoolExpr expr, Object... args) {
-				bitandOnly = false;
-			}
-
-			@Override
-			public void visit(IntExpr expr, Object... args) {
-				bitandOnly = false;
-			}
-
-			@Override
-			public void visit(UnaryExpr expr, Object... args) {
-				bitandOnly = false;
-			}
-
-			@Override
-			public void visit(VarExpr expr, Object... args) {
-				bitandOnly = false;
-			}
-
-		}
-
-		private class SimpleArithVisitor extends AbstractExpressionVisitor {
-
-			/**
-			 * Adds a constraint if op is a comparison operator.
-			 * 
-			 * @param op
-			 *            a binary operator
-			 * @param value
-			 *            an integer value
-			 */
-			private void addConstraint(BinaryOp op, int value) {
-				switch (op) {
-				case EQ:
-					tokenVariable.equals(value);
-					break;
-				case GE:
-					tokenVariable.ge(value);
-					break;
-				case GT:
-					tokenVariable.gt(value);
-					break;
-				case LE:
-					tokenVariable.le(value);
-					break;
-				case LT:
-					tokenVariable.lt(value);
-					break;
-				case NE:
-					tokenVariable.notEquals(value);
-					break;
-				}
-			}
-
-			@Override
-			public void visit(BinaryExpr expr, Object... args) {
-				Expression e1 = expr.getE1();
-				int t1 = e1.getType();
-				Expression e2 = expr.getE2();
-				int t2 = e2.getType();
-				int val;
-
-				if (t1 == Expression.INT && t2 == Expression.VAR
-						&& target.equals(((VarExpr) e2).getVar().getVariable())) {
-					val = ((IntExpr) e1).getValue();
-					addConstraint(expr.getOp().inequalityOpChangeOrder(), val);
-				} else if (t1 == Expression.VAR && t2 == Expression.INT
-						&& target.equals(((VarExpr) e1).getVar().getVariable())) {
-					val = ((IntExpr) e2).getValue();
-					addConstraint(expr.getOp(), val);
-				} else {
-					throw new OrccRuntimeException("guard not understood");
-				}
-			}
-
-		}
+		private IntVariable constraintVariable;
 
 		private Network network;
 
@@ -250,30 +299,24 @@ public class ConfigurationAnalyzer {
 
 		private Variable tokens;
 
-		private IntVariable tokenVariable;
-
 		public GuardVisitor() {
 			network = new Network();
 		}
 
+		/**
+		 * Returns the constraint variable.
+		 * 
+		 * @return the constraint variable
+		 */
 		public IntVariable getVariable() {
-			return tokenVariable;
+			return constraintVariable;
 		}
 
 		@Override
 		public void visit(Assign assign, Object... args) {
-			BitandVisitor visitor = new BitandVisitor();
+			ConstraintExpressionVisitor visitor = new ConstraintExpressionVisitor(
+					target, constraintVariable);
 			assign.getValue().accept(visitor);
-			if (visitor.hasBitandOnly()) {
-				int bitand = visitor.getValue();
-				System.out.println("bitand = " + bitand);
-			} else {
-				try {
-					assign.getValue().accept(new SimpleArithVisitor());
-				} catch (OrccRuntimeException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 
 		@Override
@@ -287,7 +330,7 @@ public class ConfigurationAnalyzer {
 		public void visit(Peek peek, Object... args) {
 			if (peek.getPort().equals(port)) {
 				tokens = peek.getTarget();
-				tokenVariable = new IntVariable(network, port.getName());
+				constraintVariable = new IntVariable(network, port.getName());
 			}
 		}
 
@@ -429,18 +472,19 @@ public class ConfigurationAnalyzer {
 	public int getConfigurationValue(Action action) {
 		IntVariable variable = values.get(action);
 
-		DefaultSolver solver = new DefaultSolver(variable.getNetwork());
-		Solution solution = solver.findFirst();
-		if (solution == null) {
-			System.out.println("no solutions found for " + action
-					+ ", returning 0");
-			return 0;
-		} else {
-			int value = solution.getIntValue(variable);
-			System.out.println("solution found for " + action + ", returning "
-					+ value);
-			return value;
+		if (variable != null) {
+			DefaultSolver solver = new DefaultSolver(variable.getNetwork());
+			Solution solution = solver.findFirst();
+			if (solution != null) {
+				int value = solution.getIntValue(variable);
+				System.out.println("solution found for " + action
+						+ ", returning " + value);
+				return value;
+			}
 		}
+
+		System.out.println("returning 0 for " + action);
+		return 0;
 	}
 
 	/**
