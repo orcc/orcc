e32a3d1aa3e8229a21de9ac7b2483077d68bbb10
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-02-25 16:05:47 +0000

    added Xtext projects net.sf.orcc.cal and net.sf.orcc.cal.ui

---
+++ plugins/cal/src/net/sf/orcc/validation/CalJavaValidator.java
@@ -0,0 +1,13 @@
+package net.sf.orcc.validation;
+ 
+
+public class CalJavaValidator extends AbstractCalJavaValidator {
+
+//	@Check
+//	public void checkTypeNameStartsWithCapital(Type type) {
+//		if (!Character.isUpperCase(type.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.TYPE__NAME);
+//		}
+//	}
+
+}
