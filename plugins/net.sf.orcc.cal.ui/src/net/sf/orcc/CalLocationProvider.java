b8104f6b08e6e17b3a837bd9dec114b1bf94e881
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-03-03 15:56:34 +0000

    better outline

---
+++ plugins/net.sf.orcc.cal.ui/src/net/sf/orcc/CalLocationProvider.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2010, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc;
+
+import net.sf.orcc.cal.Action;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.xtext.ui.core.DefaultLocationInFileProvider;
+
+/**
+ * This class provides location for the objects of the AST.
+ * 
+ * @author Matthieu Wipliez
+ * 
+ */
+public class CalLocationProvider extends DefaultLocationInFileProvider {
+
+	@Override
+	protected EStructuralFeature getIdentifierFeature(EObject obj) {
+		final EClass eClass = obj.eClass();
+		if (obj instanceof Action) {
+			return eClass.getEStructuralFeature("tag");
+		} else {
+			return super.getIdentifierFeature(obj);
+		}
+	}
+	
+}
