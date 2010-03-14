a765b73ef39758ba066511e7e941fa46c7b94599
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-03-14 15:48:53 +0000

    merged VS-2005 and VS-2008 in cmake.bat. Added support of Visual Studio 2010.

--- projects/CMake/windows/cmake.bat
+++ projects/CMake/windows/cmake.bat
@@ -1,3 +1,20 @@
-@set CMAKE_PREFIX_PATH=%CD%\SDL-1.2.13;%CD%\SDL_image-1.2.7;%CD%\orcc
-@set CMAKE_INSTALL_PREFIX=%CD%\orcc
-@cmake-gui
+@echo off
+
+if defined VS100COMNTOOLS (
+  echo Visual Studio 2010 found, calling "%VS100COMNTOOLS%vsvars32.bat"
+  call "%VS100COMNTOOLS%vsvars32.bat"
+) else if defined VS90COMNTOOLS (
+  echo Visual Studio 2008 found, calling "%VS90COMNTOOLS%vsvars32.bat"
+  call "%VS90COMNTOOLS%vsvars32.bat"
+) else if defined VS80COMNTOOLS (
+  echo Visual Studio 2005 found, calling "%VS80COMNTOOLS%vsvars32.bat"
+  call "%VS80COMNTOOLS%vsvars32.bat"
+) else (
+  echo Visual Studio not found.
+)
+
+set CMAKE_PREFIX_PATH=%CD%\SDL-1.2.13;%CD%\SDL_image-1.2.7;%CD%\orcc
+set CMAKE_INSTALL_PREFIX=%CD%\orcc
+
+echo Launching "cmake-gui"
+cmake-gui
