c7ca92ea1c35293f38260f8e2aef08666ba3b359
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-03-31 16:47:17 +0000

    more qualified name providing.

--- plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/naming/CalQualifiedNameProvider.java
+++ plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/naming/CalQualifiedNameProvider.java
@@ -1,14 +1,22 @@
 package net.sf.orcc.cal.naming;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import net.sf.orcc.cal.cal.Action;
+import net.sf.orcc.cal.cal.ForeachStatement;
+import net.sf.orcc.cal.cal.Generator;
 import net.sf.orcc.cal.cal.Tag;
 
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
 
 public class CalQualifiedNameProvider extends
 		DefaultDeclarativeQualifiedNameProvider {
+
+	private Map<ForeachStatement, Integer> foreachCount = new HashMap<ForeachStatement, Integer>();
+
+	private Map<Generator, Integer> generatorCount = new HashMap<Generator, Integer>();
 
 	public String qualifiedName(Action action) {
 		Tag tag = action.getTag();
@@ -17,6 +25,32 @@ public class CalQualifiedNameProvider extends
 		} else {
 			return getQualifiedName(tag);
 		}
+	}
+
+	public String qualifiedName(ForeachStatement statement) {
+		Integer count = foreachCount.get(statement);
+		if (count == null) {
+			foreachCount.put(statement, 0);
+			count = 0;
+		} else {
+			count++;
+			foreachCount.put(statement, count);
+		}
+		
+		return "foreach." + count;
+	}
+
+	public String qualifiedName(Generator generator) {
+		Integer count = generatorCount.get(generator);
+		if (count == null) {
+			generatorCount.put(generator, 0);
+			count = 0;
+		} else {
+			count++;
+			generatorCount.put(generator, count);
+		}
+		
+		return "generator." + count;
 	}
 
 	public String qualifiedName(Tag tag) {
