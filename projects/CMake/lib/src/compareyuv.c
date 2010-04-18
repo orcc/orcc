5585750b0fb5b4d8a0213aa21c4d11334b9b5ac7
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-04-18 21:32:07 +0000

    pretty-printed compareyuv.c

--- projects/CMake/lib/src/compareyuv.c
+++ projects/CMake/lib/src/compareyuv.c
@@ -66,104 +66,86 @@ static unsigned char Y[MAX_WIDTH * MAX_HEIGHT];
 static unsigned char U[MAX_WIDTH * MAX_HEIGHT / 4];
 static unsigned char V[MAX_WIDTH * MAX_HEIGHT / 4];
 static FILE *ptfile ;
-static char *ARG_INPUTFILE=0;
-static int  xsize_int ;
-static int  ysize_int ;
-static int  images = 0 ;
+static char *ARG_INPUTFILE = 0;
+static int  xsize_int;
+static int  ysize_int;
+static int  images = 0;
 
-
-
-int Filesize ( FILE *f )
-{
-	long  oldfp, fsize ;
+static int Filesize(FILE *f) {
+	long oldfp, fsize;
 
 	oldfp = ftell(f);
-	if ( oldfp < 0L ) 
-		return -1 ;
-	if ( 0 != fseek(f, 0, SEEK_END) ) 
-		return -1 ;
+	if (oldfp < 0L) {
+		return -1;
+	}
+	if (fseek(f, 0, SEEK_END) != 0) {
+		return -1;
+	}
+
 	fsize = ftell(f);
-	if ( fsize < 0 ) 
-		return -1 ;
-	if ( 0 != fseek(f, oldfp, SEEK_SET) ) 
-		return -1 ;
-	return fsize ;
+	if (fsize < 0) {
+		return -1;
+	}
+	if (fseek(f, oldfp, SEEK_SET) != 0) {
+		return -1;
+	}
+	return fsize;
 }
 
-static void Read_YUV_init (int xsize, int ysize, char * filename )
-{
-	if((ptfile = fopen(filename, "rb")) == NULL){
-		printf("Cannot open yuv_file concatenated input file '%s' for reading\n"
-			, filename);
-		exit( -1);
+static void Read_YUV_init(int xsize, int ysize, char * filename) {
+	ptfile = fopen(filename, "rb");
+	if (ptfile == NULL) {
+		printf("Cannot open yuv_file concatenated input file '%s' for reading\n", filename);
+		exit(-1);
 	}
 
-
-	if ( (xsize_int = xsize) == 0 ) 
-	{
+	if ((xsize_int = xsize) == 0) {
 		printf("xsize %d invalid\n", xsize);
-		exit( -2);
+		exit(-2);
 	}
-	if ( (ysize_int = ysize) == 0 ) 
-	{
+	if ((ysize_int = ysize) == 0) {
 		printf("ysize %d invalid\n", ysize);
-		exit( -3);
+		exit(-3);
 	}
 
 	NumberOfFrames = Filesize(ptfile) / (xsize * ysize + xsize * ysize / 2);
 }
 
-void Read_YUV ( unsigned char *Y, unsigned char *U, unsigned char *V )
-{
-
-	/***********************/
-	/*  Declare variables  */
-	/***********************/
-
-	/***********************/
-	/* Function's Body     */
-	/***********************/
-
+static void Read_YUV(unsigned char *Y, unsigned char *U, unsigned char *V) {
 	fread(Y, sizeof(unsigned char), xsize_int * ysize_int, ptfile);
 	fread(U, sizeof(unsigned char), xsize_int * ysize_int / 4, ptfile);
 	fread(V, sizeof(unsigned char), xsize_int * ysize_int / 4, ptfile);
-	images++ ;
+	images++;
 
-	if ( images == NumberOfFrames ) {
+	if (images == NumberOfFrames) {
 		fseek(ptfile, 0, SEEK_SET);
 		images = 0 ;
 	}
-
 }
 
-static void DiffUcharImage ( const int x_size, const int y_size, const unsigned char *img1_uchar, const unsigned char *img2_uchar )
-{
-	/*Mes variables locales*/
-	int   i,j ;
-	int   error = 0 ;
+static void DiffUcharImage(const int x_size, const int y_size, const unsigned char *img1_uchar, const unsigned char *img2_uchar) {
+	int i, j;
+	int error = 0;
 
-	for ( j = 0 ; j < y_size ; j++ ) {
+	for (j = 0; j < y_size; j++) {
+		for (i = 0; i < x_size; i++) {
+			if (abs(img1_uchar[j * x_size + i ] - img2_uchar[j * x_size + i]) != 0) {
+				error++;
 
-		for ( i = 0 ; i < x_size; i++ ) 
-		{
-			if ( abs(img1_uchar [j * x_size + i ] - img2_uchar [j * x_size + i])!=0  ) 
-			{
-				error++ ;
-
-				if( error < 100)
-					printf("error %d instead of %d at position : i= %d, j= %d, mb_x= %d, mb_y= %d \n", img1_uchar [j * x_size + i] , img2_uchar [j * x_size + i], i, j, i/16,j/16);
-
+				if (error < 100) {
+					printf("error %d instead of %d at position : i = %d, j = %d, mb_x = %d, mb_y = %d \n",
+						img1_uchar[j * x_size + i] , img2_uchar[j * x_size + i], i, j, i/16, j/16);
+				}
 			}
 		}
 	}
 
-	if ( error != 0 ) {
+	if (error != 0) {
 		printf("error %d !!!!!!!!!!!!!\n", error);
 		//system("pause");
 	}
 	//  else
 	//   printf("OK\n");
-
 }
 
 void Compare_write_mb(short tokens[384]) {
@@ -248,16 +230,13 @@ void Compare_write_mb(short tokens[384]) {
 	}
 }
 
-
-
-
 static void Compare_init(int width, int height) {
-
 	m_width = width;
 	m_height = height;
 	Read_YUV_init (width, height, yuv_file);
 }
-static int init=1;
+
+static int init = 1;
 
 void Compare_scheduler(struct schedinfo_s *si) {
 	int i = 0;
