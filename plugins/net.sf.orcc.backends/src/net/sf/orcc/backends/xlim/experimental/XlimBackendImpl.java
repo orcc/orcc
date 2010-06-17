a5982499bddc7d10b0e8d8d398afc726f686611a
Ghislain Roquier <ghislain.roquier@gmail.com>
2010-06-17 10:51:59 +0000

    added an experimental XLIM backend based on StringTemplate.

---
+++ plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/experimental/XlimBackendImpl.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2009, Ecole Polytechnique Fédérale de Lausanne
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the Ecole Polytechnique Fédérale de Lausanne nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc.backends.xlim.experimental;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.sf.orcc.OrccException;
+import net.sf.orcc.backends.AbstractBackend;
+import net.sf.orcc.backends.STPrinter;
+import net.sf.orcc.ir.Actor;
+import net.sf.orcc.ir.ActorTransformation;
+import net.sf.orcc.ir.transforms.DeadCodeElimination;
+import net.sf.orcc.ir.transforms.DeadGlobalElimination;
+import net.sf.orcc.ir.transforms.DeadVariableRemoval;
+import net.sf.orcc.ir.transforms.PhiRemoval;
+import net.sf.orcc.network.Instance;
+import net.sf.orcc.network.Network;
+
+/**
+ * XLIM back-end.
+ * 
+ * @author Ghislain Roquier
+ * 
+ */
+public class XlimBackendImpl extends AbstractBackend {
+
+	public static Boolean partitioning = false;
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		main(XlimBackendImpl.class, args);
+	}
+
+	private STPrinter printer;
+
+	@Override
+	protected void doXdfCodeGeneration(Network network) throws OrccException {
+		network.flatten();
+
+		printer = new STPrinter();
+		printer.loadGroups("XLIM_actor");
+		printer.setExpressionPrinter(XlimExprPrinter.class);
+		printer.setTypePrinter(XlimTypePrinter.class);
+		transformActors(network.getActors());
+		printInstances(network);
+	}
+
+	@Override
+	protected void printInstance(Instance instance) throws OrccException {
+		String id = instance.getId();
+		String outputName = path + File.separator + id + ".xlim";
+		try {
+			printer.printInstance(outputName, instance);
+		} catch (IOException e) {
+			throw new OrccException("I/O error", e);
+		}
+	}
+
+	@Override
+	protected void transformActor(Actor actor) throws OrccException {
+		ActorTransformation[] transformations = { new DeadGlobalElimination(),
+				new DeadCodeElimination(), new DeadVariableRemoval(),
+				new PhiRemoval() };
+
+		for (ActorTransformation transformation : transformations) {
+			transformation.transform(actor);
+		}
+	}
+
+}
---
+++ plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/experimental/XlimTypePrinter.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2009, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc.backends.xlim.experimental;
+
+import net.sf.orcc.ir.type.BoolType;
+import net.sf.orcc.ir.type.IntType;
+import net.sf.orcc.ir.type.ListType;
+import net.sf.orcc.ir.type.StringType;
+import net.sf.orcc.ir.type.TypePrinter;
+import net.sf.orcc.ir.type.UintType;
+import net.sf.orcc.ir.type.VoidType;
+
+/**
+ * This class defines a C type printer.
+ * 
+ * @author Matthieu Wipliez
+ * 
+ */
+public class XlimTypePrinter extends TypePrinter {
+
+	private void printInt(int size) {
+		builder.append("int");
+	}
+
+	@Override
+	public void visit(BoolType type) {
+		builder.append("bool");
+	}
+
+	@Override
+	public void visit(IntType type) {
+		printInt(type.getSize());
+	}
+
+	@Override
+	public void visit(ListType type) {
+		// size will be printed later
+		type.getElementType().accept(this);
+	}
+
+	@Override
+	public void visit(StringType type) {
+		builder.append("String");
+	}
+
+	@Override
+	public void visit(UintType type) {
+		builder.append("uint");
+	}
+
+	@Override
+	public void visit(VoidType type) {
+		builder.append("void");
+	}
+
+}
