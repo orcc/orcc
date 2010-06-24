1861eb1131ee7627e96134c4206a19b80a8e64dd
Ghislain Roquier <ghislain.roquier@gmail.com>
2010-06-24 13:07:00 +0000

    MoveLiteralIntegers manages Store and Assign.

--- plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/experimental/transform/MoveLiteralIntegers.java
+++ plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/experimental/transform/MoveLiteralIntegers.java
@@ -1,6 +1,5 @@
 package net.sf.orcc.backends.xlim.experimental.transform;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -12,27 +11,100 @@ import net.sf.orcc.ir.Location;
 import net.sf.orcc.ir.Procedure;
 import net.sf.orcc.ir.Use;
 import net.sf.orcc.ir.Variable;
+import net.sf.orcc.ir.expr.BinaryExpr;
+import net.sf.orcc.ir.expr.BoolExpr;
+import net.sf.orcc.ir.expr.ExpressionInterpreter;
+import net.sf.orcc.ir.expr.IntExpr;
+import net.sf.orcc.ir.expr.ListExpr;
+import net.sf.orcc.ir.expr.StringExpr;
+import net.sf.orcc.ir.expr.UnaryExpr;
 import net.sf.orcc.ir.expr.VarExpr;
 import net.sf.orcc.ir.instructions.Assign;
 import net.sf.orcc.ir.instructions.Store;
 import net.sf.orcc.ir.nodes.BlockNode;
 import net.sf.orcc.ir.transforms.AbstractActorTransformation;
+import net.sf.orcc.ir.type.BoolType;
 import net.sf.orcc.ir.type.IntType;
 import net.sf.orcc.util.OrderedMap;
 
 public class MoveLiteralIntegers extends AbstractActorTransformation {
 
+	private class MyExpressionInterpreter implements ExpressionInterpreter {
+
+		@Override
+		public Object interpret(BinaryExpr expr, Object... args) {
+			Expression e1 = (Expression) expr.getE1().accept(this, args);
+			Expression e2 = (Expression) expr.getE2().accept(this, args);
+			return new BinaryExpr(e1, expr.getOp(), e2, expr.getType());
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Object interpret(BoolExpr expr, Object... args) {
+			ListIterator<Instruction> it = (ListIterator<Instruction>) args[0];
+			String name = "lit_int_" + index++;
+			LocalVariable var = new LocalVariable(true, 0, new Location(),
+					name, null, new BoolType());
+			locals.add(var.getName(), var);
+
+			it.previous();
+			it.add(new Assign(var, expr));
+			it.next();
+			return new VarExpr(new Use(var));
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public Object interpret(IntExpr expr, Object... args) {
+			ListIterator<Instruction> it = (ListIterator<Instruction>) args[0];
+			String name = "lit_int_" + index++;
+			LocalVariable var = new LocalVariable(true, 0, new Location(),
+					name, null, new IntType(32));
+
+			locals.add(var.getName(), var);
+
+			it.previous();
+			it.add(new Assign(var, expr));
+			it.next();
+			return new VarExpr(new Use(var));
+		}
+
+		@Override
+		public Object interpret(ListExpr expr, Object... args) {
+			return expr;
+		}
+
+		@Override
+		public Object interpret(StringExpr expr, Object... args) {
+			return expr;
+		}
+
+		@Override
+		public Object interpret(UnaryExpr expr, Object... args) {
+			Expression e = (Expression) expr.getExpr().accept(this, args);
+			return new UnaryExpr(expr.getOp(), e, expr.getType());
+		}
+
+		@Override
+		public Object interpret(VarExpr expr, Object... args) {
+			return expr;
+		}
+
+	}
+
 	private static int index;
 
 	private OrderedMap<Variable> locals;
 
-	private List<Instruction> instructions;
+	@Override
+	public void visit(Assign assign, Object... args) {
+		assign.setValue((Expression) assign.getValue().accept(
+				new MyExpressionInterpreter(), args));
+	}
 
 	@Override
 	public void visit(BlockNode node, Object... args) {
-		instructions = node.getInstructions();
-		ListIterator<Instruction> it = new ArrayList<Instruction>(instructions)
-				.listIterator();
+		ListIterator<Instruction> it = node.getInstructions().listIterator();
 		while (it.hasNext()) {
 			it.next().accept(this, it);
 		}
@@ -40,21 +112,13 @@ public class MoveLiteralIntegers extends AbstractActorTransformation {
 
 	@Override
 	public void visit(Store store, Object... args) {
+
 		ListIterator<Expression> it = store.getIndexes().listIterator();
 		while (it.hasNext()) {
-			Expression expr = it.next();
-			if (expr.isIntExpr()) {
-				String name = "lit_int_" + index++;
-				LocalVariable var = new LocalVariable(true, 0, new Location(),
-						name, null, new IntType(32));
-				locals.add(var.getName(), var);
-
-				int index = instructions.indexOf(store);
-				instructions.add(index, new Assign(var, expr));
-
-				it.set(new VarExpr(new Use(var)));
-			}
+			it.set((Expression) it.next().accept(new MyExpressionInterpreter(),
+					args));
 		}
+
 	}
 
 	@Override
