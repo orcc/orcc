2e29bb836b8a7c3dd3b45a94229c1651354a5a23
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-07-15 14:54:09 +0000

    changes in name provider: copied DefaultDeclarative... implementation, with the following changes: 1) the name of a variable no longer contains the name of its action 2) block count and untagged count must be reset explicitly.

--- plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/naming/CalQualifiedNameProvider.java
+++ plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/naming/CalQualifiedNameProvider.java
@@ -28,6 +28,7 @@
  */
 package net.sf.orcc.cal.naming;
 
+import java.util.Collections;
 import java.util.Iterator;
 
 import net.sf.orcc.cal.cal.AstAction;
@@ -35,8 +36,17 @@ import net.sf.orcc.cal.cal.AstActor;
 import net.sf.orcc.cal.cal.AstGenerator;
 import net.sf.orcc.cal.cal.AstStatementForeach;
 import net.sf.orcc.cal.cal.AstTag;
+import net.sf.orcc.cal.cal.CalPackage;
 
-import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.util.IResourceScopeCache;
+import org.eclipse.xtext.util.PolymorphicDispatcher;
+import org.eclipse.xtext.util.SimpleAttributeResolver;
+import org.eclipse.xtext.util.Tuples;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 /**
  * This class defines a qualified name provider for RVC-CAL.
@@ -45,11 +55,68 @@ import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
  * 
  */
 public class CalQualifiedNameProvider extends
-		DefaultDeclarativeQualifiedNameProvider {
+		IQualifiedNameProvider.AbstractImpl {
 
 	private int blockCount;
 
+	@Inject
+	private IResourceScopeCache cache = IResourceScopeCache.NullImpl.INSTANCE;
+
+	private PolymorphicDispatcher<String> qualifiedName = new PolymorphicDispatcher<String>(
+			"qualifiedName", 1, 1, Collections.singletonList(this),
+			PolymorphicDispatcher.NullErrorHandler.<String> get()) {
+		@Override
+		protected String handleNoSuchMethod(Object... params) {
+			return null;
+		}
+	};
+
+	private SimpleAttributeResolver<EObject, String> resolver = SimpleAttributeResolver
+			.newResolver(String.class, "name");
+
 	private int untaggedCount;
+
+	public String getDelimiter() {
+		return ".";
+	}
+
+	public String getQualifiedName(final EObject obj) {
+		return cache.get(Tuples.pair(obj, "fqn"), obj.eResource(),
+				new Provider<String>() {
+
+					public String get() {
+						EObject temp = obj;
+						String name = qualifiedName.invoke(temp);
+						if (name != null)
+							return name;
+						String value = resolver.getValue(temp);
+						if (value == null)
+							return null;
+						while (temp.eContainer() != null) {
+							temp = temp.eContainer();
+							if (CalPackage.eINSTANCE.getAstAction().isInstance(
+									temp)) {
+								return value;
+							}
+
+							String parentsName = getQualifiedName(temp);
+							if (parentsName != null)
+								return parentsName + getDelimiter() + value;
+						}
+						return value;
+					}
+
+				});
+
+	}
+
+	protected SimpleAttributeResolver<EObject, String> getResolver() {
+		return resolver;
+	}
+
+	public String getWildcard() {
+		return "*";
+	}
 
 	/**
 	 * Resets the block count and returns the qualified name of the tag of the
@@ -60,8 +127,6 @@ public class CalQualifiedNameProvider extends
 	 * @return the action's tag qualified name
 	 */
 	public String qualifiedName(AstAction action) {
-		blockCount = 0;
-
 		AstTag tag = action.getTag();
 		if (tag == null) {
 			return "untagged_" + untaggedCount++;
@@ -78,8 +143,6 @@ public class CalQualifiedNameProvider extends
 	 * @return the actor name
 	 */
 	public String qualifiedName(AstActor actor) {
-		untaggedCount = 0;
-
 		return actor.getName();
 	}
 
@@ -103,6 +166,20 @@ public class CalQualifiedNameProvider extends
 		}
 
 		return builder.toString();
+	}
+
+	/**
+	 * Resets the block count.
+	 */
+	public void resetBlockCount() {
+		blockCount = 0;
+	}
+
+	/**
+	 * Resets the untagged action count.
+	 */
+	public void resetUntaggedCount() {
+		untaggedCount = 0;
 	}
 
 }
