415fffe841605ce399a2235999c9cdc59bca7b34
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-07-16 18:33:28 +0000

    removed frontend folder from core plug-in.

--- plugins/net.sf.orcc.core/frontend/macosx.x86_64/copy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp ../../../../frontend/_build/src/cli/cal2ir.d.byte .
-cp ../../../../frontend/_build/src/cli/cal2ir.native .
--- plugins/net.sf.orcc.core/frontend/win32.x86/copy.bat
+++
@@ -1,2 +0,0 @@
-copy ..\..\..\..\frontend\_build\src\cli\cal2ir.d.byte .
-copy ..\..\..\..\frontend\_build\src\cli\cal2ir.native .
