f4398f0611bcde10c256327d5bacccdd82050e52
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-07-18 18:14:35 +0000

    Updated actors to use ".." instead of Integers, and removed imports. Hopefully this does not break anything :)

--- rsync_frontend_mwipliez.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Note: The --chmod is necessary to ensure the files will be readable and
-# executable (for directories) on the web site.
-
-rsync -avP --chmod=+rx -e ssh frontend/frontend-win32.zip mwipliez,orcc@web.sourceforge.net:htdocs/
