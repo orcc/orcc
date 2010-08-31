29f759459f3078cc606bf3bf66878227d377bcff
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-08-31 20:59:39 +0000

    Revert display.c to r2178 because gorinje's "fix" (for what?) broke the display.

--- projects/CMake/lib/src/display.c
+++ projects/CMake/lib/src/display.c
@@ -26,52 +26,265 @@
 * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 * SUCH DAMAGE.
 */
+#include "SDL.h"
 
 #include "orcc.h"
 #include "orcc_fifo.h"
 
-extern struct fifo_i8_s *B;
-extern struct fifo_i16_s *WIDTH;
-extern struct fifo_i16_s *HEIGHT;
+#ifdef BENCHMARK
+#include <locale.h>
+#include <time.h>
+FILE * pFile;
+static Uint32 tInit = 0;
+#endif
 
+
+extern struct fifo_i8_s *display_B;
+extern struct fifo_i16_s *display_WIDTH;
+extern struct fifo_i16_s *display_HEIGHT;
+
+static SDL_Surface *m_screen;
+static SDL_Overlay *m_overlay;
+
+#define MAX_WIDTH 720
+#define MAX_HEIGHT 576
+
+
+static int m_x;
+static int m_y;
+static int m_width;
+static int m_height;
+
+static unsigned char img_buf_y[MAX_WIDTH * MAX_HEIGHT];
+static unsigned char img_buf_u[MAX_WIDTH * MAX_HEIGHT / 4];
+static unsigned char img_buf_v[MAX_WIDTH * MAX_HEIGHT / 4];
+
+static void press_a_key(int code) {
+	char buf[2];
+	printf("Press enter to continue\n");
+	fgets(buf, 2, stdin);
+	exit(code);
+}
+
+static Uint32 start_time;
+static int num_images_start;
+static int num_images_end;
+
+void print_fps_avg(void) {
+	Uint32 t = SDL_GetTicks();
+
+	printf("%i images in %f seconds: %f FPS\n",
+		num_images_end, (float) t / 1000.0f,
+		1000.0f * (float)num_images_end / (float)t);
+}
+
+static Uint32 t;
+
+void display_show_image(void) {
+	SDL_Rect rect = { 0, 0, m_width, m_height };
+
+	int t2;
+	SDL_Event event;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef NO_DISPLAY
+	if (SDL_LockYUVOverlay(m_overlay) < 0) {
+		fprintf(stderr, "Can't lock screen: %s\n", SDL_GetError());
+		press_a_key(-1);
+	}
+#endif
+
+	memcpy(m_overlay->pixels[0], img_buf_y, m_width * m_height );
+	memcpy(m_overlay->pixels[1], img_buf_u, m_width * m_height / 4 );
+	memcpy(m_overlay->pixels[2], img_buf_v, m_width * m_height / 4 );
+
+#ifndef NO_DISPLAY
+	SDL_UnlockYUVOverlay(m_overlay);
+	SDL_DisplayYUVOverlay(m_overlay, &rect);
+#endif
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	num_images_end++;
+	t2 = SDL_GetTicks();
+	if (t2 - t > 3000) {
+		printf("%f images/sec\n",
+			1000.0f * (float)(num_images_end - num_images_start) / (float)(t2 - t));
+
+#ifdef BENCHMARK
+		if (tInit == 0)
+			tInit = t2;
+
+		fprintf (pFile, "%d \t %f \n",(t2-tInit),1000.0f * (float)(num_images_end - num_images_start) / (float)(t2 - t));
+
+#endif
+		t = t2;
+		num_images_start = num_images_end;
+	}
+
+	/* Grab all the events off the queue. */
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT:
+				exit(0);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+void display_write_mb(unsigned char tokens[384]) {
+	int i, j, cnt, base;
+
+	//printf("display_write_mb (%i, %i)\n", m_x, m_y);
+
+	cnt = 0;
+	base = m_y * m_width + m_x;
+
+	for (i = 0; i < 16; i++) {
+		for (j = 0; j < 16; j++) {
+			int tok = tokens[cnt];
+			int idx = base + i * m_width + j;
+			cnt++;
+			img_buf_y[idx] = tok;
+		}
+	}
+
+	base = m_y / 2 * m_width / 2 + m_x / 2;
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			int tok = tokens[cnt];
+			int idx = base + i * m_width / 2 + j;
+			cnt++;
+			img_buf_u[idx] = tok;
+		}
+	}
+
+	for (i = 0; i < 8; i++) {
+		for (j = 0; j < 8; j++) {
+			int tok = tokens[cnt];
+			int idx = base + i * m_width / 2 + j;
+			cnt++;
+			img_buf_v[idx] = tok;
+		}
+	}
+
+	m_x += 16;
+	if (m_x == m_width) {
+		m_x = 0;
+		m_y += 16;
+	}
+
+	if (m_y == m_height) {
+		m_x = 0;
+		m_y = 0;
+		display_show_image();
+	}
+}
 
 static int init = 0;
-static void set_video(int width, int height);
-static void set_init();
-void write_mb(unsigned char tokens[384]);
 
+static void display_init() {
+	// First, initialize SDL's video subsystem.
+	if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+		fprintf(stderr, "Video initialization failed: %s\n", SDL_GetError());
+		press_a_key(-1);
+	}
 
-void scheduler() {
+	SDL_WM_SetCaption("display", NULL);
+
+	start_time = SDL_GetTicks();
+	t = start_time;
+
+#ifdef BENCHMARK
+	{
+		char sFile [80];	
+		time_t rawtime;
+		struct tm * timeinfo;
+
+		time ( &rawtime );
+		timeinfo = localtime ( &rawtime );
+
+		strftime (sFile,80,".//Bench/bench_%d-%m-%y_%H-%M-%S.log",timeinfo);
+
+		pFile = fopen (sFile,"w");
+		if (pFile== NULL)
+		{
+			fprintf(stderr, "Can't create log file, try to disable BENCHMARK preprocessor\n");
+			exit(1);
+		}
+		setlocale(LC_NUMERIC, "");
+		fprintf (pFile, "Time (in ms) \t fps \n");
+	}
+#endif
+
+	atexit(SDL_Quit);
+	atexit(print_fps_avg);
+
+	init = 1;
+}
+
+static void display_set_video(int width, int height) {
+	if (width == m_width && height == m_height) {
+		// video mode is already good
+		return;
+	}
+
+	m_width = width;
+	m_height = height;
+	printf("set display to %ix%i\n", width, height);
+
+	m_screen = SDL_SetVideoMode(m_width, m_height, 24, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	if (m_screen == NULL) {
+		fprintf(stderr, "Couldn't set %ix%ix24 video mode: %s\n", m_width, m_height,
+			SDL_GetError());
+		press_a_key(-1);
+	}
+
+	if (m_overlay != NULL) {
+		SDL_FreeYUVOverlay(m_overlay);
+	}
+
+	m_overlay = SDL_CreateYUVOverlay(m_width, m_height, SDL_IYUV_OVERLAY, m_screen);
+	if (m_overlay == NULL) {
+		fprintf(stderr, "Couldn't create overlay: %s\n", SDL_GetError());
+		press_a_key(-1);
+	}
+}
+
+void display_scheduler(struct schedinfo_s *si) {
 	int i = 0;
 
 	while (1) {
-		if (fifo_i16_has_tokens(WIDTH, 1) && fifo_i16_has_tokens(HEIGHT, 1)) {
+		if (fifo_i16_has_tokens(display_WIDTH, 1) && fifo_i16_has_tokens(display_HEIGHT, 1)) {
 			short *ptr, width, height;
 
-			ptr = fifo_i16_read(WIDTH, 1);
+			ptr = fifo_i16_read(display_WIDTH, 1);
 			width = ptr[0] * 16;
-			fifo_i16_read_end(WIDTH, 1);
+			fifo_i16_read_end(display_WIDTH, 1);
 
-			ptr = fifo_i16_read(HEIGHT, 1);
+			ptr = fifo_i16_read(display_HEIGHT, 1);
 			height = ptr[0] * 16;
-			fifo_i16_read_end(HEIGHT, 1);
+			fifo_i16_read_end(display_HEIGHT, 1);
 
-			set_video(width, height);
+			display_set_video(width, height);
 			i++;
 		}
 
-		if (fifo_i8_has_tokens(B, 384)) {
+		if (fifo_i8_has_tokens(display_B, 384)) {
 			if (!init) {
-				set_init();
-				init = 1;
+				display_init();
 			}
 
-			write_mb(fifo_i8_read(B, 384));
-			fifo_i8_read_end(B, 384);
+			display_write_mb(fifo_i8_read(display_B, 384));
+			fifo_i8_read_end(display_B, 384);
 			i++;
 		} else {
 			break;
 		}
 	}
 
+	si->num_firings = i;
+	si->reason = starved;
+	si->ports = 0x07; // FIFOs connected to first three input ports are empty
 }
