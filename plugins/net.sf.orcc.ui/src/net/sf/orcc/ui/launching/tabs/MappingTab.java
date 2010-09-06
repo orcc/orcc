73c5c03e6fdd459230ddf691ac6f0f6b4f9e9f65
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-09-06 12:46:05 +0000

    prints error with browser if there is an exception or it cannot render the HTML. (also, use "trusted" mode).

--- plugins/net.sf.orcc.ui/src/net/sf/orcc/ui/launching/tabs/MappingTab.java
+++ plugins/net.sf.orcc.ui/src/net/sf/orcc/ui/launching/tabs/MappingTab.java
@@ -57,6 +57,7 @@ import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.Image;
@@ -158,8 +159,7 @@ public class MappingTab extends AbstractLaunchConfigurationTab {
 		protected Object getValue(Object element) {
 			if (element instanceof Instance) {
 				Instance instance = (Instance) element;
-				String component = mapping.get(instance
-						.getHierarchicalPath());
+				String component = mapping.get(instance.getHierarchicalPath());
 				if (component != null) {
 					return component;
 				}
@@ -276,43 +276,46 @@ public class MappingTab extends AbstractLaunchConfigurationTab {
 	}
 
 	private void createLabel(Composite composite) {
-		Browser browser = new Browser(composite, SWT.NONE);
-		GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-		browser.setLayoutData(data);
+		String html = "<html>\n"
+				+ "<head>\n"
+				+ "<title>Mapping</title>\n"
+				+ "<style type=\"text/css\">\n"
+				+ "body {\n"
+				+ "font-family: Verdana;\n"
+				+ "font-size: small;\n"
+				+ "}\n"
+				+ "</style>\n"
+				+ "</head>\n\n"
 
-		browser.setFont(composite.getFont());
-		browser.setBackground(null);
-
-		String html = "<html>"
-				+ "<head>"
-				+ "<title/>"
-				+ "<style type=\"text/css\">"
-				+ "body {"
-				+ "font-family: Verdana;"
-				+ "font-size: small;"
-				+ "}"
-				+ "</style>"
-				+ "</head>"
-
-				+ "<body>"
+				+ "<body>\n\n"
 
 				+ "<p>Warning: the mapping feature is experimental and likely to evolve. "
 				+ "Furthermore, the name of the components is implementation-specific, "
 				+ "and is likely to change in the future."
-				+ "</p>"
+				+ "</p>\n\n"
 
-				+ "<p>In the meantime, you can use:</p>"
-				+ "<ul>"
+				+ "<p>In the meantime, you can use:</p>\n"
+				+ "<ul>\n"
 				+ "<li>with the C back-end: c&lt;x&gt; to indicate core x with x an integer &gt;= 0, like c0, c1, and so on. "
 				+ "If there is only one core set, usual single-threaded code will be generated. "
-				+ "At this time the mapping is not used by the C back-end.</li>"
-				+ "</ul>"
-				+ "</li>"
+				+ "At this time the mapping is not used by the C back-end.</li>\n"
+				+ "</ul>\n\n"
 
-				+ "<p>Setting the component on a network sets the component of all its sub-networks.</p>"
+				+ "<p>Setting the component on a network sets the component of all its sub-networks.</p>\n"
 
-				+ "</body>" + "</html>";
-		browser.setText(html);
+				+ "</body>\n" + "</html>\n";
+
+		try {
+			Browser browser = new Browser(composite, SWT.NONE);
+			GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
+			browser.setLayoutData(data);
+
+			if (!browser.setText(html, true)) {
+				System.err.println("browser could not render HTML");
+			}
+		} catch (SWTError e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
