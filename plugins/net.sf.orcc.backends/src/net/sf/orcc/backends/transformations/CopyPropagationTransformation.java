3b7365164ce943724211493db7d6110ee5859a1a
Jérôme Gorin <gorinje@gmail.com>
2010-09-09 14:12:30 +0000

    Add a new transformation in orcc for Copy Propagation

---
+++ plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/CopyPropagationTransformation.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2009-2010, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc.backends.transformations;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.orcc.ir.Expression;
+import net.sf.orcc.ir.Instruction;
+import net.sf.orcc.ir.LocalVariable;
+import net.sf.orcc.ir.Procedure;
+import net.sf.orcc.ir.Use;
+import net.sf.orcc.ir.Variable;
+import net.sf.orcc.ir.expr.AbstractExpressionInterpreter;
+import net.sf.orcc.ir.expr.BinaryExpr;
+import net.sf.orcc.ir.expr.UnaryExpr;
+import net.sf.orcc.ir.expr.VarExpr;
+import net.sf.orcc.ir.instructions.Assign;
+import net.sf.orcc.ir.instructions.PhiAssignment;
+import net.sf.orcc.ir.instructions.Return;
+import net.sf.orcc.ir.nodes.BlockNode;
+import net.sf.orcc.ir.transforms.AbstractActorTransformation;
+
+/**
+ * Split expression and effective node.
+ * 
+ * @author Jérôme GORIN
+ * 
+ */
+public class CopyPropagationTransformation extends AbstractActorTransformation {
+	
+	private class ExpressionCopy extends AbstractExpressionInterpreter {
+		private Map<Variable ,Expression> copyVars;
+		
+		public ExpressionCopy(Map<Variable ,Expression> copyVars) {
+			this.copyVars = copyVars;
+		}
+
+		@Override
+		public Object interpret(VarExpr expr, Object... args) {
+			Variable var = expr.getVar().getVariable();
+			if (copyVars.containsKey(var)){
+				return copyVars.get(var);	
+			}
+			
+			return expr;
+		}
+	}
+	
+	private Map<Variable ,Expression> copyVars;
+	private ExpressionCopy expressionCopy;
+	private List<Instruction> removedInstrs;
+	
+	public CopyPropagationTransformation(){
+		copyVars = new HashMap<Variable , Expression>();
+		expressionCopy = new ExpressionCopy(copyVars);
+		removedInstrs = new ArrayList<Instruction>();
+		
+	}
+	
+	@Override
+	public void visit(Assign assign, Object... args) {
+		Expression value = assign.getValue();
+		
+		if ((!(value instanceof BinaryExpr))&&(!(value instanceof UnaryExpr))){
+				LocalVariable target = assign.getTarget();
+				Expression expr = assign.getValue();
+				
+				copyVars.put(target, expr);
+				removedInstrs.add(assign);
+		}
+	}
+	
+	@Override
+	public void visit(PhiAssignment phi, Object... args) {
+		
+		List<Use> uses = phi.getVars();
+		
+		for (Use use : uses){
+			if (copyVars.containsKey(use)){
+				uses.set(uses.indexOf(use), copyVars.get(use));
+			}
+			
+		}
+	}
+	
+	
+	@Override
+	public void visit(Return returnInstr, Object... args) {
+		Expression expr = returnInstr.getValue();
+		if (expr != null){
+			Expression newExpr = (Expression)expr.accept(expressionCopy);
+			returnInstr.setValue(newExpr);
+		}
+	}
+	
+	@Override
+	public void visitProcedure(Procedure procedure) {
+		copyVars.clear();
+		super.visitProcedure(procedure);
+		BlockNode.getFirst(procedure).getInstructions().removeAll(removedInstrs);
+	}
+
+
+}
