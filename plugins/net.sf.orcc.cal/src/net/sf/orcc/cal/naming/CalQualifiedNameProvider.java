008a3aefc53b61f70bfa531d8a25ed73acee305e
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-09-15 19:06:11 +0000

    merged name provider with r1919 since detection of duplicate names inside actors (i.e. everything except ports) is not handled by NamesAreUniqueValidator since I-don't-remember-when. So the qualified name of a variable inside an action/function/procedure/input pattern is just the variable name. This makes generated code look prettier (and this time it does not mess up duplicate name detection, contrary to r1919!)

--- plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/naming/CalQualifiedNameProvider.java
+++ plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/naming/CalQualifiedNameProvider.java
@@ -28,14 +28,24 @@
  */
 package net.sf.orcc.cal.naming;
 
+import java.util.Collections;
 import java.util.Iterator;
 
 import net.sf.orcc.cal.cal.AstAction;
 import net.sf.orcc.cal.cal.AstGenerator;
 import net.sf.orcc.cal.cal.AstStatementForeach;
 import net.sf.orcc.cal.cal.AstTag;
+import net.sf.orcc.cal.cal.CalPackage;
 
-import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.naming.IQualifiedNameProvider;
+import org.eclipse.xtext.util.IResourceScopeCache;
+import org.eclipse.xtext.util.PolymorphicDispatcher;
+import org.eclipse.xtext.util.SimpleAttributeResolver;
+import org.eclipse.xtext.util.Tuples;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
 
 /**
  * This class defines a qualified name provider for RVC-CAL.
@@ -44,11 +54,74 @@ import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
  * 
  */
 public class CalQualifiedNameProvider extends
-		DefaultDeclarativeQualifiedNameProvider {
+		IQualifiedNameProvider.AbstractImpl {
 
 	private int blockCount;
 
+	@Inject
+	private IResourceScopeCache cache = IResourceScopeCache.NullImpl.INSTANCE;
+
+	private PolymorphicDispatcher<String> qualifiedName = new PolymorphicDispatcher<String>(
+			"qualifiedName", 1, 1, Collections.singletonList(this),
+			PolymorphicDispatcher.NullErrorHandler.<String> get()) {
+		@Override
+		protected String handleNoSuchMethod(Object... params) {
+			return null;
+		}
+	};
+
+	private SimpleAttributeResolver<EObject, String> resolver = SimpleAttributeResolver
+			.newResolver(String.class, "name");
+
 	private int untaggedCount;
+
+	public String getDelimiter() {
+		return ".";
+	}
+
+	public String getQualifiedName(final EObject obj) {
+		return cache.get(Tuples.pair(obj, "fqn"), obj.eResource(),
+				new Provider<String>() {
+
+					public String get() {
+						EObject temp = obj;
+						String name = qualifiedName.invoke(temp);
+						if (name != null)
+							return name;
+						String value = resolver.getValue(temp);
+						if (value == null)
+							return null;
+						while (temp.eContainer() != null) {
+							temp = temp.eContainer();
+							if (temp != null) {
+								switch (temp.eClass().getClassifierID()) {
+								case CalPackage.AST_ACTION:
+								case CalPackage.AST_FUNCTION:
+								case CalPackage.AST_INPUT_PATTERN:
+								case CalPackage.AST_PROCEDURE:
+									return value;
+								}
+							}
+
+							String parentsName = getQualifiedName(temp);
+							if (parentsName != null) {
+								return parentsName + getDelimiter() + value;
+							}
+						}
+						return value;
+					}
+
+				});
+
+	}
+
+	protected SimpleAttributeResolver<EObject, String> getResolver() {
+		return resolver;
+	}
+
+	public String getWildcard() {
+		return "*";
+	}
 
 	/**
 	 * Resets the block count and returns the qualified name of the tag of the
