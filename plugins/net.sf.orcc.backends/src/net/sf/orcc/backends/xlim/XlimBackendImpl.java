bbbb69f989bb24a934d5af1ef6b8ce0bc76bbdb4
Hervé Yviquel <hyviquel@gmail.com>
2010-11-12 18:34:34 +0000

    XLIM backend : Add NumberOfReaders information in generated C network

--- plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/XlimBackendImpl.java
+++ plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/XlimBackendImpl.java
@@ -30,13 +30,14 @@ package net.sf.orcc.backends.xlim;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import net.sf.orcc.OrccException;
 import net.sf.orcc.backends.AbstractBackend;
 import net.sf.orcc.backends.STPrinter;
 import net.sf.orcc.backends.transformations.InlineTransformation;
-import net.sf.orcc.backends.transformations.ListFlattenTransformation;
 import net.sf.orcc.backends.transformations.VariableRenamer;
 import net.sf.orcc.backends.transformations.threeAddressCodeTransformation.CastAdderTransformation;
 import net.sf.orcc.backends.transformations.threeAddressCodeTransformation.ExpressionSplitterTransformation;
@@ -46,11 +47,13 @@ import net.sf.orcc.backends.xlim.transforms.FirstPhiValuesOfWhileNodeTransformat
 import net.sf.orcc.backends.xlim.transforms.MoveLiteralIntegers;
 import net.sf.orcc.ir.Actor;
 import net.sf.orcc.ir.ActorTransformation;
+import net.sf.orcc.ir.Port;
 import net.sf.orcc.ir.transformations.BlockCombine;
 import net.sf.orcc.ir.transformations.BuildCFG;
 import net.sf.orcc.ir.transformations.DeadCodeElimination;
 import net.sf.orcc.ir.transformations.DeadGlobalElimination;
 import net.sf.orcc.ir.transformations.DeadVariableRemoval;
+import net.sf.orcc.network.Connection;
 import net.sf.orcc.network.Instance;
 import net.sf.orcc.network.Network;
 import net.sf.orcc.network.serialize.XDFWriter;
@@ -88,7 +91,10 @@ public class XlimBackendImpl extends AbstractBackend {
 				new ExpressionSplitterTransformation(), new BuildCFG(),
 				new CastAdderTransformation(),
 				new FirstPhiValuesOfWhileNodeTransformation(),
-				new MoveLiteralIntegers(), new ListFlattenTransformation(true),
+				new MoveLiteralIntegers(), /*
+											 * new
+											 * ListFlattenTransformation(true),
+											 */
 				new VariableRenamer(),
 				new ChangeActionSchedulerFormTransformation() };
 
@@ -154,11 +160,39 @@ public class XlimBackendImpl extends AbstractBackend {
 				outputName += ".c";
 				printer.loadGroups("XLIM_C_network");
 			}
+
+			Map<String, Object> optionsMap = new HashMap<String, Object>();
+			optionsMap.put("numberOfReaders", computeNumberOfReaders(network));
+			printer.setOptions(optionsMap);
+
 			printer.printNetwork(outputName, network, false, fifoSize);
 
 		} catch (IOException e) {
 			throw new OrccException("I/O error", e);
 		}
+	}
+
+	private Map<Instance, Map<Port, Integer>> computeNumberOfReaders(
+			Network network) {
+		network.computeTemplateMaps();
+
+		Map<Instance, Map<Port, Integer>> computedMap = new HashMap<Instance, Map<Port, Integer>>();
+		for (Instance instance : network.getOutgoingMap().keySet()) {
+			Map<Port, Integer> portToNumberOfReadersMap = new HashMap<Port, Integer>();
+			for (Connection connection : network.getOutgoingMap().get(instance)) {
+				Port srcPort = connection.getSource();
+				if (portToNumberOfReadersMap.get(srcPort) == null) {
+					portToNumberOfReadersMap.put(srcPort, 1);
+				} else {
+					int n = portToNumberOfReadersMap.get(srcPort);
+					n++;
+					portToNumberOfReadersMap.remove(srcPort);
+					portToNumberOfReadersMap.put(srcPort, n);
+				}
+			}
+			computedMap.put(instance, portToNumberOfReadersMap);
+		}
+		return computedMap;
 	}
 
 }
