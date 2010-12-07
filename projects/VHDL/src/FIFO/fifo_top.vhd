d7931695206dde970770e6d3a9d383c2187078c3
Nicolas Siret <nicolas.siret@live.fr>
2010-12-07 09:40:36 +0000

    Few corrections on the vhdl backend

--- projects/VHDL/src/FIFO/fifo_top.vhd
+++ projects/VHDL/src/FIFO/fifo_top.vhd
@@ -6,7 +6,7 @@
 -- Author     : Nicolas Siret (nicolas.siret@ltdsa.com)
 -- Company    : Lead Tech Design
 -- Created    : 
--- Last update: 2010-07-05
+-- Last update: 2010-11-29
 -- Platform   : 
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -57,28 +57,36 @@ entity fifo_top is
   port
     (
       reset_n  : in  std_logic;
-      wr_clk   : in  std_logic;
-      rd_clk   : in  std_logic;	  
       --
-      data_in  : in  std_logic_vector (width -1 downto 0);
+      wr_clk   : in  std_logic;
       wr_data  : in  std_logic;
+      data_in  : in  std_logic_vector (width -1 downto 0);
       wr_rdy   : out std_logic;
       wr_ack   : out std_logic;
       --
-      data_out : out std_logic_vector (width -1 downto 0);
+      rd_clk   : in  std_logic;
       send     : out std_logic;
+      data_out : out std_logic_vector (width -1 downto 0);
       rd_rdy   : in  std_logic;
       rd_ack   : in  std_logic);
 end fifo_top;
+
 
 -------------------------------------------------------------------------------
 
 
 architecture arch_fifo_top of fifo_top is
 
+  signal iFull   : std_logic;
+  signal iEmpty  : std_logic;
+  signal rd_data : std_logic;
+  
 begin
 
   create_FIFO : if depth > 1 generate
+    wr_rdy  <= not iFull;
+    rd_data <= (not iEmpty) and rd_rdy;
+    send    <= (not iEmpty) and rd_rdy;
     FIFO_generic_1 : entity work.FIFO_generic
       generic map (
         depth => depth,
@@ -86,19 +94,20 @@ begin
       port map (
         reset_n  => reset_n,
         wr_clk   => wr_clk,
-        rd_clk   => rd_clk,
-        data_in  => data_in,
         wr_data  => wr_data,
+        data_in  => data_in,
+        full     => iFull,
+        rd_clk   => rd_clk,
+        rd_data  => rd_data,
         data_out => data_out,
-        send     => send,
-        rd_ack   => rd_ack);
+        empty    => iEmpty);
   end generate;
 
   create_link : if depth = 1 generate
-        data_out <= data_in;
-        send     <= wr_data;
-        wr_rdy   <= rd_rdy;
-        wr_ack   <= rd_ack;
+  send     <= wr_data;
+  data_out <= data_in;
+  wr_rdy   <= rd_rdy;
+  wr_ack <= rd_ack;
   end generate;
 
 
