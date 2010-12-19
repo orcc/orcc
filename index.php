63fc03ec186c2a31814385c7da0631715ada1d00
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2010-12-19 22:01:35 +0000

    fixed URL in index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
   header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-  header("Location: http://sourceforge.net/drupal/");
+  header("Location: http://orcc.sourceforge.net/drupal/");
 
   exit;
 ?>
