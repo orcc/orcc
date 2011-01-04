6055860d8c5dcf1ca310559554c1d6eb03d3664c
Hervé Yviquel <hyviquel@gmail.com>
2011-01-04 15:59:46 +0000

    oops forget some new files

---
+++ projects/CMake/lib/src/thread.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2010, IETR/INSA
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+ #include "orcc.h"
+ #include "orcc_thread.h"
+ 
+ #include <semaphore.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+void sync_init(struct sync_s *sync, int threadsNb){
+	sync->threadsNb = 2;
+	sem_init(&sync->sem_monitor, 0, 0);
+	sem_init(&sync->sem_threads, 0, 0);
+}
+
+void *monitor(void *data) {
+	struct sync_s *sched_sync = data;
+	
+	while(1){
+		// wait threads synchro
+		int i;
+		for(i=0; i<sched_sync->threadsNb; i++){
+			sem_wait(&sched_sync->sem_monitor);
+		}
+		
+		// work process
+		printf("Monitor work !!\n");
+		
+		// wakeup all threads
+		for(i=0; i<sched_sync->threadsNb; i++){
+			sem_post(&sched_sync->sem_threads);
+		}
+	}
+}
