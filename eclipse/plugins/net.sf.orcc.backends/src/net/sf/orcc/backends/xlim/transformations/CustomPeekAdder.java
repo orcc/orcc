e74d08bcd0589139adbf290580d98b879613f3ad
Hervé Yviquel <hyviquel@gmail.com>
2011-02-14 15:51:01 +0000

    XLIM : Fix bug in peek with complex guard

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/transformations/CustomPeekAdder.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/xlim/transformations/CustomPeekAdder.java
@@ -28,11 +28,24 @@
  */
 package net.sf.orcc.backends.xlim.transformations;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 import net.sf.orcc.backends.xlim.instructions.CustomPeek;
+import net.sf.orcc.ir.Action;
+import net.sf.orcc.ir.Actor;
+import net.sf.orcc.ir.CFGNode;
 import net.sf.orcc.ir.Instruction;
+import net.sf.orcc.ir.Procedure;
+import net.sf.orcc.ir.Use;
+import net.sf.orcc.ir.Variable;
 import net.sf.orcc.ir.expr.IntExpr;
 import net.sf.orcc.ir.instructions.Load;
 import net.sf.orcc.ir.instructions.Peek;
+import net.sf.orcc.ir.instructions.Store;
+import net.sf.orcc.ir.nodes.BlockNode;
+import net.sf.orcc.ir.nodes.WhileNode;
 import net.sf.orcc.ir.transformations.AbstractActorTransformation;
 
 /**
@@ -45,29 +58,113 @@ import net.sf.orcc.ir.transformations.AbstractActorTransformation;
  */
 public class CustomPeekAdder extends AbstractActorTransformation {
 
+	private class NeededPeekFinder extends AbstractActorTransformation {
+
+		private List<CustomPeek> neededPeeks;
+		private Peek originalPeek;
+
+		public List<CustomPeek> find(Peek originalPeek) {
+			neededPeeks = new ArrayList<CustomPeek>();
+			this.originalPeek = originalPeek;
+			visit(originalPeek.getBlock().getProcedure());
+			return neededPeeks;
+		}
+
+		@Override
+		public void visit(Load load) {
+			if (load.getSource().getVariable() == originalPeek.getTarget()) {
+				int index = ((IntExpr) load.getIndexes().get(0)).getIntValue();
+				CustomPeek newPeek = new CustomPeek(originalPeek.getLocation(),
+						originalPeek.getPort(), index, load.getTarget());
+				neededPeeks.add(newPeek);
+				unusedLoads.add(load);
+			}
+		}
+	}
+
+	private class VariableReplacer extends AbstractActorTransformation {
+		private Variable addedVariable;
+		private Variable removedVariable;
+
+		public void replace(Variable removedVariable, Variable addedVariable,
+				Procedure procedure) {
+			this.removedVariable = removedVariable;
+			this.addedVariable = addedVariable;
+			visit(procedure);
+		}
+
+		public void visit(List<CFGNode> nodes) {
+			ListIterator<CFGNode> it = nodes.listIterator();
+			while (it.hasNext()) {
+				CFGNode node = it.next();
+				itNode = it;
+				if (node.isBlockNode()) {
+					node.accept(this);
+				}
+			}
+		}
+
+		@Override
+		public void visit(Load load) {
+			if (load.getSource().getVariable() == removedVariable) {
+				load.setSource(new Use(addedVariable));
+			}
+		}
+	}
+
+	private List<Load> unusedLoads;
+
+	@Override
+	public void transform(Actor actor) {
+		this.actor = actor;
+
+		itAction = actor.getActions().listIterator();
+		while (itAction.hasNext()) {
+			Action action = itAction.next();
+			visit(action);
+		}
+
+		itAction = actor.getInitializes().listIterator();
+		while (itAction.hasNext()) {
+			Action action = itAction.next();
+			visit(action);
+		}
+	}
+
+	@Override
+	public void visit(Action action) {
+		unusedLoads = new ArrayList<Load>();
+		visit(action.getScheduler());
+	}
+
+	@Override
+	public void visit(Load load) {
+		if (unusedLoads.contains(load)) {
+			itInstruction.remove();
+		}
+	}
+
 	@Override
 	public void visit(Peek peek) {
 		if (!peek.isUnit()) {
 			itInstruction.remove();
-			while (itInstruction.hasNext()) {
-				Instruction instruction = itInstruction.next();
-				if (instruction.isLoad()) {
-					Load load = (Load) instruction;
-					if (load.getSource().getVariable() == peek.getTarget()) {
-						itInstruction.remove();
-						int index = ((IntExpr) load.getIndexes().get(0))
-								.getIntValue();
-						itInstruction.add(new CustomPeek(peek.getLocation(),
-								peek.getPort(), index, load.getTarget()));
-					} else {
-						return;
-					}
-				} else {
-					return;
-				}
+			Instruction instruction = itInstruction.next();
+			if (instruction.isAssign() && !itInstruction.hasNext()) {
+				itInstruction.remove();
+				WhileNode whileNode = (WhileNode) itNode.next();
+				Store store = (Store) ((BlockNode) whileNode.getNodes().get(0))
+						.getInstructions().get(1);
+				(new VariableReplacer()).replace(store.getTarget(),
+						peek.getTarget(), procedure);
+				itNode.remove();
+			}
+			for (CustomPeek newPeek : (new NeededPeekFinder()).find(peek)) {
+				itInstruction.add(newPeek);
 			}
 
+			while (itInstruction.hasPrevious()) {
+				itInstruction.previous();
+			}
 		}
 	}
-
 }
