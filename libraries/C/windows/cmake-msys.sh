2d3ba49d77e88ab9613acc907adcd3d81ce3118d
Hervé Yviquel <hyviquel@gmail.com>
2011-02-16 16:42:53 +0000

    Ooops revert bad commit ; )

--- libraries/C/windows/cmake-msys.sh
+++ libraries/C/windows/cmake-msys.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export CMAKE_PREFIX_PATH=$PWD/SDL-1.2.13:$PWD/SDL_image-1.2.7:/home/hyviquel/workspace/opendf/trunk/contrib/
+export CMAKE_PREFIX_PATH=$PWD/SDL-1.2.13:$PWD/SDL_image-1.2.7
 sh -c "cmake-gui"
