6ebfe2200a2aac52749a35a4129fb2ccf22be10f
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2011-02-22 10:45:46 +0000

    UI: using filters to show tabs only on XDF graphs/vertices.

---
+++ eclipse/plugins/net.sf.orcc.ui/src/net/sf/orcc/ui/properties/OrccGraphFilter.java
@@ -0,0 +1,22 @@
+package net.sf.orcc.ui.properties;
+
+import net.sf.graphiti.model.Graph;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.jface.viewers.IFilter;
+
+public class OrccGraphFilter implements IFilter {
+
+	@Override
+	public boolean select(Object toTest) {
+		if (toTest instanceof EditPart) {
+			Object model = ((EditPart) toTest).getModel();
+			if (model instanceof Graph) {
+				Graph graph = (Graph) model;
+				return graph.getType().getName().equals("XML Dataflow Network");
+			}
+		}
+		return false;
+	}
+
+}
