e56fe27168ca0c6970ea236ed3e0105bed6c641e
Khaled Jerbi <khaled_jerbi@yahoo.fr>
2011-02-22 15:38:48 +0000

    basic transformation: fsm null and 1 repeat input

---
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/Multi2MonoToken.java
@@ -0,0 +1,491 @@
+/*
+ * Copyright (c) 2010, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+package net.sf.orcc.backends.transformations;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map.Entry;
+
+import net.sf.orcc.backends.vhdl.transformations.CodeMover;
+import net.sf.orcc.ir.AbstractActorVisitor;
+import net.sf.orcc.ir.Action;
+import net.sf.orcc.ir.CFGNode;
+import net.sf.orcc.ir.Expression;
+import net.sf.orcc.ir.FSM;
+import net.sf.orcc.ir.GlobalVariable;
+import net.sf.orcc.ir.Instruction;
+import net.sf.orcc.ir.IrFactory;
+import net.sf.orcc.ir.LocalVariable;
+import net.sf.orcc.ir.Location;
+import net.sf.orcc.ir.Pattern;
+import net.sf.orcc.ir.Port;
+import net.sf.orcc.ir.Procedure;
+import net.sf.orcc.ir.Tag;
+import net.sf.orcc.ir.Type;
+import net.sf.orcc.ir.Use;
+import net.sf.orcc.ir.Variable;
+import net.sf.orcc.ir.expr.BinaryExpr;
+import net.sf.orcc.ir.expr.BinaryOp;
+import net.sf.orcc.ir.expr.IntExpr;
+import net.sf.orcc.ir.expr.VarExpr;
+import net.sf.orcc.ir.instructions.Assign;
+import net.sf.orcc.ir.instructions.Load;
+import net.sf.orcc.ir.instructions.Read;
+import net.sf.orcc.ir.instructions.Return;
+import net.sf.orcc.ir.instructions.Store;
+import net.sf.orcc.ir.nodes.BlockNode;
+import net.sf.orcc.util.OrderedMap;
+
+/**
+ * This class defines a visitor that transforms multi-token to mono-token data
+ * transfer
+ * 
+ * @author Khaled Jerbi
+ * 
+ */
+public class Multi2MonoToken extends AbstractActorVisitor {
+
+	/**
+	 * This class defines a visitor that substitutes process variable names with
+	 * those of the newly defined actions
+	 * 
+	 * @author Khaled Jerbi
+	 * 
+	 */
+	private class ModifyProcessAction extends AbstractActorVisitor {
+
+		private Variable localTable;
+
+		private GlobalVariable tab;
+
+		public ModifyProcessAction(GlobalVariable tab) {
+			this.tab = tab;
+		}
+
+		@Override
+		public void visit(Load load) {
+			Use useArray = new Use(tab);
+
+			String name = load.getSource().getVariable().getName();
+			if (name.equals(localTable)) {
+				load.setSource(useArray);
+			}
+		}
+
+		@Override
+		public void visit(Read read) {
+			if (read.getPort() == port) {
+				localTable = read.getTarget();
+			}
+			itInstruction.remove();
+		}
+
+		@Override
+		public void visit(Store store) {
+			if (store.getTarget().getName().equals(localTable)) {
+				store.setTarget(tab);
+			}
+		}
+
+	}
+
+	private Type entryType;
+
+	private int inputIndex = 0;
+
+	private Port port;
+
+	/**
+	 * This method creates an action with the given name.
+	 * 
+	 * @param name
+	 *            name of the action
+	 * @return a new action created with the given name
+	 */
+	private Action createAction(String name) {
+		Procedure body = new Procedure(name, new Location(),
+				IrFactory.eINSTANCE.createTypeVoid());
+		Procedure scheduler = new Procedure("isSchedulable_" + name,
+				new Location(), IrFactory.eINSTANCE.createTypeBool());
+
+		List<String> identifiers = new ArrayList<String>();
+		identifiers.add(name);
+		Tag tag = new Tag(identifiers);
+		Location location = new Location();
+		Pattern inputPattern = new Pattern();
+		Pattern outputPattern = new Pattern();
+		Action newAction = new Action(location, tag, inputPattern,
+				outputPattern, scheduler, body);
+		return newAction;
+	}
+
+	/**
+	 * This method creates a global variable counter for store with the given
+	 * name.
+	 * 
+	 * @param name
+	 *            name of the counter
+	 * @return new counter with the given name
+	 */
+	private GlobalVariable createCounter(String name) {
+		GlobalVariable newCounter = new GlobalVariable(new Location(),
+				IrFactory.eINSTANCE.createTypeInt(16), name, true);
+		Expression expression = new IntExpr(0);
+		newCounter.setInitialValue(expression);
+		actor.getStateVars().put(newCounter.getName(), newCounter);
+		return newCounter;
+	}
+
+	/**
+	 * This method creates the done action that is schedulable when required
+	 * number of tokens is read (written)
+	 * 
+	 * @param actionName
+	 *            name of the action
+	 * @param counter
+	 *            global variable counter used for reading (writing) tokens
+	 * @param numTokens
+	 *            repeat value
+	 * @return
+	 */
+	private Action createDoneAction(String actionName, GlobalVariable counter,
+			int numTokens) {
+		Action newDoneAction = createAction(actionName + "NewDone");
+		defineDoneBody(counter, newDoneAction.getBody());
+		defineDoneScheduler(counter, numTokens, newDoneAction.getScheduler());
+		return newDoneAction;
+	}
+
+	/**
+	 * This method creates the required post-transformation FSM
+	 * 
+	 * @param actionName
+	 *            name of the action
+	 * @param store
+	 *            new store action
+	 * @param done
+	 *            new done action
+	 * @param process
+	 *            new process action
+	 */
+	private void createFsm(String actionName, Action store, Action done,
+			Action process) {
+		FSM fsm = new FSM();
+		String storeName = "newStateStore" + actionName;
+		String processName = "newStateProcess" + actionName;
+
+		actor.getActionScheduler().getActions().clear();
+
+		fsm.addTransition(storeName, store, storeName);
+		fsm.addTransition(storeName, done, processName);
+		fsm.addTransition(processName, process, storeName);
+		fsm.setInitialState(storeName);
+	}
+
+	/**
+	 * This method creates the process action using the nodes & locals of the
+	 * action getting transformed
+	 * 
+	 * @param action
+	 *            currently transforming action
+	 * @return new process action
+	 */
+	private Action createProcessAction(Action action) {
+		Action newProcessAction = createAction("newProcess_" + action.getName());
+		Procedure body = newProcessAction.getBody();
+		Procedure scheduler = newProcessAction.getScheduler();
+		CodeMover codeMover = new CodeMover();
+
+		// moveBody
+		ListIterator<CFGNode> listIt = action.getBody().getNodes()
+				.listIterator();
+		codeMover.setTargetProcedure(body);
+		codeMover.moveNodes(listIt);
+		Iterator<LocalVariable> it = action.getBody().getLocals().iterator();
+		moveLocals(it, body);
+
+		// moveScheduler
+		listIt = action.getScheduler().getNodes().listIterator();
+		codeMover.setTargetProcedure(scheduler);
+		codeMover.moveNodes(listIt);
+		it = action.getScheduler().getLocals().iterator();
+		moveLocals(it, scheduler);
+		return newProcessAction;
+	}
+
+	/**
+	 * This method defines a new store action that reads 1 token on the repeat
+	 * port
+	 * 
+	 * @param actionName
+	 *            name of the new store action
+	 * @param numTokens
+	 *            repeat number
+	 * @param port
+	 *            repeat port
+	 * @param readCounter
+	 *            global variable counter
+	 * @param storeList
+	 *            global variable list of store (write)
+	 * @return new store action
+	 */
+	private Action createStoreAction(String actionName, int numTokens,
+			Port port, GlobalVariable readCounter, GlobalVariable storeList) {
+		String storeName = actionName + port.getName() + "_NewStore";
+		Action newStoreAction = createAction(storeName);
+		defineStoreBody(port, readCounter, storeList, newStoreAction.getBody());
+		defineStoreScheduler(numTokens, readCounter,
+				newStoreAction.getScheduler());
+		return newStoreAction;
+	}
+
+	/**
+	 * This method creates a global variable counter for data storing (writing)
+	 * 
+	 * @param name
+	 *            name of the list
+	 * @param numTokens
+	 *            size of the list
+	 * @param entryType
+	 *            type of the list
+	 * @return a global variable list
+	 */
+	private GlobalVariable createTab(String name, int numTokens, Type entryType) {
+		Type type = IrFactory.eINSTANCE.createTypeList(numTokens, entryType);
+		GlobalVariable newList = new GlobalVariable(new Location(), type, name,
+				true);
+		actor.getStateVars().put(newList.getName(), newList);
+		return newList;
+	}
+
+	/**
+	 * This method creates the instructions for the body of the new done action
+	 * (Adds a Store of 0 to the read counter in the given procedure)
+	 * 
+	 * @param readCounter
+	 *            global variable counter
+	 * @param body
+	 *            new done action body
+	 */
+	private void defineDoneBody(GlobalVariable readCounter, Procedure body) {
+		BlockNode bodyNode = BlockNode.getFirst(body);
+
+		Store store = new Store(readCounter, new IntExpr(0));
+		bodyNode.add(store);
+		bodyNode.add(new Return(null));
+	}
+
+	/**
+	 * This method creates the instructions for the isSchedulable procedure.
+	 * 
+	 * @param readCounter
+	 *            global variable counter
+	 * @param numTokens
+	 *            isSchedulable guard condition for new done action
+	 * @param scheduler
+	 *            new done action scheduler
+	 */
+	private void defineDoneScheduler(GlobalVariable readCounter, int numTokens,
+			Procedure scheduler) {
+		BlockNode blkNode = BlockNode.getFirst(scheduler);
+
+		// add local variable and load
+		OrderedMap<String, LocalVariable> locals = scheduler.getLocals();
+		LocalVariable counter = new LocalVariable(true, 1, new Location(),
+				"local_counter", readCounter.getType());
+		locals.put(counter.getName(), counter);
+
+		Load schedulerLoad = new Load(counter, new Use(readCounter));
+		blkNode.add(schedulerLoad);
+
+		// create result variable
+		LocalVariable result = new LocalVariable(true, 1, new Location(),
+				"result", IrFactory.eINSTANCE.createTypeBool());
+		locals.put(result.getName(), result);
+
+		// create assign to result variable
+		Expression guardValue = new IntExpr(numTokens);
+		Expression counterExpression = new VarExpr(new Use(counter));
+		Expression value = new BinaryExpr(counterExpression, BinaryOp.EQ,
+				guardValue, IrFactory.eINSTANCE.createTypeBool());
+
+		Assign assign = new Assign(result, value);
+		blkNode.add(assign);
+	}
+
+	/**
+	 * This method creates the instructions for the body of the new store action
+	 * 
+	 * @param port
+	 *            repeat port
+	 * @param readCounter
+	 *            global variable counter
+	 * @param storeList
+	 *            global store (write) list
+	 * @param body
+	 *            new store action body
+	 */
+	private void defineStoreBody(Port port, GlobalVariable readCounter,
+			GlobalVariable storeList, Procedure body) {
+		BlockNode bodyNode = BlockNode.getFirst(body);
+
+		OrderedMap<String, LocalVariable> locals = body.getLocals();
+		LocalVariable counter = new LocalVariable(true, 1, new Location(),
+				port.getName() + "_Local_counter", readCounter.getType());
+		locals.put(counter.getName(), counter);
+		Use readCounterUse = new Use(readCounter);
+		Instruction load1 = new Load(counter, readCounterUse);
+		bodyNode.add(load1);
+
+		LocalVariable localINPUT = new LocalVariable(true, 0, new Location(),
+				port.getName(), IrFactory.eINSTANCE.createTypeList(1,
+						port.getType()));
+		locals.put(localINPUT.getName(), localINPUT);
+		Instruction read = new Read(port, 1, localINPUT);
+		localINPUT.setInstruction(read);
+		bodyNode.add(read);
+
+		LocalVariable input = new LocalVariable(true, 1, new Location(),
+				port.getName() + "_Input", port.getType());
+		locals.put(input.getName(), input);
+		List<Expression> load2Index = new ArrayList<Expression>(1);
+		load2Index.add(new IntExpr(0));
+		Instruction load2 = new Load(input, new Use(localINPUT), load2Index);
+		bodyNode.add(load2);
+
+		List<Expression> store1Index = new ArrayList<Expression>(1);
+		store1Index.add(new VarExpr(new Use(counter)));
+		Instruction store1 = new Store(storeList, store1Index, new VarExpr(
+				new Use(input)));
+		bodyNode.add(store1);
+
+		LocalVariable counter2 = new LocalVariable(true, 2, new Location(),
+				port.getName() + "_Local_counter", readCounter.getType());
+		locals.put(counter2.getName(), counter2);
+		Expression storeIndexElement = new VarExpr(new Use(counter));
+		Expression e2 = new IntExpr(1);
+		Expression assignValue = new BinaryExpr(storeIndexElement,
+				BinaryOp.PLUS, e2, entryType);
+		Instruction assign = new Assign(counter2, assignValue);
+		bodyNode.add(assign);
+
+		Instruction store2 = new Store(readCounter, new VarExpr(new Use(
+				counter2)));
+		bodyNode.add(store2);
+
+		Instruction returnInstruction = new Return(null);
+		bodyNode.add(returnInstruction);
+	}
+
+	/**
+	 * This method creates the instructions for the isSchedulable of the new
+	 * store action
+	 * 
+	 * @param numTokens
+	 *            isSchedulable guard condition for new store action
+	 * @param counter
+	 *            global variable counter
+	 * @param scheduler
+	 *            new store action scheduler
+	 */
+	private void defineStoreScheduler(int numTokens, GlobalVariable counter,
+			Procedure scheduler) {
+		BlockNode blkNode = BlockNode.getFirst(scheduler);
+		OrderedMap<String, LocalVariable> locals = scheduler.getLocals();
+
+		LocalVariable result = new LocalVariable(true, 1, new Location(),
+				"resultStoreScheduler", IrFactory.eINSTANCE.createTypeBool());
+		locals.put(result.getName(), result);
+		Expression guardValue = new IntExpr(numTokens);
+		Expression counterExpression = new VarExpr(new Use(counter));
+		Expression schedulerValue = new BinaryExpr(counterExpression,
+				BinaryOp.LT, guardValue, IrFactory.eINSTANCE.createTypeBool());
+		Instruction schedulerAssign = new Assign(result, schedulerValue);
+		blkNode.add(schedulerAssign);
+
+		Expression SchedulerReturnValue = new VarExpr(new Use(result));
+		Instruction schedulerReturnInstruction = new Return(
+				SchedulerReturnValue);
+		blkNode.add(schedulerReturnInstruction);
+	}
+
+	/**
+	 * This method moves the local variables of a procedure to another using a
+	 * LocalVariable iterator
+	 * 
+	 * @param itVar
+	 *            source LocalVariable iterator
+	 * @param newProc
+	 *            target procedure
+	 */
+	public void moveLocals(Iterator<LocalVariable> itVar, Procedure newProc) {
+		while (itVar.hasNext()) {
+			LocalVariable var = itVar.next();
+			itVar.remove();
+			newProc.getLocals().put(var.getName(), var);
+		}
+	}
+
+	@Override
+	public void visit(Action action) {
+		itAction.remove();
+
+		for (Entry<Port, Integer> entry : action.getInputPattern().entrySet()) {
+			int numTokens = entry.getValue();
+			inputIndex = inputIndex + 1;
+			port = entry.getKey();
+			entryType = entry.getKey().getType();
+
+			String counterName = action.getName() + "NewReadCounter"
+					+ inputIndex;
+			GlobalVariable counter = createCounter(counterName);
+			String listName = action.getName() + "NewStoreList" + inputIndex;
+			GlobalVariable tab = createTab(listName, numTokens, entryType);
+
+			Action store = createStoreAction(action.getName(), numTokens, port,
+					counter, tab);
+			itAction.add(store);
+
+			Action done = createDoneAction(action.getName(), counter, numTokens);
+			itAction.add(done);
+
+			Action process = createProcessAction(action);
+			itAction.add(process);
+
+			ModifyProcessAction modifyProcessAction = new ModifyProcessAction(
+					tab);
+			modifyProcessAction.visit(process.getBody());
+
+			createFsm(action.getName(), store, done, process);
+		}
+	}
+}
