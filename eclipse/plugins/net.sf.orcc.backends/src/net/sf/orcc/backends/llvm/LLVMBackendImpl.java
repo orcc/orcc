47a1045a2d224435e856a058917824f5d9367844
Olivier Labois <olabois@users.sourceforge.net>
2011-02-23 10:37:13 +0000

    Canceled : Fix some specifics WinXp bugs in jadeToolbox

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/LLVMBackendImpl.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/LLVMBackendImpl.java
@@ -173,12 +173,7 @@ public class LLVMBackendImpl extends AbstractBackend {
 		}
 
 		// JadeToolbox is required to finalize actors
-		// Add list of actor
-		for (Actor actor : actors) {
-			if (!actor.isNative()) {
-				runJadeToolBox(actor);
-			}
-		}
+		runJadeToolBox(actors);
 	}
 
 	@Override
@@ -199,7 +194,7 @@ public class LLVMBackendImpl extends AbstractBackend {
 		}
 	}
 
-	private void runJadeToolBox(Actor actor) throws OrccException {
+	private void runJadeToolBox(List<Actor> actors) throws OrccException {
 		List<String> cmdList = new ArrayList<String>();
 		cmdList.add(jadeToolbox);
 		cmdList.add("-" + optLevel);
@@ -215,9 +210,13 @@ public class LLVMBackendImpl extends AbstractBackend {
 			cmdList.add("-a");
 		}
 
-		//Add actor file
-		cmdList.add(actor.getName());
-		
+		// Add list of actor
+		for (Actor actor : actors) {
+			if (!actor.isNative()) {
+				cmdList.add(actor.getName());
+			}
+		}
+
 		String[] cmd = cmdList.toArray(new String[] {});
 
 		// Launch application
