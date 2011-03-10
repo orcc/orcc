e29b175633cf7a20427c5098a59c19413d8791d5
Hervé Yviquel <hyviquel@gmail.com>
2011-03-10 10:24:30 +0000

    (Genetic) Fix genetic algorithm with new scheduler.

--- libraries/C/lib/src/scheduler.c
+++ libraries/C/lib/src/scheduler.c
@@ -28,10 +28,11 @@ void sched_init(struct scheduler_s *sched, int id, int schedulers_nb,
 	sched->next_schedulable = 0;
 	sched->next_else_schedulable = 0;
 
-	sched->waiting_schedulable = (struct waiting_s **) calloc(
-			schedulers_nb, sizeof(struct waiting_s *));
+	sched->waiting_schedulable = (struct waiting_s **) calloc(schedulers_nb,
+			sizeof(struct waiting_s *));
 	for (i = 0; i < schedulers_nb; i++) {
-		sched->waiting_schedulable[i] = (struct waiting_s *) calloc(1, sizeof(struct waiting_s));
+		sched->waiting_schedulable[i] = (struct waiting_s *) calloc(1,
+				sizeof(struct waiting_s));
 		//sched->waiting_schedulable[i]->next_entry = 0;
 		//sched->waiting_schedulable[i]->next_waiting = 0;
 	}
@@ -48,16 +49,18 @@ void sched_reinit(struct scheduler_s *sched, int schedulers_nb, int num_actors,
 	int i;
 
 	sched->actors = actors;
-	for (i = 0; i < num_actors; i++) {
-		actors[i]->sched = sched;
-		if (!strcmp(actors[i]->name, "source")) {
-			sched_add_schedulable(sched, actors[i]);
-		}
-	}
 	sched->num_actors = num_actors;
 	sched->next_entry = 0;
 	sched->next_schedulable = 0;
 	sched->next_else_schedulable = 0;
+
+	for (i = 0; i < num_actors; i++) {
+		actors[i]->sched = sched;
+		actors[i]->in_list = 0;
+		if (!strcmp(actors[i]->name, "source")) {
+			sched_add_schedulable(sched, actors[i]);
+		}
+	}
 
 	for (i = 0; i < schedulers_nb; i++) {
 		sched->waiting_schedulable[i]->next_entry = 0;
