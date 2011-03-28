5f0f0d31dfa40de9bf1d452f67ced839cdad4d6d
Endri Bezati <endri.bezati@gmail.com>
2011-03-28 13:57:45 +0000

    C System Actors: adding new writer based on native procedures

--- libraries/C/lib/src/writer.c
+++ libraries/C/lib/src/writer.c
@@ -1,4 +1,33 @@
-// Source file is "/home/endrix/Projects/jpeg/src/jpeg/encoder/hw/Test/writer.cal"
+/*
+* Copyright (c) 2009, EPFL
+* All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+* 
+*   * Redistributions of source code must retain the above copyright notice,
+*     this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above copyright notice,
+*     this list of conditions and the following disclaimer in the documentation
+*     and/or other materials provided with the distribution.
+*   * Neither the name of the EPFL nor the names of its
+*     contributors may be used to endorse or promote products derived from this
+*     software without specific prior written permission.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+* STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+*/
+
+// Author : Endri Bezati (endri.bezati@epfl.ch)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,97 +36,15 @@
 #include "orcc_fifo.h"
 #include "orcc_util.h"
 
-#define SIZE 512
+FILE *F = NULL;
+static int cnt = 0; 
 
-////////////////////////////////////////////////////////////////////////////////
-// Input FIFOs
-extern struct fifo_i16_s *writer_Byte;
-extern struct fifo_i32_s *writer_pEOF;
-
-static unsigned int index_Byte;
-static unsigned int numTokens_Byte;
-#define NUM_READERS_Byte 1
-#define SIZE_Byte SIZE
-#define tokens_Byte writer_Byte->contents
-
-static unsigned int index_pEOF;
-static unsigned int numTokens_pEOF;
-#define NUM_READERS_pEOF 1
-#define SIZE_pEOF SIZE
-#define tokens_pEOF writer_pEOF->contents
-
-static FILE *F = NULL;
-static int cnt = 0;
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Input FIFOs
-////////////////////////////////////////////////////////////////////////////////
-// Input FIFOs Id
-static unsigned int fifo_writer_Byte_id; 
-static unsigned int fifo_writer_pEOF_id;  
-
-////////////////////////////////////////////////////////////////////////////////
-// Parameter values of the instance
-////////////////////////////////////////////////////////////////////////////////
-// State variables of the actor
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Initial FSM state of the actor
-enum states {
-	my_state_s0 = 0,
-	my_state_s1
-};
-
-static char *stateNames[] = {
-	"s0",
-	"s1"
-};
-
-static enum states _FSM_state;
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Token functions
-
-static void read_Byte() {
-	index_Byte = writer_Byte->read_inds[fifo_writer_Byte_id];
-	numTokens_Byte = index_Byte + fifo_i16_get_num_tokens(writer_Byte, fifo_writer_Byte_id);
-}
-
-static void read_end_Byte() {
-	writer_Byte->read_inds[fifo_writer_Byte_id] = index_Byte;
-}
-
-static void read_pEOF() {
-	index_pEOF = writer_pEOF->read_inds[fifo_writer_pEOF_id];
-	numTokens_pEOF = index_pEOF + fifo_i32_get_num_tokens(writer_pEOF, fifo_writer_pEOF_id);
-}
-
-static void read_end_pEOF() {
-	writer_pEOF->read_inds[fifo_writer_pEOF_id] = index_pEOF;
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Initializes
-void writer_initialize(unsigned int fifo_Byte_id, unsigned int fifo_pEOF_id) {
+void Writer_init() {
 	
-	/* Set initial state to current FSM state */
-	_FSM_state = my_state_s0;
-
-	/* Set initial value to global variable */
-
-	/* Initialize input FIFOs id */
-	fifo_writer_Byte_id = fifo_Byte_id; 
-	fifo_writer_pEOF_id = fifo_pEOF_id;  
-
-
 	if (write_file == NULL) {
 		print_usage();
 		fprintf(stderr, "No write file given!\n");
-		wait_for_key();
+		//wait_for_key();
 		exit(1);
 	}
 
@@ -108,104 +55,21 @@ void writer_initialize(unsigned int fifo_Byte_id, unsigned int fifo_pEOF_id) {
 		}
 
 		fprintf(stderr, "could not open file \"%s\"\n", write_file);
-		wait_for_key();
+		//wait_for_key();
 		exit(1);
 	}else{
 		fseek(F,0,SEEK_SET);	
 	}
 }
 
+void Writer_write(u8 byte){
 
-////////////////////////////////////////////////////////////////////////////////
-// Action scheduler
-void writer_scheduler(struct schedinfo_s *si) {
-	int i = 0;
-	i32 peof;
-	u8 wr;
-	si->ports = 0;
-	read_Byte();
-	read_pEOF();
-
-
-	// jump to FSM state 
-	switch (_FSM_state) {
-	case my_state_s0:
-		goto l_s0;
-	case my_state_s1:
-		goto l_s1;
-	default:
-		printf("unknown state in writer.c : %s\n", stateNames[_FSM_state]);
-		wait_for_key();
-		exit(1);
-	}
-
-	// FSM transitions
-
-l_s0:
-	if (index_pEOF + 1 <= numTokens_pEOF) {
-		int ports = 0;
-		if (ports != 0) {
-			_FSM_state = my_state_s0;
-			si->num_firings = i;
-			si->reason = full;
-			si->ports = ports;
-			goto finished;
-		}
-		peof = tokens_pEOF[(index_pEOF + 0) % SIZE_pEOF];
-		if (peof == 0) {
-			i++;		
-			index_pEOF += 1;
-			goto l_s1;
-		} else {
-			fclose(F);
-			index_pEOF += 1;
-			exit(666);		
-		}
-	} else {
-		int ports = 0;
-		if (index_pEOF + 1 > numTokens_pEOF) {
-			ports |= 0x02;
-		}
-		si->num_firings = i;
-		si->reason = starved;
-		si->ports |= ports;
-		_FSM_state = my_state_s0;
-		goto finished;
-	}
-
-l_s1:
-	if (index_Byte + 1 <= numTokens_Byte ) {
-		int ports = 0;
-		if (ports != 0) {
-			_FSM_state = my_state_s1;
-			si->num_firings = i;
-			si->reason = full;
-			si->ports = ports;
-			goto finished;
-		}
-		wr = tokens_Byte[(index_Byte + 0) % SIZE_Byte];
-		fseek(F,sizeof(u8)*cnt,SEEK_SET);
-		fwrite(&wr,sizeof(u8),1,F);
-		cnt++;	
-
-		index_Byte += 1;
-		
-		i++;
-		goto l_s0;
-	} else {
-		int ports = 0;
-		if (index_Byte + 1 > numTokens_Byte) {
-			ports |= 0x01;
-		}
-		si->num_firings = i;
-		si->reason = starved;
-		si->ports |= ports;
-		_FSM_state = my_state_s1;
-		goto finished;
-	}
-
-finished:
-	read_end_Byte();
-	read_end_pEOF();
+	fseek(F,sizeof(u8)*cnt,SEEK_SET);
+	fwrite(&byte,sizeof(u8),1,F);
+	cnt++;	
 }
 
+void Writer_close(){
+	fclose(F);
+	exit(666);
+}
