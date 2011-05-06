2f1cc2b4384793e3e3e9c50f4fa544e72ef2c942
Hervé Yviquel <hyviquel@gmail.com>
2011-05-06 16:28:14 +0000

    (LLVM backend) Rewrite CopyPropagator with EMF style

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/threeAddressCodeTransformation/CopyPropagator.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/threeAddressCodeTransformation/CopyPropagator.java
@@ -28,34 +28,16 @@
  */
 package net.sf.orcc.backends.transformations.threeAddressCodeTransformation;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import net.sf.orcc.ir.ExprBinary;
-import net.sf.orcc.ir.ExprList;
-import net.sf.orcc.ir.ExprUnary;
 import net.sf.orcc.ir.ExprVar;
-import net.sf.orcc.ir.Expression;
 import net.sf.orcc.ir.InstAssign;
-import net.sf.orcc.ir.InstCall;
-import net.sf.orcc.ir.InstLoad;
-import net.sf.orcc.ir.InstPhi;
-import net.sf.orcc.ir.InstReturn;
-import net.sf.orcc.ir.InstStore;
-import net.sf.orcc.ir.Instruction;
-import net.sf.orcc.ir.IrFactory;
-import net.sf.orcc.ir.NodeIf;
-import net.sf.orcc.ir.NodeWhile;
 import net.sf.orcc.ir.Procedure;
-import net.sf.orcc.ir.Type;
-import net.sf.orcc.ir.TypeList;
+import net.sf.orcc.ir.Use;
 import net.sf.orcc.ir.Var;
 import net.sf.orcc.ir.util.AbstractActorVisitor;
 import net.sf.orcc.ir.util.EcoreHelper;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 /**
  * Replace occurrences with direct assignments to their corresponding values. A
@@ -65,195 +47,36 @@ import org.eclipse.emf.common.util.EList;
  * @author Jerome GORIN
  * 
  */
-public class CopyPropagator extends
-		AbstractActorVisitor<Expression> {
+public class CopyPropagator extends AbstractActorVisitor<Object> {
 
-	private Map<Var, Expression> copyVars;
+	protected boolean changed;
 
-	private List<Instruction> removedInstrs;
-
-	public CopyPropagator() {
-		copyVars = new HashMap<Var, Expression>();
-		removedInstrs = new ArrayList<Instruction>();
-
+	@Override
+	public Object caseProcedure(Procedure procedure) {
+		do {
+			changed = false;
+			super.caseProcedure(procedure);
+		} while (changed);
+		return null;
 	}
 
 	@Override
-	public Expression caseExprBinary(ExprBinary expr) {
-		expr.setE1(doSwitch(expr.getE1()));
-		expr.setE2(doSwitch(expr.getE2()));
-		return expr;
-	}
-
-	@Override
-	public Expression caseExprList(ExprList expr) {
-		List<Expression> values = expr.getValue();
-		for (Expression subExpr : values) {
-			values.set(values.indexOf(subExpr), doSwitch(subExpr));
-		}
-		return expr;
-	}
-
-	@Override
-	public Expression caseExprUnary(ExprUnary expr) {
-		expr.setExpr(doSwitch(expr.getExpr()));
-		return expr;
-	}
-
-	@Override
-	public Expression caseExprVar(ExprVar expr) {
-		Var var = expr.getUse().getVariable();
-		if (copyVars.containsKey(var)) {
-			return doSwitch(copyVars.get(var));
-		}
-		return expr;
-	}
-
-	/**
-	 * Detect if the assign instructions can be removed
-	 * 
-	 * @param assign
-	 *            assign instruction
-	 */
-	@Override
-	public Expression caseInstAssign(InstAssign assign) {
-		Expression value = assign.getValue();
-
-		if ((!value.isBinaryExpr()) && (!value.isUnaryExpr())) {
-			// Assign instruction can be remove
+	public Object caseInstAssign(InstAssign assign) {
+		if (assign.getValue().isVarExpr()) {
+			Var source = ((ExprVar) assign.getValue()).getUse().getVariable();
 			Var target = assign.getTarget().getVariable();
-			Expression expr = assign.getValue();
-
-			// Set instruction and variable as to be remove
-			copyVars.put(target, expr);
-			removedInstrs.add(assign);
-		} else {
-			assign.setValue(doSwitch(value));
-		}
-
-		return null;
-	}
-
-	@Override
-	public Expression caseInstCall(InstCall call) {
-		EList<Expression> parameters = call.getParameters();
-		for (Expression expr : parameters) {
-			parameters.set(parameters.indexOf(expr), doSwitch(expr));
-		}
-		return null;
-	}
-
-	@Override
-	public Expression caseInstLoad(InstLoad load) {
-		EList<Expression> indexes = load.getIndexes();
-		for (Expression expr : indexes) {
-			indexes.set(indexes.indexOf(expr), doSwitch(expr));
-		}
-		return null;
-	}
-
-	@Override
-	public Expression caseInstPhi(InstPhi phi) {
-		Var target = phi.getTarget().getVariable();
-		EList<Var> parameters = procedure.getParameters();
-
-		EList<Expression> values = phi.getValues();
-		for (Expression expr : values) {
-			values.set(values.indexOf(expr), doSwitch(expr));
-		}
-
-		// Remove local variable with index = 0 from value
-		for (Expression value : values) {
-			if (value.isVarExpr()) {
-				ExprVar sourceExpr = (ExprVar) value;
-				Var source = sourceExpr.getUse().getVariable();
-
-				// Local variable must not be a parameter of the procedure
-				if (source.getIndex() == 0
-						&& !parameters.contains(source.getName())) {
-					Expression constExpr;
-					Type type = target.getType();
-
-					// Creating constants value
-					if (type.isList()) {
-						TypeList typeList = (TypeList) type;
-						List<Expression> constants = new ArrayList<Expression>();
-
-						for (int i = 0; i < typeList.getSize(); i++) {
-							constants.add(IrFactory.eINSTANCE.createExprInt(0));
-						}
-
-						constExpr = IrFactory.eINSTANCE
-								.createExprList(constants);
-					} else {
-						constExpr = IrFactory.eINSTANCE.createExprInt(0);
-					}
-
-					values.set(values.indexOf(value), constExpr);
+			EList<Use> targetUses = target.getUses();
+			if (!targetUses.isEmpty()) {
+				changed = true;
+				while (!targetUses.isEmpty()) {
+					targetUses.get(0).setVariable(source);
 				}
+				EcoreUtil.remove(target);
+				EcoreHelper.delete(assign);
+				indexInst--;
 			}
 		}
 		return null;
 	}
 
-	@Override
-	public Expression caseInstReturn(InstReturn instReturn) {
-		Expression expr = instReturn.getValue();
-		if (expr != null) {
-			instReturn.setValue(doSwitch(instReturn.getValue()));
-		}
-		return null;
-	}
-
-	@Override
-	public Expression caseInstStore(InstStore store) {
-		store.setValue(doSwitch(store.getValue()));
-
-		EList<Expression> indexes = store.getIndexes();
-		for (Expression expr : indexes) {
-			indexes.set(indexes.indexOf(expr), doSwitch(expr));
-		}
-
-		return null;
-	}
-
-	@Override
-	public Expression caseNodeIf(NodeIf nodeIf) {
-		nodeIf.setCondition(doSwitch(nodeIf.getCondition()));
-		return super.caseNodeIf(nodeIf);
-	}
-
-	@Override
-	public Expression caseNodeWhile(NodeWhile nodeWhile) {
-		nodeWhile.setCondition(doSwitch(nodeWhile.getCondition()));
-		return super.caseNodeWhile(nodeWhile);
-	}
-
-	@Override
-	public Expression caseProcedure(Procedure procedure) {
-		copyVars.clear();
-		super.visit(procedure);
-
-		// Remove useless instructions and variables
-		EcoreHelper.delete(removedInstrs);
-		removeVariables(copyVars);
-
-		return null;
-	}
-
-	/**
-	 * Removes the given list of variables from the procedure
-	 * 
-	 * @param vars
-	 *            a map of variable
-	 */
-	private void removeVariables(Map<Var, Expression> vars) {
-		EList<Var> lovalVars = procedure.getLocals();
-
-		for (Map.Entry<Var, Expression> entry : copyVars.entrySet()) {
-			Var var = entry.getKey();
-			lovalVars.remove(var.getName());
-		}
-
-	}
 }
