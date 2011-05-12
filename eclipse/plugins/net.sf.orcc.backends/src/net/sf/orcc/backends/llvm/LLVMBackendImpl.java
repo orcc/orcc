a65714e31d3ddeb82a86fe80b4055043cbb9f1c3
Hervé Yviquel <hyviquel@gmail.com>
2011-05-12 22:29:11 +0000

    (LLVM) Fix name of indexed variables and various other things

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/LLVMBackendImpl.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/LLVMBackendImpl.java
@@ -56,6 +56,7 @@ import net.sf.orcc.backends.transformations.tac.TacTransformation;
 import net.sf.orcc.backends.xlim.transformations.InstPhiTransformation;
 import net.sf.orcc.ir.Actor;
 import net.sf.orcc.ir.transformations.BlockCombine;
+import net.sf.orcc.ir.transformations.BuildCFG;
 import net.sf.orcc.ir.transformations.RenameTransformation;
 import net.sf.orcc.ir.transformations.SSATransformation;
 import net.sf.orcc.ir.util.ActorVisitor;
@@ -141,7 +142,8 @@ public class LLVMBackendImpl extends AbstractBackend {
 				new RenameTransformation(this.transformations),
 				new TacTransformation(true), new InstPhiTransformation(),
 				new GetElementPtrAdder(), new BlockCombine(),
-				new TypeSizeTransformation(), new CastAdder(true) };
+				new TypeSizeTransformation(), new CastAdder(true),
+				new BuildCFG() };
 
 		for (ActorVisitor<?> transformation : transformations) {
 			transformation.doSwitch(actor);
