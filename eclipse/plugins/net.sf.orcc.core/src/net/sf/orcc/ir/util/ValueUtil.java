869219a13595ac74c09fbe0e739faeeeb719b2ba
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2011-06-24 17:35:24 +0000

    (evaluator) added Javadoc.

--- eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/ir/util/ValueUtil.java
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/ir/util/ValueUtil.java
@@ -43,23 +43,84 @@ import net.sf.orcc.ir.Type;
  */
 public class ValueUtil {
 
+	/**
+	 * Returns a new integer equal to the sum of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object add(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).add(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.add(bi2));
 	}
 
+	/**
+	 * Returns a new integer equal to the bitwise and of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object and(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).and(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.and(bi2));
 	}
 
 	public static boolean areInts(Object val1, Object val2) {
 		return isInt(val1) && isInt(val2);
 	}
 
+	/**
+	 * Returns a new integer equal to the division of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object divide(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).xor(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.divide(bi2));
 	}
 
+	/**
+	 * Returns a new boolean that is <code>true</code> if val1 is greater than
+	 * or equal to val2, or <code>null</code> if the two operands are not both
+	 * of the same type.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return a boolean value or <code>null</code>
+	 */
 	public static Object ge(Object val1, Object val2) {
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
 		return getBigInteger(val1).compareTo(getBigInteger(val2)) >= 0;
 	}
 
@@ -76,7 +137,7 @@ public class ValueUtil {
 		} else if (value instanceof BigInteger) {
 			return (BigInteger) value;
 		} else {
-			throw new ClassCastException();
+			return null;
 		}
 	}
 
@@ -114,52 +175,180 @@ public class ValueUtil {
 		} else if (value instanceof BigInteger) {
 			return ((BigInteger) value).intValue();
 		} else {
-			throw new ClassCastException();
+			return 0;
 		}
 	}
 
+	/**
+	 * Returns a new boolean that is <code>true</code> if val1 is greater than
+	 * val2, or <code>null</code> if the two operands are not both of the same
+	 * type.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return a boolean value or <code>null</code>
+	 */
 	public static Object gt(Object val1, Object val2) {
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
 		return getBigInteger(val1).compareTo(getBigInteger(val2)) > 0;
 	}
 
+	/**
+	 * Returns <code>true</code> if value is a boolean.
+	 * 
+	 * @param value
+	 *            a value
+	 * @return <code>true</code> if value is a boolean
+	 */
 	public static boolean isBool(Object value) {
 		return (value instanceof Boolean);
 	}
 
+	/**
+	 * Returns <code>true</code> if value is a float.
+	 * 
+	 * @param value
+	 *            a value
+	 * @return <code>true</code> if value is a float
+	 */
 	public static boolean isFloat(Object value) {
 		return (value instanceof Float || value instanceof Double || value instanceof BigDecimal);
 	}
 
+	/**
+	 * Returns <code>true</code> if value is an integer.
+	 * 
+	 * @param value
+	 *            a value
+	 * @return <code>true</code> if value is an integer
+	 */
 	public static boolean isInt(Object value) {
 		return (value instanceof Integer || value instanceof Long || value instanceof BigInteger);
 	}
 
+	/**
+	 * Returns <code>true</code> if value is a list.
+	 * 
+	 * @param value
+	 *            a value
+	 * @return <code>true</code> if value is a list
+	 */
 	public static boolean isList(Object value) {
 		return value.getClass().isArray();
 	}
 
+	/**
+	 * Returns <code>true</code> if value is a boolean and is <code>true</code>
+	 * 
+	 * @param value
+	 *            a value
+	 * @return <code>true</code> if value is a boolean and is <code>true</code>
+	 */
 	public static boolean isTrue(Object value) {
 		return isBool(value) && (Boolean) value;
 	}
 
+	/**
+	 * Returns a new boolean that is <code>true</code> if val1 is less than or
+	 * equal to val2, or <code>null</code> if the two operands are not both of
+	 * the same type.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return a boolean value or <code>null</code>
+	 */
 	public static Object le(Object val1, Object val2) {
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
 		return getBigInteger(val1).compareTo(getBigInteger(val2)) <= 0;
 	}
 
+	/**
+	 * Returns a new boolean that is <code>true</code> if val1 is less than
+	 * val2, or <code>null</code> if the two operands are not both of the same
+	 * type.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return a boolean value or <code>null</code>
+	 */
 	public static Object lt(Object val1, Object val2) {
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
 		return getBigInteger(val1).compareTo(getBigInteger(val2)) < 0;
 	}
 
+	/**
+	 * Returns a new integer equal to the modulo of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object mod(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).mod(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.mod(bi2));
 	}
 
+	/**
+	 * Returns a new integer equal to the product of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object multiply(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).multiply(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.multiply(bi2));
 	}
 
+	/**
+	 * Returns a new integer equal to the bitwise or of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object or(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).or(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.or(bi2));
 	}
 
 	public static void set(Type type, Object array, List<Expression> indexes,
@@ -168,20 +357,82 @@ public class ValueUtil {
 
 	}
 
+	/**
+	 * Returns a new integer equal to the first operand left shifted by the
+	 * right operand, or <code>null</code> if the two operands are not both
+	 * integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object shiftLeft(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).shiftLeft(getIntValue(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		int i2 = getIntValue(val2);
+		if (bi1 == null) {
+			return null;
+		}
+		return getIntValue(getBigInteger(val1).shiftLeft(i2));
 	}
 
+	/**
+	 * Returns a new integer equal to the first operand right shifted by the
+	 * right operand, or <code>null</code> if the two operands are not both
+	 * integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object shiftRight(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).shiftRight(getIntValue(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		int i2 = getIntValue(val2);
+		if (bi1 == null) {
+			return null;
+		}
+		return getIntValue(getBigInteger(val1).shiftRight(i2));
 	}
 
+	/**
+	 * Returns a new integer equal to the difference of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object subtract(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).subtract(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.subtract(bi2));
 	}
 
+	/**
+	 * Returns a new integer equal to the bitwise xor of the two operands, or
+	 * <code>null</code> if the two operands are not both integers.
+	 * 
+	 * @param val1
+	 *            an object
+	 * @param val2
+	 *            an object
+	 * @return an integer value or <code>null</code>
+	 */
 	public static Object xor(Object val1, Object val2) {
-		return getIntValue(getBigInteger(val1).xor(getBigInteger(val2)));
+		BigInteger bi1 = getBigInteger(val1);
+		BigInteger bi2 = getBigInteger(val2);
+		if (bi1 == null || bi2 == null) {
+			return null;
+		}
+		return getIntValue(bi1.xor(bi2));
 	}
 
 }
