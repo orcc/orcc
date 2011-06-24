e309dbbd77549acca5c0c833d10a0d1592281841
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2011-06-24 17:41:50 +0000

    (misc) added TypeUtil class

---
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/ir/util/TypeUtil.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2011, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc.ir.util;
+
+import net.sf.orcc.ir.IrFactory;
+import net.sf.orcc.ir.Type;
+import net.sf.orcc.ir.TypeInt;
+import net.sf.orcc.ir.TypeList;
+import net.sf.orcc.ir.TypeUint;
+
+/**
+ * This class defines static utility methods to deal with types.
+ * 
+ * @author Matthieu Wipliez
+ * 
+ */
+public class TypeUtil {
+
+	/**
+	 * Returns the Least Upper Bound of the given types.
+	 * 
+	 * @param t1
+	 *            a type
+	 * @param t2
+	 *            another type
+	 * @return the Least Upper Bound of the given types
+	 */
+	public static Type getLub(Type t1, Type t2) {
+		if (t1 == null || t2 == null) {
+			return null;
+		}
+
+		if (t1.isBool() && t2.isBool()) {
+			return t1;
+		} else if (t1.isFloat() && t2.isFloat()) {
+			return t1;
+		} else if (t1.isString() && t2.isString()) {
+			return t1;
+		} else if (t1.isInt() && t2.isInt()) {
+			return IrFactory.eINSTANCE.createTypeInt(Math.max(
+					((TypeInt) t1).getSize(), ((TypeInt) t2).getSize()));
+		} else if (t1.isList() && t2.isList()) {
+			TypeList listType1 = (TypeList) t1;
+			TypeList listType2 = (TypeList) t2;
+			Type type = getLub(listType1.getType(), listType2.getType());
+			if (type != null) {
+				// only return a list when the underlying type is valid
+				int size = Math.max(listType1.getSize(), listType2.getSize());
+				return IrFactory.eINSTANCE.createTypeList(size, type);
+			}
+		} else if (t1.isUint() && t2.isUint()) {
+			return IrFactory.eINSTANCE.createTypeUint(Math.max(
+					((TypeUint) t1).getSize(), ((TypeUint) t2).getSize()));
+		} else if (t1.isInt() && t2.isUint()) {
+			int si = ((TypeInt) t1).getSize();
+			int su = ((TypeUint) t2).getSize();
+			if (si > su) {
+				return IrFactory.eINSTANCE.createTypeInt(si);
+			} else {
+				return IrFactory.eINSTANCE.createTypeInt(su + 1);
+			}
+		} else if (t1.isUint() && t2.isInt()) {
+			int su = ((TypeUint) t1).getSize();
+			int si = ((TypeInt) t2).getSize();
+			if (si > su) {
+				return IrFactory.eINSTANCE.createTypeInt(si);
+			} else {
+				return IrFactory.eINSTANCE.createTypeInt(su + 1);
+			}
+		}
+
+		return null;
+	}
+
+}
