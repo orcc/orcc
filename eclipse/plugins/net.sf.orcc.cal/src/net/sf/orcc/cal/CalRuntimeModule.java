37f598842f1fbb6e080e6d89fd1112f542ecfc3c
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2011-07-31 18:44:01 +0000

    (front-end) removed custom resource description because it messes up some Xtext features (like "Find references"...)

--- eclipse/plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/CalRuntimeModule.java
+++ eclipse/plugins/net.sf.orcc.cal/src/net/sf/orcc/cal/CalRuntimeModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, IETR/INSA of Rennes
+ * Copyright (c) 2010-2011, IETR/INSA of Rennes
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,6 @@ import org.eclipse.xtext.conversion.IValueConverterService;
 import org.eclipse.xtext.linking.ILinkingService;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.parser.antlr.ISyntaxErrorMessageProvider;
-import org.eclipse.xtext.resource.IResourceDescription;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -50,10 +49,6 @@ public class CalRuntimeModule extends net.sf.orcc.cal.AbstractCalRuntimeModule {
 	@Override
 	public Class<? extends IQualifiedNameProvider> bindIQualifiedNameProvider() {
 		return CalQualifiedNameProvider.class;
-	}
-
-	public Class<? extends IResourceDescription.Manager> bindIResourceDescription$Manager() {
-		return CalResourceDescriptionManager.class;
 	}
 
 	public Class<? extends ISyntaxErrorMessageProvider> bindISyntaxErrorMessageProvider() {
