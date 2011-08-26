84a337beac7ae4083ef9ea2ee30ef17ce1971c22
Hervé Yviquel <hyviquel@gmail.com>
2011-08-26 08:49:21 +0000

    (TTA) Stream_in: Transform std_logic to std_logic_vector(0 downto 0) to map with the generated files.

--- libraries/TTA/fifo/vhdl/stream_out_5.vhdl
+++ libraries/TTA/fifo/vhdl/stream_out_5.vhdl
@@ -35,8 +35,8 @@ entity stream_out_stream_out_status_V5 is
 
     -- external interface
     ext_data   : out std_logic_vector(31 downto 0);  -- the actual data to stream out
-    ext_status : in  std_logic_vector(8 downto 0);         -- status signal from outside
-    ext_dv     : out std_logic          -- datavalid signal
+    ext_status : in  std_logic_vector(8 downto 0);  -- status signal from outside
+    ext_dv     : out std_logic_vector(0 downto 0) -- datavalid signal
     );
 
 end stream_out_stream_out_status_V5;
@@ -45,7 +45,7 @@ end stream_out_stream_out_status_V5;
 architecture rtl of stream_out_stream_out_status_V5 is
   signal r1reg   : std_logic_vector(31 downto 0);
   signal datareg : std_logic_vector(31 downto 0);
-  signal dvreg   : std_logic;
+  signal dvreg   : std_logic_vector(0 downto 0);
 
 begin
   
@@ -53,21 +53,21 @@ begin
   begin  -- process regs
     if rstx = '0' then
       datareg <= (others => '0');
-      dvreg   <= '0';
+      dvreg   <= (others => '0');
     elsif clk'event and clk = '1' then
       if glock = '0' then
 
         -- reset the datavalid signal
-        dvreg <= '0';
+        dvreg <= (others => '0');
 
         if t1load = '1' then
           case t1opcode is
             when STREAM_OUT_V5 =>
               datareg <= t1data;
-              dvreg   <= '1';
+              dvreg   <= (0 => '1', others => '0');
               -- enable the datavalid signal for a while
-            --when STREAM_OUT_STATUS_V5 =>
-              --r1reg <= (0 => ext_status(0), 1 => ext_status(1), 2 => ext_status(2), 3 => ext_status(3), 4 => ext_status(4), 5 => ext_status(5), 6 => ext_status(6), 7 => ext_status(7), 8 => ext_status(8), others => '0');
+--            when STREAM_OUT_STATUS_V5 =>
+--              r1reg <= (0 => ext_status(0), 1 => ext_status(1), 2 => ext_status(2), 3 => ext_status(3), 4 => ext_status(4), 5 => ext_status(5), 6 => ext_status(6), 7 => ext_status(7), 8 => ext_status(8), others => '0');
               -- make the stream_out_status visible in the lsb
             when others => null;
           end case;
@@ -78,7 +78,7 @@ begin
   end process regs;
 
   --r1data  <= r1reg;
-  r1data <= (0 => ext_status(0), 1 => ext_status(1), 2 => ext_status(2), 3 => ext_status(3), 4 => ext_status(4), 5 => ext_status(5), 6 => ext_status(6), 7 => ext_status(7), 8 => ext_status(8), others => '0');
+  r1data   <= (0 => ext_status(0), 1 => ext_status(1), 2 => ext_status(2), 3 => ext_status(3), 4 => ext_status(4), 5 => ext_status(5), 6 => ext_status(6), 7 => ext_status(7), 8 => ext_status(8), others => '0');
   ext_data <= datareg;
   ext_dv   <= dvreg;
   
