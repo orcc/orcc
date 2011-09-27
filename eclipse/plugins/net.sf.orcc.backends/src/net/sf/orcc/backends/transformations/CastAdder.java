ffc519556f1ebde0e186d470d8f2b2eaa6acaf9a
Hervé Yviquel <hyviquel@gmail.com>
2011-09-27 15:22:29 +0000

    Fix CastAdder again...

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/CastAdder.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/CastAdder.java
@@ -399,7 +399,6 @@ public class CastAdder extends AbstractActorVisitor<Expression> {
 
 	private boolean isTypeReducer(OpBinary op) {
 		switch (op) {
-		case SHIFT_LEFT:
 		case SHIFT_RIGHT:
 		case MOD:
 			return true;
