9ccf39bee1f11a95dcf1d989d092c9013c12e93a
Gildas Cocherel <gildas.cocherel@laposte.net>
2011-10-07 08:02:09 +0000

    add new function for display

--- libraries/C/lib/orcc/src/display.c
+++ libraries/C/lib/orcc/src/display.c
@@ -34,6 +34,10 @@ static SDL_Overlay *m_overlay;
 
 static int init = 0;
 
+static int x, y , onclick = 0;
+static SDL_Rect rect;
+
+
 static void press_a_key(int code) {
 	char buf[2];
 	char *ptrBuff = NULL;
@@ -55,7 +59,7 @@ char displayYUV_getFlags(){
 static void displayYUV_setSize(int width, int height) {
 	printf("set display to %ix%i\n", width, height);
 
-	m_screen = SDL_SetVideoMode(width, height, 0, 0);
+	m_screen = SDL_SetVideoMode(width, height, 32, SDL_HWSURFACE);
 	if (m_screen == NULL) {
 		fprintf(stderr, "Couldn't set %ix%ix24 video mode: %s\n", width,
 				height, SDL_GetError());
@@ -75,10 +79,14 @@ static void displayYUV_setSize(int width, int height) {
 
 void displayYUV_displayPicture(unsigned char *pictureBufferY,
 		unsigned char *pictureBufferU, unsigned char *pictureBufferV,
-		unsigned short pictureWidth, unsigned short pictureHeight) {
-	static unsigned short lastWidth = 0;
-	static unsigned short lastHeight = 0;
-	SDL_Rect rect = { 0, 0, pictureWidth, pictureHeight };
+		unsigned int   pictureWidth, unsigned int pictureHeight) {
+	static unsigned int lastWidth = 0;
+	static unsigned int lastHeight = 0;
+	//SDL_Rect rect = { 0, 0, pictureWidth, pictureHeight };
+	rect.x = 0;
+	rect.y = 0;
+	rect.w = pictureWidth;
+	rect.h = pictureHeight;
 
 	SDL_Event event;
 
@@ -87,19 +95,18 @@ void displayYUV_displayPicture(unsigned char *pictureBufferY,
 		lastHeight = pictureHeight;
 		lastWidth = pictureWidth;
 	}
+
 	if (SDL_LockYUVOverlay(m_overlay) < 0) {
 		fprintf(stderr, "Can't lock screen: %s\n", SDL_GetError());
 		press_a_key(-1);
 	}
 
 	memcpy(m_overlay->pixels[0], pictureBufferY, pictureWidth * pictureHeight);
-	memcpy(m_overlay->pixels[1], pictureBufferV,
-			pictureWidth * pictureHeight / 4);
-	memcpy(m_overlay->pixels[2], pictureBufferU,
-			pictureWidth * pictureHeight / 4);
+	memcpy(m_overlay->pixels[1], pictureBufferV, pictureWidth * pictureHeight / 4);
+	memcpy(m_overlay->pixels[2], pictureBufferU, pictureWidth * pictureHeight / 4);
 
 	SDL_UnlockYUVOverlay(m_overlay);
-		SDL_DisplayYUVOverlay(m_overlay, &rect);
+	SDL_DisplayYUVOverlay(m_overlay, &rect);
 
 	/* Grab all the events off the queue. */
 	while (SDL_PollEvent(&event)) {
@@ -128,5 +135,92 @@ void displayYUV_init() {
 	}
 }
 
+static void displayYUV_setSize2(int winWidth, int winHeight, int pictureWidth, int pictureHeight) {
+	printf("set display to %ix%i\n", winWidth, winHeight);
+
+	m_screen = SDL_SetVideoMode(winWidth, winHeight, 32, SDL_HWSURFACE);
+	if (m_screen == NULL) {
+		fprintf(stderr, "Couldn't set %ix%ix24 video mode: %s\n", winWidth,
+				winHeight, SDL_GetError());
+		press_a_key(-1);
+	}
+
+	if (m_overlay != NULL) {
+		SDL_FreeYUVOverlay(m_overlay);
+	}
+
+	m_overlay = SDL_CreateYUVOverlay(pictureWidth, pictureHeight, SDL_YV12_OVERLAY, m_screen);
+	if (m_overlay == NULL) {
+		fprintf(stderr, "Couldn't create overlay: %s\n", SDL_GetError());
+		press_a_key(-1);
+	}
+}
+
+void displayYUV_initImage(int winWidth, int winHeight, int pictureWidth, int pictureHeight) {
+  if (!init) {
+    init = 1;
+    // First, initialize SDL's video subsystem.
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      fprintf(stderr, "Video initialization failed: %s\n", SDL_GetError());
+      press_a_key(-1);
+    }  
+    SDL_WM_SetCaption("display", NULL);
+    atexit(SDL_Quit);
+    displayYUV_setSize2(winWidth, winHeight, pictureWidth, pictureHeight);
+    x = 0;
+    y = 0;
+    onclick = 0;
+  }
+}
+
+
+void displayYUV_displayPicture2(unsigned char *pictureBufferY,
+		unsigned char *pictureBufferU, unsigned char *pictureBufferV,
+		unsigned int   pictureWidth, unsigned int pictureHeight) {
+  rect.x = x;
+  rect.y = y;
+  rect.w = pictureWidth;
+  rect.h = pictureHeight;
+  
+  if (SDL_LockYUVOverlay(m_overlay) < 0) {
+    fprintf(stderr, "Can't lock screen: %s\n", SDL_GetError());
+    press_a_key(-1);
+  }
+  
+  memcpy(m_overlay->pixels[0], pictureBufferY, pictureWidth * pictureHeight);
+  memcpy(m_overlay->pixels[1], pictureBufferV, pictureWidth * pictureHeight / 4);
+  memcpy(m_overlay->pixels[2], pictureBufferU, pictureWidth * pictureHeight / 4);
+  
+  SDL_UnlockYUVOverlay(m_overlay);
+  SDL_DisplayYUVOverlay(m_overlay, &rect);
+}
+
+void displayYUV_getEvent() {
+  SDL_Event event;
+  /* Grab all the events off the queue. */
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT:
+      SDL_FreeSurface(m_screen);
+      SDL_Quit();
+      exit(0);
+      break;
+    case SDL_MOUSEBUTTONDOWN : onclick = 1; break;
+    case SDL_MOUSEBUTTONUP   : onclick = 0; break;
+    case SDL_MOUSEMOTION     :
+      if (onclick == 1) {
+	x += event.motion.xrel;
+	y += event.motion.yrel;
+	rect.x = x ; rect.y = y;
+	SDL_FillRect(m_screen,NULL,SDL_MapRGB(m_screen->format,0,0,0));
+	SDL_Flip(m_screen);
+	SDL_DisplayYUVOverlay(m_overlay, &rect);
+      }
+      break;
+    default:
+      break;
+    }
+  }
+}
 
 
