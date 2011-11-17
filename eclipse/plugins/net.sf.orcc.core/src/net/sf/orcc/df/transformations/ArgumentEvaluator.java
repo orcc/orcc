6d030f846275baabd220ce125c70b48598e5d960
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2011-11-17 16:14:34 +0000

    (network) added evaluation of arguments. Works on AVC FRExt.

--- eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/df/transformations/ArgumentEvaluator.java
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/df/transformations/ArgumentEvaluator.java
@@ -28,7 +28,13 @@
  */
 package net.sf.orcc.df.transformations;
 
+import net.sf.orcc.df.Argument;
+import net.sf.orcc.df.Instance;
 import net.sf.orcc.df.Network;
+import net.sf.orcc.df.util.DfSwitch;
+import net.sf.orcc.ir.Var;
+import net.sf.orcc.ir.util.ExpressionEvaluator;
+import net.sf.orcc.ir.util.ValueUtil;
 
 /**
  * This class defines a transformation that evaluates all the arguments of all
@@ -37,10 +43,31 @@ import net.sf.orcc.df.Network;
  * @author Matthieu Wipliez
  * 
  */
-public class ArgumentEvaluator implements NetworkVisitor<Void> {
+public class ArgumentEvaluator extends DfSwitch<Void> {
 
 	@Override
-	public Void doSwitch(Network network) {
+	public Void caseInstance(Instance instance) {
+		for (Argument argument : instance.getArguments()) {
+			Var var = argument.getVariable();
+			Object value = new ExpressionEvaluator().doSwitch(argument.getValue());
+			var.setValue(new ExpressionEvaluator().doSwitch(argument.getValue()));
+			argument.setValue(ValueUtil.getExpression(value));
+		}
+
+		doSwitch(instance.getContents());
+		return null;
+	}
+
+	@Override
+	public Void caseNetwork(Network network) {
+		for (Var var : network.getVariables()) {
+			var.setValue(new ExpressionEvaluator().doSwitch(var
+					.getInitialValue()));
+		}
+
+		for (Instance instance : network.getInstances()) {
+			doSwitch(instance);
+		}
 		return null;
 	}
 
