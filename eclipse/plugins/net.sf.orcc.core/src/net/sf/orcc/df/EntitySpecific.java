cbe68283233914e57829e60d1d9cf57fcec2c671
Ghislain Roquier <ghislain.roquier@gmail.com>
2011-12-07 19:44:17 +0000

    oups, add missing file generated from the df model.

---
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/df/EntitySpecific.java
@@ -0,0 +1,21 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package net.sf.orcc.df;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Entity Specific</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see net.sf.orcc.df.DfPackage#getEntitySpecific()
+ * @model
+ * @generated
+ */
+public interface EntitySpecific extends Entity {
+} // EntitySpecific
