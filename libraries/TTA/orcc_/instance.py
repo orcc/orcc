8c04a379ab65954449c37c95ac5f793dbd5c210d
Hervé Yviquel <hyviquel@gmail.com>
2012-01-12 14:41:10 +0000

    (TTA) Don't print the mapping at the end of simulation anymore.

--- libraries/TTA/orcc_/instance.py
+++ libraries/TTA/orcc_/instance.py
@@ -157,7 +157,7 @@ class Instance:
                 shutil.copy(srcTrace, tgtTrace)
 
             # Launch the simulation
-            retcode = subprocess.call(["ttasim", "--no-debugmode", "-e", "run; set filename stats.txt; set fileId [open $filename w]; puts $fileId [info proc cycles]; puts $fileId [info proc stats]; puts $fileId [info proc mapping]; close $fileId; exit;", "-a", self._adfFile, "-p", self._tpefFile])
+            retcode = subprocess.call(["ttasim", "--no-debugmode", "-e", "run; set filename stats.txt; set fileId [open $filename w]; puts $fileId [info proc cycles]; puts $fileId [info proc stats]; close $fileId; exit;", "-a", self._adfFile, "-p", self._tpefFile])
 
             # Check generated data
             i = 0
