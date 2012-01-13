32b33c8d85a651a722f00edb8127e16803672086
Hervé Yviquel <hyviquel@gmail.com>
2012-01-13 00:23:21 +0000

    (TTA) Add Xilinx implementations of the wrappers.

---
+++ libraries/TTA/wrapper/xilinx/fifo.vhd
@@ -0,0 +1,135 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+-- synthesis translate_off
+library XilinxCoreLib;
+-- synthesis translate_on
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.all;
+
+entity fifo is
+  generic
+    (
+      size          : integer := 256;
+      width         : integer := 32;
+      widthu        : integer := 8;
+      device_family : string  := "virtex5"
+      );
+  port
+    (
+      wrreq  : in  std_logic;
+      rst_n  : in  std_logic;
+      clk    : in  std_logic;
+      rdreq  : in  std_logic;
+      data   : in  std_logic_vector(width-1 downto 0);
+      q      : out std_logic_vector(width-1 downto 0);
+      status : out std_logic_vector(31 downto 0)
+      );
+end fifo;
+
+
+architecture rtl_fifo of fifo is
+
+  signal status_i : std_logic_vector(31 downto 0) := (others => '0');
+  signal rst      : std_logic;
+  signal empty    : std_logic;
+
+begin
+
+  rst    <= not(rst_n);
+  status <= status_i;
+  
+  data_count : process(clk)
+  begin
+    if (clk'event and clk = '1') then
+      if ((wrreq = '1' and status_i(widthu) = '0') and (rdreq = '1' and empty = '0')) then
+        status_i(widthu-1 downto 0) <= status_i(widthu-1 downto 0);
+      else
+        if (wrreq = '1' and s_status(widthu) = '0') then
+          status_i(widthu-1 downto 0) <= status_i(widthu-1 downto 0) + 1;
+        end if;
+        if (rdreq = '1' and empty = '0') then
+          status_i(widthu-1 downto 0) <= status_i(widthu-1 downto 0) - 1;
+        end if;
+      end if;
+    end if;
+  end process data_count;
+
+  fifo_component : XilinxCoreLib.fifo_generator_v6_1(behavioral)
+    generic map(
+      c_has_int_clk                  => 0,
+      c_wr_response_latency          => 1,
+      c_rd_freq                      => 1,
+      c_has_srst                     => 0,
+      c_enable_rst_sync              => 1,
+      c_has_rd_data_count            => 0,
+      c_din_width                    => width,
+      c_has_wr_data_count            => 0,
+      c_full_flags_rst_val           => 1,
+      c_implementation_type          => 0,
+      c_family                       => "device_family",
+      c_use_embedded_reg             => 0,
+      c_has_wr_rst                   => 0,
+      c_wr_freq                      => 1,
+      c_use_dout_rst                 => 1,
+      c_underflow_low                => 0,
+      c_has_meminit_file             => 0,
+      c_has_overflow                 => 0,
+      c_preload_latency              => 0,
+      c_dout_width                   => width,
+      c_msgon_val                    => 1,
+      c_rd_depth                     => size,
+      c_default_value                => "BlankString",
+      c_mif_file_name                => "BlankString",
+      c_error_injection_type         => 0,
+      c_has_underflow                => 0,
+      c_has_rd_rst                   => 0,
+      c_has_almost_full              => 0,
+      c_has_rst                      => 1,
+      c_data_count_width             => widthu+1,
+      c_has_wr_ack                   => 0,
+      c_use_ecc                      => 0,
+      c_wr_ack_low                   => 0,
+      c_common_clock                 => 1,
+      c_rd_pntr_width                => widthu,
+      c_use_fwft_data_count          => 1,
+      c_has_almost_empty             => 0,
+      c_rd_data_count_width          => widthu+1,
+      c_enable_rlocs                 => 0,
+      c_wr_pntr_width                => widthu,
+      c_overflow_low                 => 0,
+      c_prog_empty_type              => 0,
+      c_optimization_mode            => 0,
+      c_wr_data_count_width          => widthu+1,
+      c_preload_regs                 => 1,
+      c_dout_rst_val                 => "0",
+      c_has_data_count               => 0,
+      c_prog_full_thresh_negate_val  => size-2,
+      c_wr_depth                     => size,
+      c_prog_empty_thresh_negate_val => 5,
+      c_prog_empty_thresh_assert_val => 4,
+      c_has_valid                    => 0,
+      c_init_wr_pntr_val             => 0,
+      c_prog_full_thresh_assert_val  => size-1,
+      c_use_fifo16_flags             => 0,
+      c_has_backup                   => 0,
+      c_valid_low                    => 0,
+      c_prim_fifo_type               => "512x36",
+      c_count_type                   => 0,
+      c_prog_full_type               => 0,
+      c_memory_type                  => 1)
+    port map (
+      clk   => clk,
+      rst   => rst,
+      din   => data,
+      wr_en => wrreq,
+      rd_en => rdreq,
+      dout  => q,
+      full  => status_i(widthu),
+      empty => empty
+      );
+  
+end rtl_fifo;
---
+++ libraries/TTA/wrapper/xilinx/ram.vhd
@@ -0,0 +1,106 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- synthesis translate_off
+library XilinxCoreLib;
+-- synthesis translate_on
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+
+entity dram is
+  generic
+    (
+      size          : integer;
+      width         : integer := 32;
+      widthu        : integer;
+      init_file     : string;
+      device_family : string  := "virtex5"
+      );
+  port
+    (
+      address : in  std_logic_vector (widthu-1 downto 0);
+      byteena : in  std_logic_vector (3 downto 0) := (others => '1');
+      clock   : in  std_logic;
+      data    : in  std_logic_vector (width-1 downto 0);
+      wren    : in  std_logic;
+      q       : out std_logic_vector (width-1 downto 0)
+      );
+end dram;
+
+architecture rtl_dram of dram is
+
+  signal bytemask_i  : std_logic_vector(3 downto 0);
+  signal bytemask_i2 : std_logic_vector(3 downto 0);
+
+begin
+
+  bytemask_i2 <= wren & wren & wren & wren;
+  bytemask_i  <= bytemask_i2 and byteena;
+
+  ram_component : entity XilinxCoreLib.blk_mem_gen_v4_1(behavioral)
+    generic map(
+      c_has_regceb                => 0,
+      c_has_regcea                => 0,
+      c_mem_type                  => 0,
+      c_rstram_b                  => 0,
+      c_rstram_a                  => 0,
+      c_has_injecterr             => 0,
+      c_rst_type                  => "SYNC",
+      c_prim_type                 => 1,
+      c_read_width_b              => width,
+      c_initb_val                 => "0",
+      c_family                    => "virtex5",
+      c_read_width_a              => width,
+      c_disable_warn_bhv_coll     => 0,
+      c_use_softecc               => 0,
+      c_write_mode_b              => "WRITE_FIRST",
+      c_init_file_name            => init_file,
+      c_write_mode_a              => "WRITE_FIRST",
+      c_mux_pipeline_stages       => 0,
+      c_has_softecc_output_regs_b => 0,
+      c_has_softecc_output_regs_a => 0,
+      c_has_mem_output_regs_b     => 0,
+      c_has_mem_output_regs_a     => 0,
+      c_load_init_file            => 1,
+      c_xdevicefamily             => device_family,
+      c_write_depth_b             => size,
+      c_write_depth_a             => size,
+      c_has_rstb                  => 0,
+      c_has_rsta                  => 0,
+      c_has_mux_output_regs_b     => 0,
+      c_inita_val                 => "0",
+      c_has_mux_output_regs_a     => 0,
+      c_addra_width               => widthu,
+      c_has_softecc_input_regs_b  => 0,
+      c_has_softecc_input_regs_a  => 0,
+      c_addrb_width               => widthu,
+      c_default_data              => "0",
+      c_use_ecc                   => 0,
+      c_algorithm                 => 1,
+      c_disable_warn_bhv_range    => 0,
+      c_write_width_b             => width,
+      c_write_width_a             => width,
+      c_read_depth_b              => size,
+      c_read_depth_a              => size,
+      c_byte_size                 => 8,
+      c_sim_collision_check       => "ALL",
+      c_common_clk                => 0,
+      c_wea_width                 => 4,
+      c_has_enb                   => 0,
+      c_web_width                 => 4,
+      c_has_ena                   => 0,
+      c_use_byte_web              => 1,
+      c_use_byte_wea              => 1,
+      c_rst_priority_b            => "CE",
+      c_rst_priority_a            => "CE",
+      c_use_default_data          => 0);
+  port map (
+    clka  => clock,
+    wea   => bytemask_i,
+    addra => address,
+    dina  => data,
+    douta => q);
+
+
+end rtl_dram;
