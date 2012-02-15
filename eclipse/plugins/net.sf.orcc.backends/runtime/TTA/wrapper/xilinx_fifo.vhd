34542895790250baeaa2534e8bd59a3f64785053
Hervé Yviquel <hyviquel@gmail.com>
2012-02-15 17:20:26 +0000

    (TTA) Update the Xilinx wrapper of FIFO.

--- eclipse/plugins/net.sf.orcc.backends/runtime/TTA/wrapper/xilinx_fifo.vhd
+++ eclipse/plugins/net.sf.orcc.backends/runtime/TTA/wrapper/xilinx_fifo.vhd
@@ -34,6 +34,18 @@ end fifo;
 
 architecture rtl_fifo of fifo is
 
+  component fifo_br
+    port (
+      clk   : in  std_logic;
+      rst   : in  std_logic;
+      din   : in  std_logic_vector(31 downto 0);
+      wr_en : in  std_logic;
+      rd_en : in  std_logic;
+      dout  : out std_logic_vector(31 downto 0);
+      full  : out std_logic;
+      empty : out std_logic);
+  end component;
+
   signal nb_tokens_i : std_logic_vector(31 downto 0) := (others => '0');
   signal rst         : std_logic;
   signal empty       : std_logic;
@@ -50,7 +62,7 @@ begin
       if ((wrreq = '1' and nb_tokens_i(widthu) = '0') and (rdreq = '1' and empty = '0')) then
         nb_tokens_i(widthu-1 downto 0) <= nb_tokens_i(widthu-1 downto 0);
       else
-        if (wrreq = '1' and s_status(widthu) = '0') then
+        if (wrreq = '1' and nb_tokens_i(widthu) = '0') then
           nb_tokens_i(widthu-1 downto 0) <= nb_tokens_i(widthu-1 downto 0) + 1;
         end if;
         if (rdreq = '1' and empty = '0') then
@@ -60,69 +72,8 @@ begin
     end if;
   end process token_count;
 
-  fifo_component : XilinxCoreLib.fifo_generator_v6_1(behavioral)
-    generic map(
-      c_has_int_clk                  => 0,
-      c_wr_response_latency          => 1,
-      c_rd_freq                      => 1,
-      c_has_srst                     => 0,
-      c_enable_rst_sync              => 1,
-      c_has_rd_data_count            => 0,
-      c_din_width                    => width,
-      c_has_wr_data_count            => 0,
-      c_full_flags_rst_val           => 1,
-      c_implementation_type          => 0,
-      c_family                       => device_family,
-      c_use_embedded_reg             => 0,
-      c_has_wr_rst                   => 0,
-      c_wr_freq                      => 1,
-      c_use_dout_rst                 => 1,
-      c_underflow_low                => 0,
-      c_has_meminit_file             => 0,
-      c_has_overflow                 => 0,
-      c_preload_latency              => 0,
-      c_dout_width                   => width,
-      c_msgon_val                    => 1,
-      c_rd_depth                     => size,
-      c_default_value                => "BlankString",
-      c_mif_file_name                => "BlankString",
-      c_error_injection_type         => 0,
-      c_has_underflow                => 0,
-      c_has_rd_rst                   => 0,
-      c_has_almost_full              => 0,
-      c_has_rst                      => 1,
-      c_data_count_width             => widthu+1,
-      c_has_wr_ack                   => 0,
-      c_use_ecc                      => 0,
-      c_wr_ack_low                   => 0,
-      c_common_clock                 => 1,
-      c_rd_pntr_width                => widthu,
-      c_use_fwft_data_count          => 1,
-      c_has_almost_empty             => 0,
-      c_rd_data_count_width          => widthu+1,
-      c_enable_rlocs                 => 0,
-      c_wr_pntr_width                => widthu,
-      c_overflow_low                 => 0,
-      c_prog_empty_type              => 0,
-      c_optimization_mode            => 0,
-      c_wr_data_count_width          => widthu+1,
-      c_preload_regs                 => 1,
-      c_dout_rst_val                 => "0",
-      c_has_data_count               => 0,
-      c_prog_full_thresh_negate_val  => size-2,
-      c_wr_depth                     => size,
-      c_prog_empty_thresh_negate_val => 5,
-      c_prog_empty_thresh_assert_val => 4,
-      c_has_valid                    => 0,
-      c_init_wr_pntr_val             => 0,
-      c_prog_full_thresh_assert_val  => size-1,
-      c_use_fifo16_flags             => 0,
-      c_has_backup                   => 0,
-      c_valid_low                    => 0,
-      c_prim_fifo_type               => "512x36",
-      c_count_type                   => 0,
-      c_prog_full_type               => 0,
-      c_memory_type                  => 1)
+
+  fifo_component : fifo_br
     port map (
       clk   => clk,
       rst   => rst,
@@ -131,7 +82,5 @@ begin
       rd_en => rdreq,
       dout  => q,
       full  => nb_tokens_i(widthu),
-      empty => empty
-      );
-
+      empty => empty);
 end rtl_fifo;
