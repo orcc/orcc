accedcf7306c6caabc6997c6edf67cefbf5069ce
Richard Thavot <rthavot@epfl.ch>
2012-04-03 06:50:19 +0000

    (Ir Transformations) DeadProcedureElimination: This class removes all unused procedures/functions

---
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/ir/transformations/DeadProcedureElimination.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2009, IETR/INSA of Rennes and EFPL
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes and EPFL nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+package net.sf.orcc.ir.transformations;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import net.sf.orcc.df.Action;
+import net.sf.orcc.df.Actor;
+import net.sf.orcc.ir.InstCall;
+import net.sf.orcc.ir.Procedure;
+import net.sf.orcc.ir.util.AbstractActorVisitor;
+
+/**
+ * This class removes all unused procedures/functions
+ * 
+ * @author Matthieu Wipliez
+ * @author Richard Thavot
+ *
+ */
+public class DeadProcedureElimination extends AbstractActorVisitor<Object> {
+
+	private Map<Procedure, List<InstCall>> procToCalledInstsMap;
+	private Map<Procedure, List<InstCall>> procToUsedInstsMap;
+
+	public DeadProcedureElimination() {
+		procToCalledInstsMap = new HashMap<Procedure, List<InstCall>>();
+		procToUsedInstsMap = new HashMap<Procedure, List<InstCall>>();
+	}
+
+	private DeadProcedureElimination(
+			Map<Procedure, List<InstCall>> procToCalledInstsMap,
+			Map<Procedure, List<InstCall>> procToUsedInstsMap) {
+		this.procToCalledInstsMap = procToCalledInstsMap;
+		this.procToUsedInstsMap = procToUsedInstsMap;
+	}
+
+	@Override
+	public Object caseActor(Actor actor) {
+
+		// 1. Visit all InstCall define inside actions
+		for (Action initialize : actor.getInitializes()) {
+			super.doSwitch(initialize);
+		}
+		for (Action action : actor.getActions()) {
+			super.doSwitch(action);
+		}
+
+		// 2 Remove uncalled procedure
+		Set<Procedure> removeProcSet = new HashSet<Procedure>();
+		for (Procedure procedure : actor.getProcs()) {
+			if (!procedure.isNative()) {
+				List<InstCall> instCalls = procToCalledInstsMap.get(procedure);
+				if (instCalls == null) {
+					removeProcSet.add(procedure);
+				} 
+			}
+		}
+		actor.getProcs().removeAll(removeProcSet);
+
+		return null;
+	}
+
+	@Override
+	public Object caseProcedure(Procedure procedure) {
+		procToUsedInstsMap.put(procedure, new ArrayList<InstCall>());
+		this.procedure = procedure;
+		return doSwitch(procedure.getNodes());
+	}
+
+	@Override
+	public Object caseInstCall(InstCall call) {
+		procToUsedInstsMap.get(procedure).add(call);
+		Procedure calledProc = call.getProcedure();
+		List<InstCall> instCalls = procToCalledInstsMap.get(calledProc);
+		if (instCalls == null) {
+			new DeadProcedureElimination(procToCalledInstsMap,
+					procToUsedInstsMap).doSwitch(calledProc);
+			instCalls = new ArrayList<InstCall>();
+			procToCalledInstsMap.put(calledProc, instCalls);
+		}
+		instCalls.add(call);
+
+		return null;
+	}
+
+}
