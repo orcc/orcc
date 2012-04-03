d80a88d792ddfae092e7b280fbccf99b4ad4b5ad
Richard Thavot <rthavot@epfl.ch>
2012-04-03 05:55:46 +0000

    (Backend Transformations) Inliner improvement : possibility to inline a Procedure/Function Call, or a list of Procedure/Function calls, or all Procedure/Function Calls contained into an actor.

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/Inliner.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/transformations/Inliner.java
@@ -29,223 +29,309 @@
 package net.sf.orcc.backends.transformations;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import net.sf.dftools.util.util.EcoreHelper;
+import net.sf.orcc.df.Actor;
 import net.sf.orcc.ir.Arg;
 import net.sf.orcc.ir.ArgByRef;
 import net.sf.orcc.ir.ArgByVal;
-import net.sf.orcc.ir.Def;
 import net.sf.orcc.ir.ExprVar;
 import net.sf.orcc.ir.Expression;
 import net.sf.orcc.ir.InstAssign;
 import net.sf.orcc.ir.InstCall;
+import net.sf.orcc.ir.InstLoad;
 import net.sf.orcc.ir.InstReturn;
+import net.sf.orcc.ir.InstStore;
 import net.sf.orcc.ir.IrFactory;
 import net.sf.orcc.ir.Node;
 import net.sf.orcc.ir.NodeBlock;
+import net.sf.orcc.ir.NodeIf;
+import net.sf.orcc.ir.NodeWhile;
+import net.sf.orcc.ir.Param;
 import net.sf.orcc.ir.Procedure;
 import net.sf.orcc.ir.Type;
-import net.sf.orcc.ir.Use;
 import net.sf.orcc.ir.Var;
 import net.sf.orcc.ir.util.AbstractActorVisitor;
 import net.sf.orcc.ir.util.IrUtil;
 
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.emf.ecore.util.EcoreUtil.Copier;
 
 /**
- * This class defines an actor transformation that inline the functions and/or
- * the procedures
+ * This class defines an actor transformation that inline function(s) and/or
+ * procedure(s)
  * 
  * @author Herve Yviquel
  * @author Matthieu Wipliez
+ * @author Thavot Richard
  * 
  */
 public class Inliner extends AbstractActorVisitor<Object> {
 
-	private boolean inlineFunction;
+	private class CallGetter extends AbstractActorVisitor<Object> {
 
-	private boolean inlineProcedure;
+		private boolean inlineFunction;
+		private boolean inlineProcedure;
 
-	protected Map<Var, Var> variableToLocalVariableMap;
+		private List<InstCall> instCallList = new ArrayList<InstCall>();
 
-	public Inliner(boolean inlineProcedure, boolean inlineFunction) {
-		this.inlineProcedure = inlineProcedure;
-		this.inlineFunction = inlineFunction;
-	}
-
-	@Override
-	public Object caseInstCall(InstCall call) {
-		// Function case
-		Procedure procedure = call.getProcedure();
-		Type returnType = procedure.getReturnType();
-
-		if (returnType.isVoid() && inlineProcedure || !returnType.isVoid()
-				&& inlineFunction) {
-			inline(call);
+		public CallGetter(boolean inlineProcedure, boolean inlineFunction) {
+			this.inlineProcedure = inlineProcedure;
+			this.inlineFunction = inlineFunction;
 		}
 
+		@Override
+		public Object caseInstCall(InstCall call) {
+			// Function case
+			Procedure procedure = call.getProcedure();
+			Type returnType = procedure.getReturnType();
+
+			if (returnType.isVoid() && inlineProcedure || !returnType.isVoid()
+					&& inlineFunction) {
+				instCallList.add(call);
+			}
+			return null;
+		}
+
+		public List<InstCall> getCalls() {
+			return instCallList;
+		}
+	}
+
+	private CallGetter callGetter;
+	private List<InstCall> instCallList;
+
+	private InstCall currentCall;
+	private Map<Var, Var> localToLocalsMap;
+	private InstReturn instReturn;
+
+	public Inliner(boolean inlineProcedure, boolean inlineFunction) {
+		callGetter = new CallGetter(inlineProcedure, inlineFunction);
+	}
+
+	/**
+	 * Inline the procedure/function
+	 * 
+	 * @param call
+	 */
+	public Inliner(InstCall call) {
+		this.instCallList = new ArrayList<InstCall>();
+		this.instCallList.add(call);
+	}
+
+	/**
+	 * Inline the procedure(s)/function(s)
+	 * 
+	 * @param calls
+	 */
+	public Inliner(List<InstCall> calls) {
+		this.instCallList = new ArrayList<InstCall>();
+	}
+
+	public Object caseActor(Actor actor) {
+		if (callGetter != null) {
+			callGetter.doSwitch(actor);
+			this.instCallList = callGetter.getCalls();
+		}
+		for (InstCall call : instCallList) {
+			if (!call.getProcedure().isNative()) {
+				this.currentCall = call;
+				inlineProcedure();
+			}
+		}
 		return null;
 	}
 
 	/**
-	 * Clones the given nodes and replace every def/use with a def/use of the
-	 * variable mapped in the variableToLocalVariableMap.
-	 * 
-	 * @param nodes
-	 *            a collection of Nodes
-	 * @return a collection of cloned Nodes
+	 * Inline the currentCall;
 	 */
-	private Collection<Node> cloneNodes(Collection<Node> nodes) {
-		Copier copier = new Copier();
-		Collection<Node> clonedNodes = copier.copyAll(nodes);
-		copier.copyReferences();
-
-		TreeIterator<EObject> it = EcoreUtil.getAllContents(nodes);
-		while (it.hasNext()) {
-			EObject object = it.next();
-
-			if (object instanceof Def) {
-				Def def = (Def) object;
-				Def copyDef = (Def) copier.get(def);
-				Var oldVar = def.getVariable();
-				Var newVar = oldVar.isLocal() ? variableToLocalVariableMap
-						.get(oldVar) : oldVar;
-				copyDef.setVariable(newVar);
-			} else if (object instanceof Use) {
-				Use use = (Use) object;
-				Use copyUse = (Use) copier.get(use);
-				Var oldVar = use.getVariable();
-				Var newVar = oldVar.isLocal() ? variableToLocalVariableMap
-						.get(oldVar) : oldVar;
-				copyUse.setVariable(newVar);
-			}
-		}
-
-		return clonedNodes;
-	}
-
-	/**
-	 * Inline the called function at call location. All variables of the inlined
-	 * function are copied in the local procedure.
-	 * 
-	 * @param call
-	 *            a call instruction to inline
-	 */
-	private void inline(InstCall call) {
-		// The function or the procedure
-		Procedure function = call.getProcedure();
-
-		// Set the function/procedure to external thus it will not be printed
-		function.setNative(true);
-
-		// Create a new local variable to all function/procedure's variable
-		// except for list (reference is using)
-		variableToLocalVariableMap = new HashMap<Var, Var>();
-		for (Var var : function.getLocals()) {
-			Var newVar = procedure.newTempLocalVariable(var.getType(),
+	private void inlineProcedure() {
+		// 1. Clone procedure
+		Procedure calledProc = IrUtil.copy(currentCall.getProcedure());
+		// 2. Get current procedure
+		Procedure callerProc = EcoreHelper.getContainerOfType(currentCall,
+				Procedure.class);
+		// 3.1 Create a new local variable
+		localToLocalsMap = new HashMap<Var, Var>();
+		for (Var var : calledProc.getLocals()) {
+			Var newVar = callerProc.newTempLocalVariable(var.getType(),
 					"inlined_" + var.getName());
 			newVar.setIndex(var.getIndex());
 			newVar.setLineNumber(var.getLineNumber());
 			newVar.setAssignable(var.isAssignable());
-			variableToLocalVariableMap.put(var, newVar);
+			localToLocalsMap.put(var, newVar);
 		}
-		for (int i = 0; i < function.getParameters().size(); i++) {
-			Var newVar;
-			Var var = function.getParameters().get(i).getVariable();
-			if (var.getType().isList()) {
-				// In case of list, the parameter could be a global variable
-				Arg arg = call.getParameters().get(i);
-				if (arg.isByRef()) {
-					newVar = ((ArgByRef) arg).getUse().getVariable();
-				} else {
-					// TOFIX: probably wrong...
-					newVar = ((ExprVar) ((ArgByVal) arg).getValue()).getUse()
-							.getVariable();
-				}
+		// 3.2
+		int i = 0;
+		NodeBlock assignBlock = IrFactory.eINSTANCE.createNodeBlock();
+		for (Arg arg : currentCall.getParameters()) {
+			Param param = calledProc.getParameters().get(i);
+			if (arg.isByRef()) {
+				ArgByRef ref = (ArgByRef) arg;
+				localToLocalsMap.put(param.getVariable(), ref.getUse()
+						.getVariable());
 			} else {
-				newVar = procedure.newTempLocalVariable(var.getType(),
-						"inlined_" + var.getName());
-				newVar.setIndex(var.getIndex());
-				newVar.setLineNumber(var.getLineNumber());
-				newVar.setAssignable(var.isAssignable());
-
-			}
-			variableToLocalVariableMap.put(var, newVar);
-		}
-
-		// Assign all parameters except for list
-		NodeBlock parametersBlock = IrFactory.eINSTANCE.createNodeBlock();
-		for (int i = 0; i < function.getParameters().size(); i++) {
-			Var parameter = function.getParameters().get(i).getVariable();
-			if (!parameter.getType().isList()) {
-				Arg arg = call.getParameters().get(i);
-				if (arg.isByVal()) {
-					Expression expr = ((ArgByVal) arg).getValue();
-					InstAssign assign = IrFactory.eINSTANCE.createInstAssign(
-							variableToLocalVariableMap.get(parameter),
-							IrUtil.copy(expr));
-					parametersBlock.add(assign);
+				ArgByVal val = (ArgByVal) arg;
+				if (val.getValue().isExprVar()) {
+					Var var = ((ExprVar) val.getValue()).getUse().getVariable();
+					localToLocalsMap.put(param.getVariable(), var);
+				} else {
+					Var newVar = callerProc.newTempLocalVariable(param
+							.getVariable().getType(), "inlined_"
+							+ param.getVariable().getName());
+					newVar.setAssignable(true);
+					localToLocalsMap.put(param.getVariable(), newVar);
+					InstAssign instAssign = IrFactory.eINSTANCE
+							.createInstAssign(newVar, val.getValue());
+					assignBlock.add(instAssign);
 				}
 			}
+			i++;
 		}
-
-		// Clone function/procedure body
-		Collection<Node> clonedNodes = cloneNodes(function.getNodes());
-		transformInstReturn(clonedNodes, call.getTarget());
-
-		// Cut the block containing call instruction in two parts to put
-		// inlined nodes between them
-		NodeBlock currentBlock = EcoreHelper.getContainerOfType(call,
-				NodeBlock.class);
+		// 4. Rename local variable
+		super.doSwitch(calledProc);
+		// 5. Transform return
+		if ((currentCall.getTarget() == null)
+				| calledProc.getReturnType().isVoid()) {
+			IrUtil.delete(instReturn);
+		} else {
+			InstAssign instAssign = IrFactory.eINSTANCE.createInstAssign(
+					currentCall.getTarget().getVariable(),
+					IrUtil.copy(instReturn.getValue()));
+			EcoreUtil.replace(instReturn, instAssign);
+		}
+		// 6. Cut the block containing call instruction in two parts
+		NodeBlock beginningBlock = currentCall.getBlock();
 		NodeBlock followingBlock = IrFactory.eINSTANCE.createNodeBlock();
-		while (indexInst < currentBlock.getInstructions().size()) {
-			followingBlock.add(currentBlock.getInstructions().get(indexInst));
+		indexInst = 0;
+		while (indexInst < beginningBlock.getInstructions().size()) {
+			if (beginningBlock.getInstructions().get(indexInst)
+					.equals(currentCall))
+				break;
+			indexInst++;
 		}
+		while (indexInst < beginningBlock.getInstructions().size()) {
+			followingBlock.add(beginningBlock.getInstructions().get(indexInst));
+		}
+		// 7. Add all inlined blocks
+		List<Node> nodes = findNode(callerProc.getNodes(), beginningBlock);
+		indexNode = nodes.indexOf(beginningBlock);
+		List<Node> inlined = calledProc.getNodes();
+		if (!assignBlock.getInstructions().isEmpty()) {
+			inlined.add(0, assignBlock);
+		}
+		inlined.add(followingBlock);
+		nodes.addAll(indexNode + 1, inlined);
+		// 8.
+		IrUtil.delete(currentCall);
+	}
 
-		// Add all inlined blocks
-		Collection<Node> inlinedNodes = new ArrayList<Node>();
-		inlinedNodes.add(parametersBlock);
-		inlinedNodes.addAll(clonedNodes);
-		inlinedNodes.add(followingBlock);
+	@Override
+	public Object caseInstAssign(InstAssign assign) {
+		// Replace the local variable name by visiting caseExprVar
+		super.doSwitch(assign.getValue());
+		Var var = assign.getTarget().getVariable();
+		if (localToLocalsMap.containsKey(var)) {
+			assign.getTarget().setVariable(localToLocalsMap.get(var));
+		}
+		return null;
+	}
 
-		List<Node> currentNodes = EcoreHelper.getContainingList(currentBlock);
-		currentNodes.addAll(indexNode + 1, inlinedNodes);
+	@Override
+	public Object caseInstLoad(InstLoad load) {
+		// Replace the local variable name by visiting caseExprVar
+		Var var = load.getTarget().getVariable();
+		if (localToLocalsMap.containsKey(var)) {
+			load.getTarget().setVariable(localToLocalsMap.get(var));
+		}
+		var = load.getSource().getVariable();
+		if (localToLocalsMap.containsKey(var)) {
+			load.getSource().setVariable(localToLocalsMap.get(var));
+		}
+		for(Expression e : load.getIndexes()){
+			super.doSwitch(e);
+		}
+		return null;
+	}
 
-		// Remove useless call instruction
-		IrUtil.delete(call);
+	@Override
+	public Object caseInstStore(InstStore store) {
+		// Replace the local variable name by visiting caseExprVar
+		super.doSwitch(store.getValue());
+		Var var = store.getTarget().getVariable();
+		if (localToLocalsMap.containsKey(var)) {
+			store.getTarget().setVariable(localToLocalsMap.get(var));
+		}
+		for(Expression e : store.getIndexes()){
+			super.doSwitch(e);
+		}
+		return null;
+	}
+
+	@Override
+	public Object caseInstReturn(InstReturn instReturn) {
+		// Replace the local variable name by visiting caseExprVar
+		super.doSwitch(instReturn.getValue());
+		this.instReturn = instReturn;
+		return null;
+	}
+
+	@Override
+	public Object caseNodeIf(NodeIf nodeif) {
+		// Replace the local variable name by visiting caseExprVar
+		super.doSwitch(nodeif.getCondition());
+		super.caseNodeIf(nodeif);
+		return null;
+	}
+
+	@Override
+	public Object caseNodeWhile(NodeWhile nodeWhile) {
+		// Replace the local variable name by visiting caseExprVar
+		super.doSwitch(nodeWhile.getCondition());
+		super.caseNodeWhile(nodeWhile);
+		return null;
 	}
 
 	/**
-	 * Replace the return instruction by an assignment to the given variable
-	 * 
-	 * @param nodes
-	 *            a collection of Nodes
-	 * @param callTarget
-	 *            a variable
+	 * Replace the local variable name if a reference already exists to a global
+	 * variable
 	 */
-	private void transformInstReturn(Collection<Node> nodes, Def callTarget) {
-		TreeIterator<EObject> it = EcoreUtil.getAllContents(nodes);
-		while (it.hasNext()) {
-			EObject object = it.next();
+	@Override
+	public Object caseExprVar(ExprVar exprVar) {
+		Var var = exprVar.getUse().getVariable();
+		if (localToLocalsMap.containsKey(var)) {
+			exprVar.getUse().setVariable(localToLocalsMap.get(var));
+		}
+		return null;
+	}
 
-			if (object instanceof InstReturn) {
-				InstReturn instReturn = (InstReturn) object;
-				if (callTarget != null) {
-					InstAssign instAssign = IrFactory.eINSTANCE
-							.createInstAssign(callTarget.getVariable(),
-									IrUtil.copy(instReturn.getValue()));
-					EcoreUtil.replace(instReturn, instAssign);
+	/**
+	 * Find the given node into the given nodes location.
+	 * 
+	 * @param locationNodes
+	 * @param nodeToFind
+	 * @return
+	 */
+	public List<Node> findNode(List<Node> locationNodes, Node nodeToFind) {
+		if (locationNodes.contains(nodeToFind)) {
+			return locationNodes;
+		} else {
+			List<Node> n = null;
+			for (Node node : locationNodes) {
+				if (node.isNodeIf()) {
+					n = findNode(((NodeIf) node).getElseNodes(), nodeToFind);
+					if (n == null)
+						n = findNode(((NodeIf) node).getThenNodes(), nodeToFind);
+				} else if (node.isNodeWhile()) {
+					n = findNode(((NodeWhile) node).getNodes(), nodeToFind);
 				}
-				IrUtil.delete(instReturn);
+				if (n != null)
+					return n;
 			}
 		}
+		return null;
 	}
 
 }
