867f1c94502bea56205b9b464707d15019836f94
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2012-04-15 17:19:16 +0000

    (simulators) corrected compile error

--- eclipse/plugins/net.sf.orcc.simulators/src/net/sf/orcc/simulators/JadeSimulatorImpl.java
+++ eclipse/plugins/net.sf.orcc.simulators/src/net/sf/orcc/simulators/JadeSimulatorImpl.java
@@ -118,7 +118,7 @@ public class JadeSimulatorImpl extends AbstractSimulator {
 		ResourceSet set = new ResourceSetImpl();
 		Network network = EcoreHelper.getEObject(set, xdfFile);
 		// instantiate networks (but not actors) and flattens network
-		network = new Instantiator(true).doSwitch(network);
+		network = new Instantiator().doSwitch(network);
 		new NetworkFlattener().doSwitch(network);
 
 		xdfFlattenFile = new File(vtlFolder, network.getSimpleName() + ".xdf");
