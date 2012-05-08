e8cfaeb0ff01ce8c653de0f594f4e8ac5a9d3787
Hervé Yviquel <hyviquel@gmail.com>
2012-05-08 20:52:50 +0000

    (TTA) Update RAM to support byteenable.

--- eclipse/plugins/net.sf.orcc.backends/runtime/TTA/wrapper/inferredRAM_2p.vhd
+++ eclipse/plugins/net.sf.orcc.backends/runtime/TTA/wrapper/inferredRAM_2p.vhd
@@ -53,22 +53,25 @@ library work;
 
 entity inferredRAM_2p is
   generic (
-    depth   : integer;
-    width   : integer;
-    widtha  : integer;
-    initVal : integer := 0);
+    depth      : integer;
+    addr_width : integer;
+    byte_width : integer;
+    bytes      : integer;
+    initVal    : integer := 0);
   port (
     clk        : in  std_logic;
     --
     wren_p1    : in  std_logic;
-    address_p1 : in  std_logic_vector(widtha -1 downto 0);
-    data_p1    : in  std_logic_vector(width -1 downto 0);
-    q_p1       : out std_logic_vector(width -1 downto 0);
+    address_p1 : in  std_logic_vector(addr_width-1 downto 0);
+    byteen_p1  : in  std_logic_vector(bytes-1 downto 0);
+    data_p1    : in  std_logic_vector(bytes*byte_width-1 downto 0);
+    queue_p1   : out std_logic_vector(bytes*byte_width-1 downto 0);
     --
     wren_p2    : in  std_logic;
-    address_p2 : in  std_logic_vector(widtha -1 downto 0);
-    data_p2    : in  std_logic_vector(width -1 downto 0);
-    q_p2       : out std_logic_vector(width -1 downto 0));
+    address_p2 : in  std_logic_vector(addr_width-1 downto 0);
+    byteen_p2  : in  std_logic_vector(bytes-1 downto 0);
+    data_p2    : in  std_logic_vector(bytes*byte_width-1 downto 0);
+    queue_p2   : out std_logic_vector(bytes*byte_width-1 downto 0));
 end inferredRAM_2p;
 
 -------------------------------------------------------------------------------
@@ -76,50 +79,77 @@ end inferredRAM_2p;
 
 architecture arch_inferredRAM_2p of inferredRAM_2p is
 
-
   -----------------------------------------------------------------------------
   -- Internal type declarations
   -----------------------------------------------------------------------------
-
-  type ram_type is array (0 to depth -1) of
-    std_logic_vector(width -1 downto 0);
+  
+  type word_type is array (0 to bytes -1) of std_logic_vector(byte_width-1 downto 0);
+  type ram_type is array (0 to depth -1) of word_type;
 
   -----------------------------------------------------------------------------
   -- Internal signal declarations
   -----------------------------------------------------------------------------
-  shared variable ram : ram_type := (others => std_logic_vector(to_signed(initVal, width)));
-  signal iaddress_p1  : integer range DEPTH - 1 downto 0;
-  signal iaddress_p2  : integer range DEPTH - 1 downto 0;
-  --
+  shared variable ram   : ram_type := (others => (others => std_logic_vector(to_signed(initVal, byte_width))));
+  signal iaddress_p1    : integer range depth - 1 downto 0;
+  signal iaddress_p2    : integer range depth - 1 downto 0;
+  signal queue_p1_local : word_type;
+  signal queue_p2_local : word_type;
   -----------------------------------------------------------------------------
   
 begin
 
   iaddress_p1 <= to_integer(unsigned(address_p1));
   iaddress_p2 <= to_integer(unsigned(address_p2));
+  
+  -- Reorganize the read data from the RAM to match the output
+  unpack: for i in 0 to bytes - 1 generate
+    queue_p1(byte_width*(i+1) - 1 downto byte_width*i) <= queue_p1_local(i);
+    queue_p2(byte_width*(i+1) - 1 downto byte_width*i) <= queue_p2_local(i);    
+  end generate unpack;
 
-                                        -- read and write data processes
+  -- Read and write data process for p1
   rdwrData_p1 : process (clk)
   begin
     if rising_edge(clk) then
       if (wren_p1 = '1') then
-        ram(iaddress_p1) := data_p1;
-        q_p1             <= data_p1;
-      else
-        q_p1 <= ram(iaddress_p1);
+        -- edit this code if using other than four bytes per word
+        if(byteen_p1(0) = '1') then
+          ram(iaddress_p1)(0) <= data_p1;
+        end if;
+        if(byteen_p1(1) = '1') then
+          ram(iaddress_p1)(1) <= data_p1;
+        end if;
+        if(byteen_p1(2) = '1') then
+          ram(iaddress_p1)(2) <= data_p1;
+        end if;
+        if(byteen_p1(3) = '1') then
+          ram(iaddress_p1)(3) <= data_p1;
+        end if;
       end if;
+      queue_p1_local <= ram(iaddress_p1);
     end if;
   end process rdwrData_p1;
 
+  -- Read and write data process for p2
   rdwrData_p2 : process (clk)
   begin
-    if rising_edge(clk) then
+   if rising_edge(clk) then
       if (wren_p2 = '1') then
-        ram(iaddress_p2) := data_p2;
-        q_p2             <= data_p2;
-      else
-        q_p2 <= ram(iaddress_p2);
+        -- edit this code if using other than four bytes per word
+        if(byteen_p2(0) = '1') then
+          ram(iaddress_p2)(0) <= data_p2;
+        end if;
+        if(byteen_p2(1) = '1') then
+          ram(iaddress_p2)(1) <= data_p2;
+        end if;
+        if(byteen_p2(2) = '1') then
+          ram(iaddress_p2)(2) <= data_p2;
+        end if;
+        if(byteen_p2(3) = '1') then
+          ram(iaddress_p2)(3) <= data_p2;
+        end if;
       end if;
+      queue_p2_local <= ram(iaddress_p2);
     end if;
   end process rdwrData_p2;
 
