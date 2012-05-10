b67068a697602e93a65b3827b293cae92becc788
Hervé Yviquel <hyviquel@gmail.com>
2012-05-10 22:05:58 +0000

    (TTA) Comment ArchitectureBuilder

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/tta/architecture/util/ArchitectureBuilder.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/tta/architecture/util/ArchitectureBuilder.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2012, IRISA
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 package net.sf.orcc.backends.llvm.tta.architecture.util;
 
 import java.util.ArrayList;
@@ -31,6 +59,14 @@ import net.sf.orcc.ir.Type;
 import net.sf.orcc.ir.TypeList;
 import net.sf.orcc.ir.Var;
 
+/**
+ * This class define an architecture builder from the mapping of actors on a set
+ * of processors. The processors and their interconnections are instantiated
+ * during the visit of a given process network.
+ * 
+ * @author Herve Yviquel
+ * 
+ */
 public class ArchitectureBuilder extends DfSwitch<Design> {
 
 	private Map<Component, Map<Component, Buffer>> bufferMap;
@@ -87,6 +123,14 @@ public class ArchitectureBuilder extends DfSwitch<Design> {
 		return buffer;
 	}
 
+	/**
+	 * Add a simple signal to the design. The signal is the translation of
+	 * native connection. External ports and functional units are automatically
+	 * added to the design and their processor.
+	 * 
+	 * @param connection
+	 *            the native connection which represents the signal
+	 */
 	private void addSignal(Connection connection) {
 		Vertex source = componentMap.get(connection.getSource());
 		Vertex target = componentMap.get(connection.getTarget());
@@ -137,7 +181,18 @@ public class ArchitectureBuilder extends DfSwitch<Design> {
 		design.add(signal);
 	}
 
+	/**
+	 * Map a connection on a circular buffer. If the actors source and target of
+	 * the connection are mapped on different processors, then the connection is
+	 * mapped to their interconnected memory and this memory is created in case
+	 * it doesn't yet exist. If both actors are mapped to the same processor,
+	 * then the connection is mapped to its internal RAM.
+	 * 
+	 * @param connection
+	 *            the connection to map on a buffer
+	 */
 	private void mapToBuffer(Connection connection) {
+		// TODO: when both actors are mapped to the same processor
 		Component source = componentMap.get(connection.getSource());
 		Component target = componentMap.get(connection.getTarget());
 
@@ -192,13 +247,19 @@ public class ArchitectureBuilder extends DfSwitch<Design> {
 		Actor actor = instance.getActor();
 		Component component;
 		if (actor.isNative()) {
+			// A native actor describes a VHDL component from the library.
 			component = factory.createComponent(instance.getActor()
 					.getSimpleName());
+			// The parameter of this actor is used as generics.
 			for (Argument arg : instance.getArguments()) {
 				component.setAttribute(arg.getVariable().getName(),
 						arg.getValue());
 			}
 		} else {
+			// FIXME: Map several actors on the same processor
+			// TODO: Let the user choosing the configuration of the processor
+
+			// FIXME: Compute the RAM size during the last compilation step
 			int memorySize = computeNeededMemorySize(instance);
 
 			ProcessorConfiguration conf = ProcessorConfiguration.STANDARD;
