1364c520b8242e248757ae1d18f02fe56be2f556
Hervé Yviquel <hyviquel@gmail.com>
2012-06-12 13:15:55 +0000

    (Core) Move SDFActionsMerger in the package merger.action.

--- eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/tools/merger/action/SDFActionsMerger.java
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/tools/merger/action/SDFActionsMerger.java
@@ -26,7 +26,7 @@
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-package net.sf.orcc.tools.classifier;
+package net.sf.orcc.tools.merger.action;
 
 import java.util.ArrayList;
 import java.util.Iterator;
