b4ea3a30bd3fc825d26feab916495957c0f887eb
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2012-06-29 14:09:28 +0000

    (c lib) Reorganize files in "runtime/C" folder

---
+++ eclipse/plugins/net.sf.orcc.backends/runtime/C/run_cmake_with_VS_env.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if defined VS100COMNTOOLS (
+  echo Visual Studio 2010 found, calling "%VS100COMNTOOLS%vsvars32.bat"
+  call "%VS100COMNTOOLS%vsvars32.bat"
+) else if defined VS90COMNTOOLS (
+  echo Visual Studio 2008 found, calling "%VS90COMNTOOLS%vsvars32.bat"
+  call "%VS90COMNTOOLS%vsvars32.bat"
+) else if defined VS80COMNTOOLS (
+  echo Visual Studio 2005 found, calling "%VS80COMNTOOLS%vsvars32.bat"
+  call "%VS80COMNTOOLS%vsvars32.bat"
+) else (
+  echo Visual Studio not found.
+)
+
+echo Launching "cmake-gui"
+cmake-gui .
