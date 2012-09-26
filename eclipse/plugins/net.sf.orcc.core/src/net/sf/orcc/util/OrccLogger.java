b33bb05a9576c6b352cd44d84badfc0f68f886fe
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2012-09-26 09:22:44 +0000

    (util) Add a class to manager debug, info, warning and errors message from backends and transfo

---
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/util/OrccLogger.java
@@ -0,0 +1,259 @@
+/*
+ * Copyright (c) 2012, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc.util;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+/**
+ * Define the singleton managing Orcc loggers. When using this helper class,
+ * your debug, info, warning and errors messages will be displayed in the right
+ * eclipse console. If no Eclipse GUI plugin is loaded (i.e. executing a job in
+ * command line), all the messages will be sent to the system console.
+ * 
+ * @author Antoine Lorence
+ * 
+ */
+public class OrccLogger {
+
+	/**
+	 * Define how text must be printed to logger (Eclipse or System console)
+	 * 
+	 * @author Antoine Lorence
+	 * 
+	 */
+	private static class DefaultOrccFormatter extends Formatter {
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.util.logging.Formatter#format(java.util.logging.LogRecord)
+		 */
+		@Override
+		public String format(LogRecord record) {
+
+			String output = "";
+
+			if (!(record.getParameters() != null
+					&& record.getParameters().length > 0 && record
+						.getParameters()[0]
+					.equals("-noTime"))) {
+				Date date = new Date(record.getMillis());
+				DateFormat df = DateFormat.getTimeInstance();
+
+				output += df.format(date);
+			}
+			if (record.getLevel().intValue() > Level.INFO.intValue()) {
+				output += " " + record.getLevel();
+			}
+			if (!output.isEmpty()) {
+				output += " : ";
+			}
+			output += record.getMessage();
+			return output;
+		}
+
+	}
+
+	private static Logger logger;
+
+	public static void configureLoggerWithHandler(Handler handler) {
+		logger = null;
+
+		Logger newLog = Logger.getAnonymousLogger();
+		handler.setFormatter(new DefaultOrccFormatter());
+		newLog.addHandler(handler);
+		newLog.setUseParentHandlers(false);
+
+		logger = newLog;
+
+		setLevel(Level.INFO);
+	}
+
+	/**
+	 * Display a debug message to current console. A debug message will be
+	 * displayed only if backend is launched in "Debug Mode"
+	 * 
+	 * @param message
+	 */
+	public static void debug(String message) {
+		getLogger().log(Level.FINE, message);
+	}
+
+	/**
+	 * Display a debug message to current console, appended with a line
+	 * separator character. A debug message will be displayed only if backend is
+	 * launched in "Debug Mode"
+	 * 
+	 * @param message
+	 */
+	public static void debugln(String message) {
+		debug(message + System.getProperty("line.separator"));
+	}
+
+	/**
+	 * Display a debug message on the current console, without prepending time.
+	 * 
+	 * @param message
+	 */
+	public static void debugNoTime(String message) {
+		LogRecord record = new LogRecord(Level.FINE, message);
+		record.setParameters(new Object[] { "-noTime" });
+		getLogger().log(record);
+	}
+
+	/**
+	 * Return the current logger, or a newly created one if it doesn't exists.
+	 * If it is created here, a default ConsoleHandler is used as Logger's
+	 * Handler.
+	 * 
+	 * @return
+	 */
+	private static Logger getLogger() {
+		if (logger == null) {
+			configureLoggerWithHandler(new ConsoleHandler());
+		}
+		return logger;
+	}
+
+	/**
+	 * Set the minimum level displayed. By default, OrccLogger display messages
+	 * from INFO level and highest. Call this method with Level.FINE as argument
+	 * to display debug messages.
+	 * 
+	 * @param level
+	 */
+	public static void setLevel(Level level) {
+		logger.setLevel(level);
+	}
+
+	/**
+	 * Display an error on the current console.
+	 * 
+	 * @param message
+	 */
+	public static void severe(String message) {
+		getLogger().log(Level.SEVERE, message);
+	}
+
+	/**
+	 * Display an error on the current console, appended with a line separator
+	 * character.
+	 * 
+	 * @param message
+	 */
+	public static void severeln(String message) {
+		severe(message + System.getProperty("line.separator"));
+	}
+
+	/**
+	 * Display an error on the current console, without prepending time.
+	 * 
+	 * @param message
+	 */
+	public static void severeNoTime(String message) {
+		LogRecord record = new LogRecord(Level.SEVERE, message);
+		record.setParameters(new Object[] { "-noTime" });
+		getLogger().log(record);
+	}
+
+	/**
+	 * Display an information message on current Eclipse console or on sytem
+	 * console (if used from command line)
+	 * 
+	 * @param message
+	 */
+	public static void trace(String message) {
+		getLogger().log(Level.INFO, message);
+	}
+
+	/**
+	 * Display an information message on current Eclipse console or on sytem
+	 * console (if used from command line). The message will be appended with a
+	 * line separator character.
+	 * 
+	 * @param message
+	 */
+	public static void traceln(String message) {
+		trace(message + System.getProperty("line.separator"));
+	}
+
+	/**
+	 * Display a warning on the current console, without prepending time.
+	 * 
+	 * @param message
+	 */
+	public static void traceNoTime(String message) {
+		LogRecord record = new LogRecord(Level.INFO, message);
+		record.setParameters(new Object[] { "-noTime" });
+		getLogger().log(record);
+	}
+
+	/**
+	 * Display a warning on the current console.
+	 * 
+	 * @param message
+	 */
+	public static void warn(String message) {
+		getLogger().log(Level.WARNING, message);
+	}
+
+	/**
+	 * Display a warning on the current console, appended with a line separator
+	 * character.
+	 * 
+	 * @param message
+	 */
+	public static void warnln(String message) {
+		warn(message + System.getProperty("line.separator"));
+	}
+
+	/**
+	 * Display a warning on the current console, without prepending time.
+	 * 
+	 * @param message
+	 */
+	public static void warnNoTime(String message) {
+		LogRecord record = new LogRecord(Level.WARNING, message);
+		record.setParameters(new Object[] { "-noTime" });
+		getLogger().log(record);
+	}
+
+	/**
+	 * Ensure class can't be instantiated
+	 */
+	private OrccLogger() {
+	}
+}
---
+++ eclipse/plugins/net.sf.orcc.ui/src/net/sf/orcc/ui/console/OrccUiConsoleHandler.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2012, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc.ui.console;
+
+import java.io.IOException;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+import net.sf.orcc.OrccRuntimeException;
+
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IOConsole;
+import org.eclipse.ui.console.IOConsoleOutputStream;
+
+/**
+ * This class declare a simple Handler to print messages in the Eclipse console.
+ * 
+ * @author mpelcat
+ * @author mwipliez
+ * @author Antoine Lorence
+ * @see java.util.logging.Handler
+ */
+public class OrccUiConsoleHandler extends Handler {
+
+	IOConsole console = null;
+
+	/**
+	 * Build the Ecliple console handler
+	 * 
+	 * @param currentConsole
+	 *            the current eclipse ProcessConsole
+	 * 
+	 */
+	public OrccUiConsoleHandler(IConsole currentConsole) {
+		super();
+		if (currentConsole instanceof IOConsole) {
+			this.console = (IOConsole) currentConsole;
+			console.activate();
+		} else {
+			throw new OrccRuntimeException(
+					"Invalid console passed in parameter");
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.logging.Handler#close()
+	 */
+	@Override
+	public void close() throws SecurityException {
+		console.destroy();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.logging.Handler#flush()
+	 */
+	@Override
+	public void flush() {
+		console.clearConsole();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.logging.Handler#publish(java.util.logging.LogRecord)
+	 */
+	@Override
+	public void publish(LogRecord record) {
+		console.activate();
+
+		IOConsoleOutputStream outStream = console.newOutputStream();
+
+		try {
+			outStream.write(record.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
