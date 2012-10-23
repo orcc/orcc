cd050b844e44483a63f8e2ac9a1529a728b7f304
Matthieu Wipliez <matthieu.wipliez@gmail.com>
2012-10-23 15:05:14 +0000

    (misc) now using a LinkedHashSet in NetworkImpl to return the networks in the same order they were visited

--- eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/df/impl/NetworkImpl.java
+++ eclipse/plugins/net.sf.orcc.core/src/net/sf/orcc/df/impl/NetworkImpl.java
@@ -33,6 +33,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -471,7 +472,7 @@ public class NetworkImpl extends GraphImpl implements Network {
 
 	@Override
 	public List<Network> getAllNetworks() {
-		Set<Network> networks = new HashSet<Network>();
+		Set<Network> networks = new LinkedHashSet<Network>();
 		for (Vertex vertex : getChildren()) {
 			Network network = vertex.getAdapter(Network.class);
 			if (network != null) {
