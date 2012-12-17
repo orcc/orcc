c7e8e8493eb7a3d137be6cba1ee98d631288de1d
Herve Yviquel <hyviquel@gmail.com>
2012-12-17 19:56:11 +0100

    (TTA) Make the PrintRemoval more aggressive by removing any existing string (expressions, arguments, variables and so on).

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/tta/transform/PrintRemoval.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/llvm/tta/transform/PrintRemoval.java
@@ -1,17 +1,70 @@
 package net.sf.orcc.backends.llvm.tta.transform;
 
+import java.util.ArrayList;
+
+import net.sf.orcc.df.Actor;
+import net.sf.orcc.df.util.DfVisitor;
+import net.sf.orcc.ir.Arg;
+import net.sf.orcc.ir.ArgByVal;
+import net.sf.orcc.ir.Expression;
 import net.sf.orcc.ir.InstCall;
+import net.sf.orcc.ir.Param;
+import net.sf.orcc.ir.Procedure;
+import net.sf.orcc.ir.Var;
 import net.sf.orcc.ir.util.AbstractIrVisitor;
 import net.sf.orcc.ir.util.IrUtil;
 
-public class PrintRemoval extends AbstractIrVisitor<Void> {
+public class PrintRemoval extends DfVisitor<Void> {
 
-	public Void caseInstCall(InstCall call) {
-		if (call.isPrint()) {
-			IrUtil.delete(call);
-			indexInst--;
+	private class InnerPrintRemoval extends AbstractIrVisitor<Void> {
+
+		public InnerPrintRemoval() {
+			super(true);
 		}
-		return null;
+
+		public Void caseInstCall(InstCall call) {
+			if (call.isPrint()) {
+				IrUtil.delete(call);
+				indexInst--;
+			} else {
+				for (Arg arg : new ArrayList<Arg>(call.getArguments())) {
+					if (arg.isByVal()) {
+						Expression expr = ((ArgByVal) arg).getValue();
+						if (expr.isExprString()) {
+							IrUtil.delete(arg);
+						}
+					}
+				}
+			}
+			return null;
+		}
+
+		public Void caseProcedure(Procedure proc) {
+			for (Param param : new ArrayList<Param>(proc.getParameters())) {
+				if (param.getVariable().getType().isString()) {
+					IrUtil.delete(param);
+				}
+			}
+			for (Var var : new ArrayList<Var>(proc.getLocals())) {
+				if (var.getType().isString()) {
+					IrUtil.delete(var);
+				}
+			}
+			return super.caseProcedure(proc);
+		}
+	}
+
+	public PrintRemoval() {
+		this.irVisitor = new InnerPrintRemoval();
+	}
+
+	public Void caseActor(Actor actor) {
+		for (Var var : new ArrayList<Var>(actor.getStateVars())) {
+			if (var.getType().isString()) {
+				IrUtil.delete(var);
+			}
+		}
+		return super.caseActor(actor);
 	}
 
 }
