70d52c6a6ee20d864a0f85f12ee23b962a193773
Alexandre Sanchez <Alexandre.Sanchez@insa-rennes.fr>
2013-09-17 15:28:33 +0200

    (C) Change min repeat size used for vectorization

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/BackendsConstants.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/BackendsConstants.java
@@ -54,5 +54,5 @@ public class BackendsConstants {
 
 	public static final String HMPP_NO_PRAGMAS = "net.sf.orcc.backends.c.hmpp.disablePragma";
 
-	public static final int MIN_VECTORIZABLE = 32;
+	public static final int MIN_VECTORIZABLE = 2;
 }
