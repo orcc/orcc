1e3b4593305658c00a178f254fe5a626cf8dfaa7
Antoine <antoine.lorence@insa-rennes.fr>
2013-10-08 16:57:29 +0200

    Customize the rendering for an Instance
    Sizes are defined with constants
    An instance can be deleted, but not removed
    An instance can't be resized
    Styles are set for all elements of the graphics algorithm

--- eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/patterns/InstancePattern.java
+++ eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/patterns/InstancePattern.java
@@ -28,13 +28,21 @@
  */
 package net.sf.orcc.xdf.ui.patterns;
 
+import net.sf.orcc.df.DfFactory;
 import net.sf.orcc.df.Instance;
+import net.sf.orcc.util.OrccLogger;
+import net.sf.orcc.xdf.ui.styles.StyleUtil;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.graphiti.features.IDirectEditingInfo;
 import org.eclipse.graphiti.features.context.IAddContext;
 import org.eclipse.graphiti.features.context.ICreateContext;
+import org.eclipse.graphiti.features.context.IDeleteContext;
 import org.eclipse.graphiti.features.context.ILayoutContext;
+import org.eclipse.graphiti.features.context.IRemoveContext;
+import org.eclipse.graphiti.features.context.IResizeShapeContext;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
+import org.eclipse.graphiti.mm.algorithms.Polyline;
 import org.eclipse.graphiti.mm.algorithms.RoundedRectangle;
 import org.eclipse.graphiti.mm.algorithms.Text;
 import org.eclipse.graphiti.mm.algorithms.styles.Orientation;
@@ -47,15 +55,26 @@ import org.eclipse.graphiti.pattern.IPattern;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.services.IGaService;
 import org.eclipse.graphiti.services.IPeCreateService;
+import org.eclipse.graphiti.services.IPeService;
 
 /**
- * This is a default class, from a new Graphiti project. It should be modified
- * to fit our needs.
+ * This class configure as most features as possible, relative to Instances that
+ * can be added to a Network.
  * 
  * @author Antoine Lorence
  * 
  */
 public class InstancePattern extends AbstractPattern implements IPattern {
+
+	private static int TOTAL_WIDTH = 120;
+	private static int TOTAL_HEIGHT = 140;
+	private static int LABEL_HEIGHT = 40;
+
+	private static String PROPERTY_ID = "XDF_ID";
+
+	private enum IDS {
+		LABEL, INPORTS_LIST, OUTPORTS_LIST
+	};
 
 	public InstancePattern() {
 		super(null);
@@ -83,6 +102,23 @@ public class InstancePattern extends AbstractPattern implements IPattern {
 		return isMainBusinessObjectApplicable(domainObject);
 	}
 
+	/*
+	 * An instance can't be removed from a graph. Instead it must be deleted.
+	 * 
+	 * @see
+	 * org.eclipse.graphiti.pattern.AbstractPattern#canRemove(org.eclipse.graphiti
+	 * .features.context.IRemoveContext)
+	 */
+	@Override
+	public boolean canRemove(IRemoveContext context) {
+		return false;
+	}
+
+	@Override
+	public boolean canDelete(IDeleteContext context) {
+		return true;
+	}
+
 	@Override
 	public boolean canCreate(ICreateContext context) {
 		return context.getTargetContainer() instanceof Diagram;
@@ -90,12 +126,14 @@ public class InstancePattern extends AbstractPattern implements IPattern {
 
 	@Override
 	public Object[] create(ICreateContext context) {
-		// TODO: create the domain object here
-		Object newInstance = null;
+		Instance newInstance = null;
+		if (true) {
+			newInstance = DfFactory.eINSTANCE.createInstance();
+		}
 
-		// TODO: in case of an EMF object add the new object to a suitable
-		// resource
-		// getDiagram().eResource().getContents().add(newInstance);
+		// TODO: We must add the new instance to an Xdf resource created
+		// earlier, instead of in the .diragram file directly
+		getDiagram().eResource().getContents().add(newInstance);
 
 		addGraphicalRepresentation(context, newInstance);
 		return new Object[] { newInstance };
@@ -103,48 +141,82 @@ public class InstancePattern extends AbstractPattern implements IPattern {
 
 	@Override
 	public boolean canAdd(IAddContext context) {
-		// TODO: check for right domain object instance below
-		return /* context.getNewObject() instanceof Instance && */context
-				.getTargetContainer() instanceof Diagram;
+		boolean canAdd = false;
+		if (context.getTargetContainer() instanceof Diagram) {
+			if (context.getNewObject() instanceof Instance) {
+				canAdd = true;
+			}
+		}
+		return canAdd;
 	}
 
 	@Override
 	public PictogramElement add(IAddContext context) {
+		final Diagram targetDiagram = (Diagram) context.getTargetContainer();
+		final IPeService peService = Graphiti.getPeService();
+		final IPeCreateService peCreateService = Graphiti.getPeCreateService();
+		final IGaService gaService = Graphiti.getGaService();
 
-		Diagram targetDiagram = (Diagram) context.getTargetContainer();
-		IPeCreateService peCreateService = Graphiti.getPeCreateService();
-		IGaService gaService = Graphiti.getGaService();
+		// Create the container
+		final ContainerShape containerShape;
+		{
+			containerShape = peCreateService.createContainerShape(
+					targetDiagram, true);
+			final RoundedRectangle roundedRectangle = gaService
+					.createPlainRoundedRectangle(containerShape, 5, 5);
+			roundedRectangle.setStyle(StyleUtil
+					.getStyleForInstance(getDiagram()));
+			gaService.setLocationAndSize(roundedRectangle, context.getX(),
+					context.getY(), TOTAL_WIDTH, TOTAL_HEIGHT);
+		}
 
-		ContainerShape containerShape = peCreateService.createContainerShape(
-				targetDiagram, true);
-		RoundedRectangle roundedRectangle = gaService.createRoundedRectangle(
-				containerShape, 5, 5);
-		gaService.setLocationAndSize(roundedRectangle, context.getX(),
-				context.getY(), context.getWidth(), context.getHeight());
-		roundedRectangle.setFilled(false);
+		{
+			// The text label for Instance name
+			final Shape shape = peCreateService.createShape(containerShape,
+					false);
+			final Text text = gaService.createPlainText(shape);
+			text.setHorizontalAlignment(Orientation.ALIGNMENT_CENTER);
+			text.setVerticalAlignment(Orientation.ALIGNMENT_CENTER);
+			text.setStyle(StyleUtil.getStyleForInstanceText(getDiagram()));
+			gaService.setLocationAndSize(text, 0, 0, TOTAL_WIDTH, LABEL_HEIGHT);
 
-		Shape shape = peCreateService.createShape(containerShape, false);
-		Text text = gaService.createText(shape, "Instance");
-		text.setHorizontalAlignment(Orientation.ALIGNMENT_CENTER);
-		text.setVerticalAlignment(Orientation.ALIGNMENT_CENTER);
-		gaService.setLocationAndSize(text, 0, 0, context.getWidth(),
-				context.getHeight());
+			peService.setPropertyValue(shape, PROPERTY_ID, IDS.LABEL.name());
 
-		peCreateService.createChopboxAnchor(containerShape);
+		}
 
-		// TODO: enable the link to the domain object
-		// Object addedDomainObject = context.getNewObject();
-		// link(containerShape, addedDomainObject);
+		{
+			// The line separator
+			final Shape shape = peCreateService.createShape(containerShape,
+					false);
+			final int[] xy = { 0, LABEL_HEIGHT, TOTAL_WIDTH, LABEL_HEIGHT };
+			final Polyline line = gaService.createPlainPolyline(shape, xy);
+			line.setStyle(StyleUtil.getCommonStyle(getDiagram()));
+		}
+
+		// We link graphical representation and domain model object
+		Instance addedDomainObject = (Instance) context.getNewObject();
+		link(containerShape, addedDomainObject);
 
 		return containerShape;
 	}
 
+	/*
+	 * We don't want to authorize user to resize the Instance pictogram
+	 * 
+	 * @see
+	 * org.eclipse.graphiti.pattern.AbstractPattern#canResizeShape(org.eclipse
+	 * .graphiti.features.context.IResizeShapeContext)
+	 */
+	@Override
+	public boolean canResizeShape(IResizeShapeContext context) {
+		return false;
+	}
+
 	@Override
 	public boolean canLayout(ILayoutContext context) {
-		// TODO: check for right domain object instances below
-		return context.getPictogramElement() instanceof ContainerShape;
-		// && getBusinessObjectForPictogramElement(context
-		// .getPictogramElement()) instanceof Instance;
+		PictogramElement elt = context.getPictogramElement();
+		Object obj = getBusinessObjectForPictogramElement(elt);
+		return elt instanceof ContainerShape && obj instanceof Instance;
 	}
 
 	@Override
---
+++ eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/styles/StyleUtil.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2013, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of IRISA nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package net.sf.orcc.xdf.ui.styles;
+
+import org.eclipse.graphiti.mm.algorithms.styles.LineStyle;
+import org.eclipse.graphiti.mm.algorithms.styles.Orientation;
+import org.eclipse.graphiti.mm.algorithms.styles.Style;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
+import org.eclipse.graphiti.services.Graphiti;
+import org.eclipse.graphiti.services.IGaService;
+import org.eclipse.graphiti.util.ColorConstant;
+import org.eclipse.graphiti.util.IColorConstant;
+import org.eclipse.graphiti.util.PredefinedColoredAreas;
+
+/**
+ * Styles are created here with "plain"-methods, i.e. all values have to be set
+ * explicitly.
+ * 
+ * See chapters "Using Styles" and "Color Schemas" in tutorial for details.
+ */
+public class StyleUtil {
+
+	private static final IColorConstant INSTANCE_TEXT_FOREGROUND = new ColorConstant(
+			0, 0, 0);
+	private static final IColorConstant INSTANCE_FOREGROUND = new ColorConstant(
+			98, 131, 167);
+
+	private static void setCommonTextValues(Diagram diagram,
+			IGaService gaService, Style style) {
+		style.setFilled(false);
+		style.setAngle(0);
+		style.setHorizontalAlignment(Orientation.ALIGNMENT_CENTER);
+		style.setVerticalAlignment(Orientation.ALIGNMENT_CENTER);
+		style.setForeground(gaService.manageColor(diagram,
+				INSTANCE_TEXT_FOREGROUND));
+	}
+
+	private static void setCommonValues(Style style) {
+		style.setLineStyle(LineStyle.SOLID);
+		style.setLineVisible(true);
+		style.setLineWidth(2);
+		style.setTransparency(0.0);
+	}
+
+	public static Style getCommonStyle(Diagram diagram) {
+		final String styleId = "COMMONSTYLE";
+		IGaService gaService = Graphiti.getGaService();
+
+		// Is style already persisted?
+		Style style = gaService.findStyle(diagram, styleId);
+
+		if (style == null) { // style not found - create new style
+			style = gaService.createPlainStyle(diagram, styleId);
+			setCommonValues(style);
+		}
+		return style;
+	}
+
+	public static Style getStyleForInstance(Diagram diagram) {
+		final String styleId = "INSTANCE";
+		IGaService gaService = Graphiti.getGaService();
+
+		// this is a child style of the common-values-style
+		Style parentStyle = getCommonStyle(diagram);
+		Style style = gaService.findStyle(parentStyle, styleId);
+
+		if (style == null) { // style not found - create new style
+			style = gaService.createPlainStyle(parentStyle, styleId);
+			style.setFilled(true);
+			style.setForeground(gaService.manageColor(diagram,
+					INSTANCE_FOREGROUND));
+
+			gaService.setRenderingStyle(style,
+					PredefinedColoredAreas.getCopperWhiteGlossAdaptions());
+		}
+		return style;
+	}
+
+	public static Style getStyleForInstanceText(Diagram diagram) {
+		final String styleId = "INSTANCE-TEXT";
+		IGaService gaService = Graphiti.getGaService();
+
+		// this is a child style of the common-values-style
+		Style parentStyle = getCommonStyle(diagram);
+		Style style = gaService.findStyle(parentStyle, styleId);
+
+		if (style == null) { // style not found - create new style
+			style = gaService.createPlainStyle(parentStyle, styleId);
+			setCommonTextValues(diagram, gaService, style);
+			style.setFont(gaService.manageDefaultFont(diagram, false, true));
+		}
+		return style;
+	}
+
+	public static Style getStyleForTextDecorator(Diagram diagram) {
+		final String styleId = "TEXT-DECORATOR-TEXT";
+		IGaService gaService = Graphiti.getGaService();
+
+		// this is a child style of the common-values-style
+		Style parentStyle = getCommonStyle(diagram);
+		Style style = gaService.findStyle(parentStyle, styleId);
+
+		if (style == null) { // style not found - create new style
+			style = gaService.createPlainStyle(parentStyle, styleId);
+			setCommonTextValues(diagram, gaService, style);
+			style.setFont(gaService.manageDefaultFont(diagram, true, false));
+		}
+		return style;
+	}
+
+}
