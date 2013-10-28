2886fbca05481d06b2e115de6cc11a59998ad230
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2013-10-28 14:12:42 +0100

    Create both diagram and xdf file from wizard

--- eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/wizards/NewNetworkWizard.java
+++ eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/wizards/NewNetworkWizard.java
@@ -28,9 +28,23 @@
  */
 package net.sf.orcc.xdf.ui.wizards;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import net.sf.orcc.df.DfFactory;
+import net.sf.orcc.df.Network;
 import net.sf.orcc.xdf.ui.Activator;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
+import org.eclipse.graphiti.mm.pictograms.PictogramLink;
+import org.eclipse.graphiti.mm.pictograms.PictogramsFactory;
+import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
@@ -95,6 +109,46 @@ public class NewNetworkWizard extends Wizard implements INewWizard {
 		
 		IFile file = page.createNewFile();
 		if (file == null) {
+			return false;
+		}
+
+		// File name (without extension)
+		String networkName = file.getFullPath().removeFileExtension().lastSegment();
+
+		// Create a new network
+		Network network = DfFactory.eINSTANCE.createNetwork();
+		network.setName(networkName);
+		// Create a new diagram
+		Diagram diagram = Graphiti.getPeCreateService().createDiagram(Activator.DIAGRAM_SUFFIX, networkName, true);
+
+		// Link diagram to network
+		PictogramLink link = PictogramsFactory.eINSTANCE.createPictogramLink();
+		link.getBusinessObjects().add(network);
+		diagram.setLink(link);
+
+		// Initialize URIs for diagram and xdf resources
+		IPath xdfPath = file.getFullPath();
+		URI xdfUri = URI.createPlatformResourceURI(xdfPath.toString(), true);
+		IPath diagramPath = xdfPath.removeFileExtension().addFileExtension("diagram");
+		URI diagramUri = URI.createPlatformResourceURI(diagramPath.toString(), true);
+
+		// Create a resource to store network content
+		// Obtain a new resource set
+		ResourceSet resourceSet = new ResourceSetImpl();
+
+		// Create resources
+		Resource xdfResource = resourceSet.createResource(xdfUri);
+		Resource diagramResource = resourceSet.createResource(diagramUri);
+
+		// Get the first model element and cast it to the right type, in my
+		// example everything is hierarchical included in this first node
+		xdfResource.getContents().add(network);
+		diagramResource.getContents().add(diagram);
+
+		try {
+			xdfResource.save(Collections.EMPTY_MAP);
+			diagramResource.save(Collections.EMPTY_MAP);
+		} catch (IOException e) {
 			return false;
 		}
 
