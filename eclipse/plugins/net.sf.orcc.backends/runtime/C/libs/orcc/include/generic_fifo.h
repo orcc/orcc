10a0dc6f23f0b19be0135dd1ca6ed4c4bf8541ba
Herve Yviquel <hyviquel@gmail.com>
2013-11-13 11:19:54 +0100

    (C) Remove generic_fifo.inl

--- eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc/include/generic_fifo.h
+++ eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc/include/generic_fifo.h
@@ -19,13 +19,17 @@
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * CONSEQUENTIAL DAMAGES (INCL#include <string.h>
+#include <stdlib.h>UDING, BUT NOT LIMITED TO, PROCUREMENT OF
  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
  * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+
+#include <string.h>
+#include <stdlib.h>
 
 /** lock free fifo ring buffer structure */
 struct FIFO_S(T) {
@@ -37,4 +41,52 @@ struct FIFO_S(T) {
 	unsigned int write_ind; /** the current position of the writer */
 };
 
-#include "generic_fifo.inl"
+static unsigned int FIFO_HAS_TOKENS(T)(struct FIFO_S(T) *fifo, unsigned int reader_id, unsigned int n) {
+    return fifo->write_ind - fifo->read_inds[reader_id] >= n;
+}
+
+static unsigned int FIFO_GET_NUM_TOKENS(T)(struct FIFO_S(T) *fifo, unsigned int reader_id) {
+    return fifo->write_ind - fifo->read_inds[reader_id];
+}
+
+static unsigned int FIFO_HAS_ROOM(T)(struct FIFO_S(T) *fifo, unsigned int num_readers, unsigned int n) {
+    unsigned int i;
+    for(i = 0; i < num_readers; i++) {
+        if (fifo->size + 1 - (fifo->write_ind - fifo->read_inds[i]) <= n) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+static unsigned int FIFO_GET_ROOM(T)(struct FIFO_S(T) *fifo, unsigned int num_readers) {
+    unsigned int i;
+    unsigned int max_num_tokens = 0;
+
+    for (i = 0; i < num_readers; i++) {
+        unsigned int num_tokens = fifo->write_ind - fifo->read_inds[i];
+        max_num_tokens = max_num_tokens > num_tokens ? max_num_tokens : num_tokens;
+    }
+
+    return fifo->size - max_num_tokens;
+}
+
+static void FIFO_CLEAR(T)(struct FIFO_S(T) *fifo) {
+    unsigned int i;
+    fifo->write_ind = 0;
+    for (i = 0; i < fifo->readers_nb; i++) {
+        fifo->read_inds[i] = 0;
+    }
+}
+
+static T FIFO_READ(T)(struct FIFO_S(T) *fifo, unsigned int reader_id) {
+    T value = fifo->contents[fifo->read_inds[reader_id] & (fifo->size - 1)];
+    fifo->read_inds[reader_id]++;
+    return value;
+}
+
+static void FIFO_WRITE(T)(struct FIFO_S(T) *fifo, T value) {
+    fifo->contents[fifo->write_ind & (fifo->size - 1)] = value;
+    fifo->write_ind++;
+}
