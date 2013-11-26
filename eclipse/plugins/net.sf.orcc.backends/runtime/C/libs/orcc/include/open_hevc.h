b2dd6240506d550bc143cb5dc9d13addac018233
kjerbi <khaled_jerbi@yahoo.fr>
2013-11-26 11:20:52 +0100

    (lib openhevc_SSE) remove useless list dimensions from interpol wrappers

--- eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc/include/open_hevc.h
+++ eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc/include/open_hevc.h
@@ -48,41 +48,41 @@
 
 int openHEVCIsPresent();
 
-void put_hevc_qpel_pixel_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
+void put_hevc_qpel_pixel_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
   u8 width, u8 height);
-void put_hevc_epel_pixel_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
-  u8 width, u8 height, i32 component);
+void put_hevc_epel_pixel_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
+  u8 width, u8 height);
 
-void put_hevc_qpel_h_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
+void put_hevc_qpel_h_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
   i32 filterIdx,  u8 width, u8 height);
 
-void put_hevc_qpel_v_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
+void put_hevc_qpel_v_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
   i32 filterIdx,  u8 width, u8 height);
 
-void put_hevc_epel_h_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
-  i32 filterIdx,  u8 width, u8 height, i32 component);
+void put_hevc_epel_h_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
+  i32 filterIdx,  u8 width, u8 height);
 
-void put_hevc_epel_v_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
-  i32 filterIdx,  u8 width, u8 height, i32 component);
+void put_hevc_epel_v_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
+  i32 filterIdx,  u8 width, u8 height);
 
-void put_hevc_qpel_hv_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
+void put_hevc_qpel_hv_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
   i32 filterIdx[2],  u8 width, u8 height);
 
-void put_hevc_epel_hv_orcc(i16 _dst[2][3][64*64], u8 listIdx,
-  u8 _src[3][71*71], u8 srcstride,
-  i32 filterIdx[2],  u8 width, u8 height, i32 component);
+void put_hevc_epel_hv_orcc(i16 _dst[2][64*64], u8 listIdx,
+  u8 _src[71*71], u8 srcstride,
+  i32 filterIdx[2],  u8 width, u8 height);
 
-void put_unweighted_pred_orcc(u8 _dst[2][3][64*64],
-  i16 _src[2][3][64*64], u8 width, u8 height, u8 rdList, i32 component);
+void put_unweighted_pred_orcc(u8 _dst[2][64*64],
+  i16 _src[2][64*64], u8 width, u8 height, u8 rdList);
 
-void put_unweighted_pred_avg_orcc(u8 _dst[2][3][64*64],
-  i16 _src[2][3][64*64], u8 width, u8 height, i32 component);
+void put_unweighted_pred_avg_orcc(u8 _dst[2][64*64],
+  i16 _src[2][64*64], u8 width, u8 height);
 
 #endif
