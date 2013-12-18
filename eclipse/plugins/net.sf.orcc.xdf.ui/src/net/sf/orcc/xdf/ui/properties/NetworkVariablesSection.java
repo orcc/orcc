118ded6a7f6105f231375a6abd1d81592ba92e22
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2013-12-18 16:17:32 +0100

    Implements network variables declarations

--- eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/properties/NetworkVariablesSection.java
+++ eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/properties/NetworkVariablesSection.java
@@ -28,9 +28,85 @@
  */
 package net.sf.orcc.xdf.ui.properties;
 
+import net.sf.orcc.OrccRuntimeException;
+import net.sf.orcc.df.Network;
+import net.sf.orcc.ir.Var;
+import net.sf.orcc.ir.util.ExpressionPrinter;
+import net.sf.orcc.ir.util.TypePrinter;
+import net.sf.orcc.xdf.ui.util.XdfUtil;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
 public class NetworkVariablesSection extends AbstractTableBasedSection {
+
+	/**
+	 * Define the dialog used to edit a network parameter.
+	 * 
+	 * @author Antoine Lorence
+	 * 
+	 */
+	private class VariableItemEditor extends ItemEditor {
+
+		private Text type;
+		private Text name;
+		private Text value;
+
+		protected VariableItemEditor(final TableItem item) {
+			super(item);
+		}
+
+		@Override
+		protected String getDialogTitle() {
+			return "Edit variable";
+		}
+
+		@Override
+		protected Control createDialogArea(Composite parent) {
+			final Composite container = (Composite) super.createDialogArea(parent);
+			final GridData fill = new GridData(SWT.FILL, SWT.FILL, true, true);
+
+			final Label typeLabel = new Label(container, SWT.NONE);
+			typeLabel.setText("Type:");
+
+			type = new Text(container, SWT.NONE);
+			type.setLayoutData(fill);
+
+			final Label nameLabel = new Label(container, SWT.NONE);
+			nameLabel.setText("Name:");
+
+			name = new Text(container, SWT.NONE);
+			name.setLayoutData(fill);
+
+			final Label valueLabel = new Label(container, SWT.NONE);
+			valueLabel.setText("Value:");
+
+			value = new Text(container, SWT.NONE);
+			value.setLayoutData(fill);
+
+			if (getItem() != null) {
+				type.setText(getItem().getText(0));
+				name.setText(getItem().getText(1));
+				value.setText(getItem().getText(2));
+			}
+
+			return container;
+		}
+
+		@Override
+		protected void okPressed() {
+			getItem().setText(new String[] { type.getText(), name.getText(), value.getText() });
+			super.okPressed();
+		}
+	}
 
 	@Override
 	protected String getFormText() {
@@ -38,19 +114,70 @@ public class NetworkVariablesSection extends AbstractTableBasedSection {
 	}
 
 	@Override
+	public void createControls(Composite parent, TabbedPropertySheetPage aTabbedPropertySheetPage) {
+		super.createControls(parent, aTabbedPropertySheetPage);
+
+		final TableColumn typeColumn = new TableColumn(table, SWT.NONE);
+		typeColumn.setText("Type");
+		typeColumn.setWidth(80);
+
+		final TableColumn nameColumn = new TableColumn(table, SWT.NONE);
+		nameColumn.setText("Name");
+		nameColumn.setWidth(100);
+
+		final TableColumn valueColumn = new TableColumn(table, SWT.NONE);
+		valueColumn.setText("Value");
+	}
+
+	@Override
 	void editTableItem(TableItem item) {
+		final VariableItemEditor editor = new VariableItemEditor(item);
+		editor.open();
 	}
 
 	@Override
 	protected void readValuesFromModels() {
-		// TODO Auto-generated method stub
+		final Network network = (Network) businessObject;
+		final TypePrinter typePrinter = new TypePrinter();
+		final ExpressionPrinter exprPrinter = new ExpressionPrinter();
 
+		table.removeAll();
+
+		for (final Var var : network.getVariables()) {
+			final String[] itemValues = { typePrinter.doSwitch(var.getType()), var.getName(),
+					exprPrinter.doSwitch(var.getInitialValue()) };
+			final TableItem item = new TableItem(table, SWT.NONE);
+			item.setText(itemValues);
+		}
 	}
 
 	@Override
 	protected void writeValuesToModel() {
-		// TODO Auto-generated method stub
+		final Network network = (Network) businessObject;
 
+		// Declare parameters which can be referenced by this variable
+		calParser.setDeclaredVars(network.getParameters());
+
+		network.getVariables().clear();
+
+		for (final TableItem item : table.getItems()) {
+			final String typeText = item.getText(0);
+			final String nameText = item.getText(1);
+			final String valueText = item.getText(2);
+
+			try {
+				final Var var = calParser.parseVariableDeclaration(typeText + " " + nameText + " = " + valueText);
+				if (var != null) {
+					network.getVariables().add(var);
+					calParser.addDeclaredVar(var);
+				} else {
+					MessageDialog.openError(XdfUtil.getDefaultShell(), "Syntax error",
+							"Values you entered could not be parsed");
+				}
+			} catch (OrccRuntimeException e) {
+				MessageDialog.openError(XdfUtil.getDefaultShell(), "Error", e.getMessage());
+			}
+		}
 	}
 
 }
