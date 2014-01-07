50ba08cd8721828d87de57ecb6cad371f6ce7578
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2014-01-07 13:17:03 +0100

    Fix licenses

--- eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/layout/PolylineAutoLayoutFeature.java
+++ eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/layout/PolylineAutoLayoutFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, IETR/INSA of Rennes
+ * Copyright (c) 2014, IETR/INSA of Rennes
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
