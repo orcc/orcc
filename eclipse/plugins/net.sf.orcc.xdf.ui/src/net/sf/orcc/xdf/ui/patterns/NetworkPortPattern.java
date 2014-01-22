orcc/orcc

commit 23af01dc24aeb9257f772e0bc4938e8513b274a5

Fix bug on Network port layouting

eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/patterns/NetworkPortPattern.java
@@ -333,17 +333,13 @@ abstract public class NetworkPortPattern extends AbstractPattern implements IPat
 		final int minTxtWidth = XdfUtil.getTextMinWidth(txt);
 		final int newTextWidth = Math.max(minTxtWidth, SHAPE_WIDTH);
 
-		if (minTxtWidth > SHAPE_WIDTH) {
-			int xscale = (minTxtWidth - SHAPE_WIDTH) / 2;
-			poly.setX(xscale);
-		}
-
-		// Set the new width for the port text
+		// Update text width
 		gaService.setSize(txt, newTextWidth, XdfUtil.getTextMinHeight(txt));
-		topLevelPe.getGraphicsAlgorithm().setWidth(newTextWidth);
+		// Update top level invisible rectangle width
+		topLevelGa.setWidth(newTextWidth);
 
-		gaService.setSize(topLevelGa, Math.max(newTextWidth, SHAPE_WIDTH), SHAPE_HEIGHT + TEXT_PORT_SPACE
-				+ TEXT_DEFAULT_HEIGHT);
+		// Update position of polygon in the top level rectangle
+		gaService.setLocation(poly, (newTextWidth - SHAPE_WIDTH) / 2, 0);
 
 		return true;
 	}

commit 618bfd65ab60ac8ef497c3b80540f595a91b3588

Fix bug introduced in 23af01dc2

eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/patterns/NetworkPortPattern.java
@@ -332,9 +332,8 @@ abstract public class NetworkPortPattern extends AbstractPattern implements IPat
 
 		// Update text width
 		gaService.setSize(txt, newTextWidth, XdfUtil.getTextMinHeight(txt));
-		// Update top level invisible rectangle width
-		topLevelGa.setWidth(newTextWidth);
-
+		// Update top level invisible rectangle size & position
+		gaService.setSize(topLevelGa, newTextWidth, SHAPE_HEIGHT + TEXT_PORT_SPACE + TEXT_DEFAULT_HEIGHT);
 		// Update position of polygon in the top level rectangle
 		gaService.setLocation(poly, (newTextWidth - SHAPE_WIDTH) / 2, 0);
 