orcc/orcc

commit fabe202bf3a531f1e7fe4a05a20d55a6659c9d64

(gui) Allow refinement only with a different entity

eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/patterns/InstancePattern.java
@@ -488,6 +488,12 @@ public class InstancePattern extends AbstractPattern {
 
 		// Set the current instance's entity
 		final Instance instance = (Instance) getBusinessObjectForPictogramElement(instanceShape);
+
+		// Do not update refinement with the same entity
+		if (instance.getEntity() == entity) {
+			return;
+		}
+
 		instance.setEntity(entity);
 		Graphiti.getPeService().setPropertyValue(instanceShape, REFINEMENT_KEY,
 				entity.eResource().getURI().toPlatformString(true));

commit 938fa669dbae51246c8f569d453ee90247128f77

(gui) Allow setRefinement on instance already refined
Fixes issue introduced in fabe202bf3a53
Now setInstanceRefinement() can run even if Instance is already refined
on the same entity. To prevent refining 2 times or more on the same entity,
we now check the refinement value in the instance shape

eclipse/plugins/net.sf.orcc.xdf.ui/src/net/sf/orcc/xdf/ui/patterns/InstancePattern.java
@@ -487,17 +487,20 @@ public class InstancePattern extends AbstractPattern {
 			return;
 		}
 
-		// Set the current instance's entity
-		final Instance instance = (Instance) getBusinessObjectForPictogramElement(instanceShape);
-
-		// Do not update refinement with the same entity
-		if (instance.getEntity() == entity) {
+		// Do not allow
+		final String existingRefinement = Graphiti.getPeService()
+				.getPropertyValue(instanceShape, REFINEMENT_KEY);
+		final String entityUri = entity.eResource().getURI()
+				.toPlatformString(true);
+		if (existingRefinement != null && existingRefinement.equals(entityUri)) {
 			return;
 		}
 
+		// Set the current instance's entity
+		final Instance instance = (Instance) getBusinessObjectForPictogramElement(instanceShape);
 		instance.setEntity(entity);
 		Graphiti.getPeService().setPropertyValue(instanceShape, REFINEMENT_KEY,
-				entity.eResource().getURI().toPlatformString(true));
+				entityUri);
 
 		// Clean all ports anchors and graphics in the instance
 		final List<GraphicsAlgorithm> gaChildren = new ArrayList<GraphicsAlgorithm>(instanceShape