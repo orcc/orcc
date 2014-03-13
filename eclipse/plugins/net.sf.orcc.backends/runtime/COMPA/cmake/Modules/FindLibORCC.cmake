057db4c8ed8244d674aa030f68010532c02b87d6
unknown <yolivave@LAURMIKER.irisa.fr>
2014-03-13 17:41:53 +0100

    cmake module for finding orcc library is added

---
+++ eclipse/plugins/net.sf.orcc.backends/runtime/COMPA/cmake/Modules/FindLibORCC.cmake
@@ -0,0 +1,15 @@
+# - Try to find liborcc.a 
+
+find_path(LibORCC_INCLUDE_DIR orcc.h 
+		HINTS ${CMAKE_SOURCE_DIR} ../libs/orcc/include)
+
+set(LibORCC_INCLUDE_DIRS ${LibORCC_INCLUDE_DIR} )
+
+find_library(LibORCC_LIBRARY orcc 
+		HINTS ${CMAKE_SOURCE_DIR} ../libs/orcc)
+set(LibORCC_LIBRARIES ${LibORCC_LIBRARY} )
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibORCC DEFAULT_MSG LibORCC_LIBRARY LibORCC_INCLUDE_DIR)
+
+mark_as_advanced(LibORCC_INCLUDE_DIR LibORCC_LIBRARY )
