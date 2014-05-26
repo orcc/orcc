008eb0bc69b38126b32abc389c5c01a33e79f016
Daniele Renzi <daniele.renzi@epfl.ch>
2014-05-26 17:23:19 +0200

    (orcc-native) openhevc_sse: remove parameter from functions hevc_v_loop_filter_luma and hevc_v_loop_filter_chroma

--- eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/openhevc_sse.c
+++ eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/openhevc_sse.c
@@ -181,13 +181,13 @@ void hevc_h_loop_filter_luma_orcc (u8 *_pix, int _stride, int *_beta, int *_tc, 
   hevcDsp.hevc_h_loop_filter_luma(pix, _stride, _beta, _tc, _no_p, _no_q);
 }
 
-void hevc_v_loop_filter_chroma_orcc (u8 *_pix, int _stride, int *_tc, u8 *_no_p, u8 *_no_q, u16 blkAddr[2][2], int idxBlk)
+void hevc_v_loop_filter_chroma_orcc (u8 *_pix, int _stride, int *_tc, u8 *_no_p, u8 *_no_q, u16 blkAddr[2][2])
 {
   u8 * pix = &_pix[(blkAddr[0][1] >> 1) * _stride + (blkAddr[0][0] >> 1)];
   hevcDsp.hevc_v_loop_filter_chroma(pix, _stride, _tc, _no_p, _no_q);
 }
 
-void hevc_h_loop_filter_chroma_orcc (u8 *_pix, int _stride, int *_tc, u8 *_no_p, u8 *_no_q, u16 blkAddr[2][2], int idxBlk)
+void hevc_h_loop_filter_chroma_orcc (u8 *_pix, int _stride, int *_tc, u8 *_no_p, u8 *_no_q, u16 blkAddr[2][2])
 {
 	u8 * pix = &_pix[(blkAddr[0][1] >> 1) * _stride + (blkAddr[0][0] >> 1)];
   hevcDsp.hevc_h_loop_filter_chroma(pix, _stride, _tc, _no_p, _no_q);
