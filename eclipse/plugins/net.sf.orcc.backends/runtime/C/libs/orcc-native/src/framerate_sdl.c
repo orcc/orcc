orcc/orcc

commit 45276517f1586ba0137b43ad33fbfc1264f94ae6

(orcc-lib) Remove the depencencies to sys/stat.h which is not standard: This improve the portability of the library.

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/native_util.c
@@ -1,0 +1,42 @@
+/*
+ * Copyright (c) 2013, INSA of Rennes
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "native_util.h"
+
+
+int fsize(FILE *fp) {
+    int prev=ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    int sz=ftell(fp);
+    fseek(fp,prev,SEEK_SET); //go back to where we were
+    return sz;
+}

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/CMakeLists.txt
@@ -7,10 +7,12 @@ set(orcc_native_sources
     src/compare.c
     src/compareyuv.c
     src/native.c
+    src/native_util.c
 )
 
 set(orcc_native_headers
     include/native.h
+    include/native_util.h
 )
 
 # Helps CMake to find SDL libraries (Windows only)

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/include/native_util.h
@@ -1,0 +1,35 @@
+/*
+ * Copyright (c) 2014, INSA of Rennes
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _ORCC_NATIV_UTIL_H_
+#define _ORCC_NATIV_UTIL_H_
+
+int fsize(FILE *fp);
+
+#endif  /* _ORCC_NATIV_UTIL_H_ */

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/access_file.c
@@ -31,7 +31,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
 #include "orcc.h"
 
@@ -58,9 +57,7 @@ int closeFile(long desc) {
 
 int sizeOfFile(long desc){
     FILE* fd = (FILE*) desc;
-    struct stat st; 
-    fstat(fileno(fd), &st); 
-    return st.st_size;
+    return fsize(fd);
 }
 
 unsigned char readByte(long desc){

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/compare.c
@@ -29,7 +29,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
 #include "orcc.h"
 #include "util.h"

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/compareyuv.c
@@ -26,7 +26,6 @@
 * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 * SUCH DAMAGE.
 */
-#include <sys/stat.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,6 +34,8 @@
 #include "types.h"
 #include "util.h"
 #include "options.h"
+#include "native_util.h"
+
 
 static FILE        *ptrFile ;
 static unsigned int fileSize;
@@ -89,8 +90,6 @@ static int compareYUV_compareComponent(
 
 void compareYUV_init()
 {
-    struct stat st;
-
     //Fix me!! Dirty but it's the only way for the moment.
     if (opt->yuv_file == NULL) {
         useCompare = 0;
@@ -103,8 +102,7 @@ void compareYUV_init()
         exit(-1);
     }
 
-    fstat(fileno(ptrFile), &st);
-    fileSize = st.st_size;
+    fileSize = fsize(ptrFile);
 
     compareErrors = 0;
 }

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/source.c
@@ -29,11 +29,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 #include <time.h>
 #include <string.h>
 
 #include "orcc.h"
+#include "native_util.h"
 #include "util.h"
 #include "options.h"
 
@@ -136,16 +136,12 @@ void source_exit(int exitCode)
 }
 
 unsigned int source_sizeOfFile() {
-    struct stat st; 
-    fstat(fileno(file), &st); 
-    return st.st_size; 
+    return fsize(file);
 }
 
 int source_sizeOfFileFd(long long fdVal) {
     FILE* fd = (FILE*) fdVal;
-    struct stat st;
-    fstat(fileno(fd), &st);
-    return st.st_size;
+    return fsize(fd);
 }
 
 int source_is_stopped() {

commit 2b51a07a8da1a199d53f6aa77dc494fd70516ba9

Move variable declarations to fix old MSVC versions
Commit orcc/orcc@45276517f1586ba0137b43ad33fbfc1264f94ae6 introduced a
compilation error for old versions of Microsoft's MSVC. Deepayan
Bhowmik has identified two cases in orcc-native/ that violate C90
rules, where declarations follow statements in framerate_sdl.c and
native_util.c . See http://stackoverflow.com/a/13336213/1526266 .
Whilst this is fixed in the Microsoft Platform Toolset v120, this
commit provides a fix for older platform versions too.

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/framerate_sdl.c
@@ -79,12 +79,13 @@ void fpsPrintInit_mapping() {
 
 void fpsPrintNewPicDecoded(void) {
     unsigned int endTime;
+    float relativeTime;
 
     numPicturesDecoded++;
     partialNumPicturesDecoded++;
     endTime = SDL_GetTicks();
 
-    float relativeTime = (endTime - relativeStartTime) / 1000.0f;
+    relativeTime = (endTime - relativeStartTime) / 1000.0f;
 
     if(relativeTime >= 5) {
         float framerate = (numPicturesDecoded - lastNumPic) / relativeTime;

eclipse/plugins/net.sf.orcc.backends/runtime/C/libs/orcc-native/src/native_util.c
@@ -34,9 +34,10 @@
 
 
 int fsize(FILE *fp) {
+    int sz;
     int prev=ftell(fp);
     fseek(fp, 0L, SEEK_END);
-    int sz=ftell(fp);
+    sz=ftell(fp);
     fseek(fp,prev,SEEK_SET); //go back to where we were
     return sz;
 }