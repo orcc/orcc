a21c1e4835ec4807985186c57cb1cdd35d48c770
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2014-09-09 17:51:23 +0200

    (hls) First step of migration to the new backend infrastructure

--- eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/c/hls/HLSBackend.java
+++ eclipse/plugins/net.sf.orcc.backends/src/net/sf/orcc/backends/c/hls/HLSBackend.java
@@ -29,9 +29,7 @@
 package net.sf.orcc.backends.c.hls;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import net.sf.orcc.backends.c.CBackend;
@@ -39,7 +37,6 @@ import net.sf.orcc.backends.transform.CastArgFuncCall;
 import net.sf.orcc.backends.transform.DisconnectedOutputPortRemoval;
 import net.sf.orcc.backends.transform.Multi2MonoToken;
 import net.sf.orcc.backends.util.Validator;
-import net.sf.orcc.df.Actor;
 import net.sf.orcc.df.Instance;
 import net.sf.orcc.df.Network;
 import net.sf.orcc.df.transform.ArgumentEvaluator;
@@ -48,18 +45,13 @@ import net.sf.orcc.df.transform.Instantiator;
 import net.sf.orcc.df.transform.NetworkFlattener;
 import net.sf.orcc.df.transform.TypeResizer;
 import net.sf.orcc.df.transform.UnitImporter;
-import net.sf.orcc.df.util.DfSwitch;
 import net.sf.orcc.df.util.DfVisitor;
 import net.sf.orcc.df.util.NetworkValidator;
 import net.sf.orcc.ir.transform.RenameTransformation;
 import net.sf.orcc.tools.classifier.Classifier;
 import net.sf.orcc.tools.merger.action.ActionMerger;
-import net.sf.orcc.tools.merger.actor.ActorMerger;
 import net.sf.orcc.util.OrccLogger;
-import net.sf.orcc.util.OrccUtil;
 import net.sf.orcc.util.Result;
-
-import org.eclipse.emf.ecore.util.EcoreUtil;
 
 /**
  * C backend targeting high-level synthesis tools
@@ -70,10 +62,6 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
  */
 public class HLSBackend extends CBackend {
 
-	/**
-	 * Path to target "testBench" folder
-	 */
-	
 	private String commandPath;
 
 	@Override
@@ -85,89 +73,71 @@ public class HLSBackend extends CBackend {
 
 	@Override
 	protected void doInitializeOptions() {
+
+		super.doInitializeOptions();
+
 		srcPath = path + File.separator + "HLSBackend";
 		commandPath = srcPath + File.separator + "batchCommand";
-	}
 
-	@Override
-	protected void doTransformActor(Actor actor) {
-		Map<String, String> replacementMap = new HashMap<String, String>();
-		replacementMap.put("abs", "abs_my_precious");
-		replacementMap.put("getw", "getw_my_precious");
-		replacementMap.put("index", "index_my_precious");
-		replacementMap.put("max", "max_my_precious");
-		replacementMap.put("min", "min_my_precious");
-		replacementMap.put("select", "select_my_precious");
-		replacementMap.put("OUT", "OUT_my_precious");
-		replacementMap.put("IN", "IN_my_precious");
-		replacementMap.put("bitand", "bitand_my_precious");
+		// Configure the map used in RenameTransformation
+		final Map<String, String> renameMap = new HashMap<String, String>();
+		renameMap.put("abs", "abs_my_precious");
+		renameMap.put("getw", "getw_my_precious");
+		renameMap.put("index", "index_my_precious");
+		renameMap.put("max", "max_my_precious");
+		renameMap.put("min", "min_my_precious");
+		renameMap.put("select", "select_my_precious");
+		renameMap.put("OUT", "OUT_my_precious");
+		renameMap.put("IN", "IN_my_precious");
+		renameMap.put("bitand", "bitand_my_precious");
 
+		// -----------------------------------------------------
+		// Transformations that will be applied on the Network
+		// -----------------------------------------------------
+		networkTransfos.add(new Instantiator(false));
+		networkTransfos.add(new NetworkFlattener());
+		if (classify) {
+			OrccLogger.traceln("Classification of actors...");
+			networkTransfos.add(new Classifier());
+		}
+		if (mergeActors) {
+			OrccLogger.traceln("Merging of actors...");
+			networkTransfos.add(new ActionMerger());
+		}
+		networkTransfos.add(new BroadcastAdder());
+		networkTransfos.add(new ArgumentEvaluator());
+		networkTransfos.add(new DisconnectedOutputPortRemoval());
+
+		// -----------------------------------------------------
+		// Transformations that will be applied on Instances
+		// -----------------------------------------------------
 		if (mergeActions) {
-			new ActionMerger().doSwitch(actor);
+			childrenTransfos.add(new ActionMerger());
 		}
 		if (convertMulti2Mono) {
-			new Multi2MonoToken().doSwitch(actor);
+			childrenTransfos.add(new Multi2MonoToken());
 		}
-
-		List<DfSwitch<?>> transformations = new ArrayList<DfSwitch<?>>();
-		transformations.add(new UnitImporter());
-		transformations.add(new TypeResizer(true, true, true, false));
-
-		transformations.add(new RenameTransformation(replacementMap));
-		transformations.add(new DfVisitor<Void>(new CastArgFuncCall()));
-		for (DfSwitch<?> transformation : transformations) {
-			transformation.doSwitch(actor);
-			if (debug) {
-				OrccUtil.validateObject(transformation.toString() + " on "
-						+ actor.getName(), actor);
-			}
-		}
+		childrenTransfos.add(new UnitImporter());
+		childrenTransfos.add(new TypeResizer(true, true, true, false));
+		childrenTransfos.add(new RenameTransformation(renameMap));
+		childrenTransfos.add(new DfVisitor<Void>(new CastArgFuncCall()));
 	}
+
 	@Override
 	protected void doValidate(Network network) {
-		// TODO Auto-generated method stub
 		Validator.checkMinimalFifoSize(network, fifoSize);
 
 		new NetworkValidator().doSwitch(network);
 	}
+
 	@Override
-	protected void doTransformNetwork(Network network) {
-		// instantiate and flattens network
-		OrccLogger.traceln("Instantiating...");
-		new Instantiator(false).doSwitch(network);
-		OrccLogger.traceln("Flattening...");
-		new NetworkFlattener().doSwitch(network);
-
-		if (classify) {
-			OrccLogger.traceln("Classification of actors...");
-			new Classifier().doSwitch(network);
-		}
-		if (mergeActors) {
-			OrccLogger.traceln("Merging of actors...");
-			new ActorMerger().doSwitch(network);
-		}
-
-		new BroadcastAdder().doSwitch(network);
-		new ArgumentEvaluator().doSwitch(network);
-		new DisconnectedOutputPortRemoval().doSwitch(network);
-
+	protected void beforeGeneration(Network network) {
+		network.computeTemplateMaps();
 	}
 
 	@Override
-	protected void doXdfCodeGeneration(Network network) {
-		doTransformNetwork(network);
-
-		if (debug) {
-			// Serialization of the actors will break proxy link
-			EcoreUtil.resolveAll(network);
-		}
-		transformActors(network.getAllActors());
-
-		network.computeTemplateMaps();
-
-		// print instances
-		printChildren(network);
-
+	protected Result doGenerateNetwork(Network network) {
+		final Result result = Result.newInstance();
 		// print network
 		OrccLogger.trace("Printing network... ");
 		if (new NetworkPrinter(network, getOptions()).print(srcPath) > 0) {
@@ -175,7 +145,12 @@ public class HLSBackend extends CBackend {
 		} else {
 			OrccLogger.traceRaw("Done\n");
 		}
+		return result;
+	}
 
+	@Override
+	protected Result doAdditionalGeneration(Network network) {
+		final Result result = Result.newInstance();
 		OrccLogger.trace("Printing network testbench... ");
 		if (new NetworkTestBenchPrinter(network, getOptions()).print(srcPath) > 0) {// VHDLTestBenchPath
 			OrccLogger.traceRaw("Cached\n");
@@ -203,19 +178,34 @@ public class HLSBackend extends CBackend {
 		} else {
 			OrccLogger.traceRaw("Done\n");
 		}
-
+		return result;
 	}
 
 	@Override
-	protected boolean printInstance(Instance instance) {
-
+	protected Result doGenerateInstance(Instance instance) {
+		final Result result = Result.newInstance();
+		new InstancePrinter(getOptions()).print(srcPath, instance);
+		return result;
+	}
+	
+	@Override
+	protected Result doAdditionalGeneration(Instance instance) {
+		final Result result = Result.newInstance();
 		new InstanceCosimPrinter(getOptions()).print(srcPath, instance);
 		new InstancePrinterCast(getOptions()).print(srcPath, instance);
 		new ActorTopVhdlPrinter(getOptions()).print(srcPath, instance);
 		new ActorNetworkTestBenchPrinter(getOptions()).print(srcPath, instance);
 		new UnitaryBatchCommandPrinter(getOptions()).print(commandPath, instance);
 		new UnitaryBatchCommandPrinterLinux(getOptions()).print(commandPath, instance);
-		return new InstancePrinter(getOptions()).print(srcPath, instance) > 0;
+		return result;
+	}
 
+	//FIXME: delete these useless methods
+	@Override
+	protected boolean printInstance(Instance instance) {
+		return false;
+	}
+	@Override
+	protected void doXdfCodeGeneration(Network network) {
 	}
 }
