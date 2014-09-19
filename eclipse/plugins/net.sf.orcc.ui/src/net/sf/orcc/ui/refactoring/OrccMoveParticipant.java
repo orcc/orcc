c8fa27fb8cee353dfdd4c41d89d175648ee9b2ff
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2014-09-19 13:51:05 +0200

    (ui) Create a new class to manage all updates related to move actions

---
+++ eclipse/plugins/net.sf.orcc.ui/src/net/sf/orcc/ui/refactoring/OrccMoveParticipant.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2014, IETR/INSA of Rennes
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the IETR/INSA of Rennes nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+package net.sf.orcc.ui.refactoring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
+import org.eclipse.ltk.core.refactoring.participants.ISharableParticipant;
+import org.eclipse.ltk.core.refactoring.participants.MoveParticipant;
+import org.eclipse.ltk.core.refactoring.participants.RefactoringArguments;
+
+/**
+ * This sharable move participant perform all updates necessary when 1 or more
+ * file are moved accross packages.
+ * 
+ * @author Antoine Lorence
+ *
+ */
+public class OrccMoveParticipant extends MoveParticipant implements
+		ISharableParticipant {
+
+	private final ChangesFactory factory;
+
+	private IFolder destinationFolder;
+	private final List<IFile> files;
+
+	public OrccMoveParticipant() {
+		super();
+		factory = new ChangesFactory();
+		destinationFolder = null;
+		files = new ArrayList<IFile>();
+	}
+
+	@Override
+	protected boolean initialize(Object element) {
+		final Object dest = getArguments().getDestination();
+		if (dest instanceof IFolder) {
+			if (element instanceof IFile) {
+				files.clear();
+				files.add((IFile) element);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void addElement(Object element, RefactoringArguments arguments) {
+		if (element instanceof IFile) {
+			files.add((IFile) element);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "Orcc Move participant";
+	}
+
+	@Override
+	public RefactoringStatus checkConditions(IProgressMonitor pm,
+			CheckConditionsContext context) throws OperationCanceledException {
+		return new RefactoringStatus();
+	}
+
+	@Override
+	public Change createChange(IProgressMonitor pm) throws CoreException,
+			OperationCanceledException {
+		return null;
+	}
+
+}
